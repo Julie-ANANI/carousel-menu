ffe99d70be5fac3f6f5a1e9d70d9d8d4c8aa9c8f
Bastien Scanu <bastien@MacBook-Pro-de-Philippe.local>
2017-09-05 15:02:56 +0200

    squelette CampaignService

---
+++ src/app/services/campaign/campaign.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '../http';
+import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/observable/throw';
+
+@Injectable()
+export class CampaignService {
+
+}
