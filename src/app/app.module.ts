ebfb1eb5b084f986c8fce451fc730b7de717b9a7
Bastien Scanu <bastien@MacBook-Pro-de-Philippe.local>
2017-09-12 17:44:26 +0200

    smartQueryService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import {CookieService, CookieOptions} from "angular2-cookie/core";
 import {WhitemarkService} from "./services/whitemark/whitemark.service";
 import {InnovationService} from "./services/innovation/innovation.service";
 import {CampaignService} from "./services/campaign/campaign.service";
+import {SmartQueryService} from "./services/smartQuery/smartQuery.service";
 import {UserService} from "./services/user/user.service";
 import {MediaService} from "./services/media/media.service";
 import {SimpleNotificationsModule} from "angular2-notifications";
@@ -51,6 +52,7 @@ import {HttpLoaderComponent} from "./components/http-loader/http-loader.componen
     CampaignService,
     MediaService,
     LoaderService,
+    SmartQueryService,
     {
       provide: Http,
       useFactory: httpFactory,
