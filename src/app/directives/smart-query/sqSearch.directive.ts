75594282a1a51681dd6e79359df9e72a8f596412
Bastien Scanu <bastien@MacBook-Pro-de-Philippe.local>
2017-09-13 17:42:50 +0200

    TODO: directives pour filtrer smartQuery

---
+++ src/app/directives/smart-query/sqSearch.directive.ts
@@ -0,0 +1,12 @@
+/**
+ * Created by bastien on 11/09/2017.
+ */
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[sqSearch]'
+})
+export class SqSearch {
+  //TODO
+  constructor() { }
+}
