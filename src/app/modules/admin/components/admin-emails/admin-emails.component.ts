61e3eca5f7ff508dde10e1e2ba518e319e5021d5
Antoine Hintzy <antoine.hintzy@gmail.com>
2017-11-17 14:39:47 +0100

    Commit fichiers oubliés

---
+++ src/app/modules/admin/components/admin-emails/admin-emails.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-emails',
+  templateUrl: './admin-emails.component.html',
+  styleUrls: ['./admin-emails.component.scss']
+})
+export class AdminEmailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/modules/admin/components/admin-patents/admin-patents.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-patents',
+  templateUrl: './admin-patents.component.html',
+  styleUrls: ['./admin-patents.component.scss']
+})
+export class AdminPatentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
