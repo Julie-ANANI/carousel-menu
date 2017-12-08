7d24ce595710e226febd67fab5948c05b8c9d1f8
Antoine Hintzy <antoine.hintzy@gmail.com>
2017-12-08 15:40:07 +0100

    Ajout des placeholder dans l'édition d'un projet en français, manque traduction

--- src/app/modules/client/components/client-project-edit/i18n/fr.ts
+++ src/app/modules/client/components/client-project-edit/i18n/fr.ts
@@ -23,17 +23,19 @@ export const fr = {
     },
     'MARKET': {
       'LABEL': 'Secteurs d’activité',
-      'PLACEHOLDER': 'Branche micro-électronique, secteur photovoltaïque, sécurité industrielle...'
+      'PLACEHOLDER': 'Ex : Branche micro-électronique, secteur photovoltaïque, sécurité industrielle...'
     },
     'PROFESSIONALS': {
       'LABEL': 'Profils des professionnels',
-      'PLACEHOLDER': 'Ingénieur R&D sur les matériaux isolants, responsable business development\nEx : John Doe chez IBM http://www.linkedin.com/in/johndoe',
-      'TO_EXCLUDE': 'Indiquer des personnes à exclure'
+      'PLACEHOLDER': 'Ex : Ingénieur R&D sur les matériaux isolants, responsable business development\nEx : John Doe chez IBM http://www.linkedin.com/in/johndoe',
+      'TO_EXCLUDE': 'Indiquer des personnes à exclure',
+      'NEW_PROFESSIONAL_TO_EXCLUDE_PLACEHOLDER': 'Ex : John Doe (http://www.linkedin.com/in/johndoe)'
     },
     'COMPANIES': {
       'LABEL': 'Types d’entreprises',
-      'PLACEHOLDER': 'Fabricants industriels de composants, distributeurs, éditeurs de logiciels... Ex : IBM, Intel',
-      'TO_EXCLUDE': 'Indiquer des entreprises à ne pas contacter'
+      'PLACEHOLDER': 'Ex : Les équipementiers et constructeurs automobiles, et les sociétés de transport',
+      'TO_EXCLUDE': 'Indiquer des entreprises à ne pas contacter',
+      'NEW_COMPANY_TO_EXCLUDE_PLACEHOLDER': 'Ex : Google'
     },
     'OTHER': {
       'LABEL': 'Compléments / Mots-clefs',
@@ -45,23 +47,36 @@ export const fr = {
       'MAIN_LANG': 'Langue principale',
       'TITLE': {
         'LABEL': 'Donnez un titre à votre projet',
-        'PLACEHOLDER': ''
+        'PLACEHOLDER': 'Ex : Peinture murale luminescente'
       },
       'SUMMARY': {
-        'LABEL': 'Décrivez votre projet en une courte phrase',
-        'PLACEHOLDER': ''
+        'LABEL': 'Résumez votre projet en une courte phrase',
+        'PLACEHOLDER': 'Ex : Renforcement de la sécurité et du confort des usagers de la route en conditions de ' +
+        'visibilité dégradée (nuit, brouillard, pluie, neige, ...) grâce à une nouvelle peinture luminescente, ' +
+        'résistante aux UV et à l\'humidité.'
       },
       'PROBLEM': {
-        'LABEL': 'Quel problème a motivé la création de votre projet ?',
-        'PLACEHOLDER': ''
+        'LABEL': 'Quel problème / point de douleur adressez-vous ? (Domaine visé > Solutions existantes > Limites actuelles)',
+        'PLACEHOLDER': 'Ex : Le renforcement de la sécurité des usagers de la route est l\'enjeu prioritaire pour ' +
+        'tous les acteurs professionnels de ce domaine.\n\nLe marquage routier existant n\'est pas optimal pour le ' +
+        'confort et la sécurité des usagers en conditions de visibilité dégradée et ne conviennent pas aux véhicules ' +
+        'autonomes.\n\nDes peintures luminescentes ont déjà été développées mais elles ne sont pas suffisamment ' +
+        'résistantes aux aggressions extérieures (météo, UV, température, passages répétés de véhicules...). Leur ' +
+        'tenue n\'est que de quelques semaines avant dégradation.'
       },
       'SOLUTION': {
-        'LABEL': 'Quelle solution votre projet apporte ?',
-        'PLACEHOLDER': ''
+        'LABEL': 'Quelle solution proposez-vous et quels en sont les bénéfices ? (Principe > Avantages > Usages)',
+        'PLACEHOLDER': 'Ex : Nous avons développé une nouvelle peinture luminescente qui permet d\'assurer un ' +
+        'meilleur niveau de confort et de sécurité en conditions de visibilité dégradée. Cette peinture luminescente ' +
+        'emmagasine la lumière solaire la journée et celle des phares la nuit, et la restitue en cas de visibilité ' +
+        'dégradée.\n\nNotre formulation est beaucoup plus résistante aux UV et à l\'humidité (3 ans minimum)\n\nDe ' +
+        'plus, cette peinture permet de s\'affranchir d\'éclairage dans certaines zones tout en maintenant un niveau ' +
+        'de confort et de sécurité suffisant; ce qui entraîne d\'importants gains sur la consommation d\'énergie et ' +
+        'la simplicité d\'installation.'
       },
       'ADVANTAGES': {
         'LABEL': 'Quels avantages votre solution apporte-t-elle au marché ?',
-        'INPUT': 'Ajouter cet avantage'
+        'INPUT': 'Ex : Amélioration de la sécurité'
       }
     },
     'WHAT_STAGE': 'À quel stade en est votre projet ?',
