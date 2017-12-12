61e5ac8cd254b34b308b959f33ac5f9ce7c7caa2
Bastien Scanu <bscanu@umi.us>
2017-12-12 11:22:32 +0100

    implémentation du nouveau smart query pour la liste des utilisateurs

--- src/app/modules/admin/components/admin-users/admin-users.component.ts
+++ src/app/modules/admin/components/admin-users/admin-users.component.ts
@@ -15,25 +15,34 @@ export class AdminUsersComponent implements OnInit {
 
   private _users = [];
   private _total = 0;
+  private _config = {
+    fields: '',
+    limit: 10,
+    offset: 0,
+    search: {},
+    sort: {
+      created: -1
+    }
+  };
 
   constructor(private _router: Router,
               private _translateService: TranslateService,
               private _titleService: TranslateTitleService,
-              private _sq: SmartQueryService,
               private _userService: UserService,
-              private _notificationsService: NotificationsService) {
-    this._sq.setRoute('/user');
-  }
+              private _notificationsService: NotificationsService) {}
 
   ngOnInit(): void {
     initTranslation(this._translateService);
     this._titleService.setTitle('USERS.TITLE');
+    this.loadUsers(this._config);
+  }
 
-    this._sq.data$.subscribe(users => {
+  loadUsers(config: any): void {
+    this._config = config;
+    this._userService.getAll(this._config).subscribe(users => {
       this._users = users.result;
       this._total = users._metadata.totalCount;
     });
-    this._sq.getData();
   }
 
   loadInnovations(userId): void {
@@ -42,8 +51,12 @@ export class AdminUsersComponent implements OnInit {
     });
   }
 
-  get sq(): any {
-    return this._sq;
+  set config(value: any) {
+    this._config = value;
+  }
+
+  get config(): any {
+    return this._config;
   }
 
   get total(): number {
