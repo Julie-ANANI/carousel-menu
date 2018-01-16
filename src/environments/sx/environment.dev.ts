8624174e22a1d3d86919a8110ea14db4937751c8
juandavidcruzgomez <jdcruz-gomez@unitedmotionideas.com>
2018-01-16 16:05:31 +0100

    sx

---
+++ src/environments/sx/environment.dev.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://dev.umi.us/api',
+  companyName: 'SpaceX',
+  domain: 'demo',
+  innovationUrl: 'https://sx.umi.us'
+};
---
+++ src/environments/sx/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://appv3.umi.us/api',
+  companyName: 'SpaceX',
+  domain: 'demo',
+  innovationUrl: 'https://sx.umi.us'
+};
