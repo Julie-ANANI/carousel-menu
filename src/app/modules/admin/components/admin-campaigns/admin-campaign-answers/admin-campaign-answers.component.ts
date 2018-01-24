5e6c0ae4d548ec86f9f54cd6d8e66215ed320178
marcvincenti <mvincenti@umi.us>
2018-01-24 15:07:33 +0100

    Check if project have a preset before getting sections of the preset.

--- src/app/modules/admin/components/admin-campaigns/admin-campaign-answers/admin-campaign-answers.component.ts
+++ src/app/modules/admin/components/admin-campaigns/admin-campaign-answers/admin-campaign-answers.component.ts
@@ -31,10 +31,11 @@ export class AdminCampaignAnswersComponent implements OnInit {
     this._campaign = this._activatedRoute.snapshot.data['campaign'];
     this.loadAnswers();
     this._modalAnswer = null;
-    this._campaign.innovation.preset.sections.forEach(section => {
-      this._questions = this._questions.concat(section.questions);
-    });
-    console.log(this._questions);
+    if (this._campaign.innovation.preset && Array.isArray(this._campaign.innovation.preset.sections)) {
+      this._campaign.innovation.preset.sections.forEach(section => {
+        this._questions = this._questions.concat(section.questions);
+      });
+    }
   }
 
   loadAnswers(): void {
