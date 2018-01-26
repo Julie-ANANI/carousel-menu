1506d1ad2fa9b8a9e5259314ce7cca6208b92e99
Bastien Scanu <bscanu@umi.us>
2018-01-26 18:01:45 +0100

    Settings du projet côté admin : on peut changer le preset, et sauvegarder toutes les options

--- src/app/modules/admin/components/admin-projects/admin-project-details/admin-project-details.component.ts
+++ src/app/modules/admin/components/admin-projects/admin-project-details/admin-project-details.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { TranslateTitleService } from '../../../../../services/title/title.service';
 import { InnovationService } from '../../../../../services/innovation/innovation.service';
 import { TranslateService } from '@ngx-translate/core';
+import { ISubscription } from "rxjs/Subscription";
+import {TranslateNotificationsService} from '../../../../../services/notifications/notifications.service';
 
 @Component({
   selector: 'app-admin-project-details',
@@ -11,35 +13,62 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class AdminProjectsDetailsComponent implements OnInit {
 
-  private _projectInformation: any = {};
+  private _project: any = {};
+  private _preset: any;
   private _tabs = ['settings', 'cards', 'campaigns', 'synthesis', 'mail_config'];
   private _currentPage = 'settings';
+  private _subscriptions: Array<ISubscription> = [];
 
   constructor(private _activatedRoute: ActivatedRoute,
               private _router: Router,
               private _translateService: TranslateService,
               private _innovationService: InnovationService,
+              private _notificationsService: TranslateNotificationsService,
               private _titleService: TranslateTitleService) {}
 
   ngOnInit(): void {
     this._titleService.setTitle('MY_PROJECTS.TITLE');
-    this._projectInformation = this._activatedRoute.snapshot.data['innovation'];
+    this._project = this._activatedRoute.snapshot.data['innovation'];
     const url = this._router.routerState.snapshot.url.split('/');
     if (url && url[5]) { this._currentPage = url[5]; }
+    this._preset = [this._project.preset];
   }
 
   get innovationTitle(): string {
-    return this._projectInformation.name || 'Untitled';
+    return this._project.name || 'Untitled';
   }
 
-  get projectSettings(): any {
-    return this._projectInformation.settings;
+  set preset(value: any) { this._preset = value; }
+  get preset(): any { return this._preset; }
+  
+  public updatePreset(event) {
+    this._preset = event.value;
+    this._project.preset = this._preset[0];
   }
 
-  set projectSettings(_settings: any) {
-    this._projectInformation.settings = _settings;
+  public updateSettings(value) {
+    this._project.settings = value;
   }
 
+  /**
+   * Sauvegarde
+   */
+  public save() {
+    const saveSubs = this._innovationService
+      .save(this._project.id, this._project)
+      .subscribe(data => {
+        this._project = data;
+      }, err => {
+        this._notificationsService.error('ERROR.PROJECT.UNFORBIDDEN', err);
+      });
+    this._subscriptions.push(saveSubs);
+  }
+
+  ngOnDestroy() {
+    this._subscriptions.forEach(subs=>{
+      subs.unsubscribe();
+    });
+  }
 
 
   /**
@@ -70,8 +99,8 @@ export class AdminProjectsDetailsComponent implements OnInit {
   get dateFormat(): string {
     return this._translateService.currentLang === 'fr' ? 'dd/MM/y' : 'y/MM/dd';
   }
-  get baseUrl(): any { return `/admin/projects/project/${this._projectInformation._id}/`; }
+  get baseUrl(): any { return `/admin/projects/project/${this._project._id}/`; }
   get tabs(): any { return this._tabs; }
   get currentPage() { return this._currentPage; }
-  get project() { return this._projectInformation; }
+  get project() { return this._project; }
 }
