ed98fac3d8a73681a6e26b73f5b94f314b7e547f
BastienScanu <bscanu@umi.us>
2018-02-15 11:55:06 +0100

    mise en forme plus claire

--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -1,11 +1,20 @@
+// Modules externes
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
+
+//Modules
 import { AdminRoutingModule } from './admin-routing.module';
+import { SharedModule } from '../shared/shared.module';
+import { AdminPresetsModule } from './components/admin-preset/admin-presets/admin-presets.module';
+import { AdminQuestionsModule } from './components/admin-preset/admin-questions/admin-questions.module';
+import { AdminSectionsModule } from './components/admin-preset/admin-sections/admin-sections.module';
+import { AdminSearchModule } from './components/admin-search/admin-search.module';
+
+//Components
 import { AdminComponent } from './admin.component';
 import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
 import { AdminUsersComponent } from './components/admin-users/admin-users.component';
-import { SharedModule } from '../shared/shared.module';
-import { TranslateModule } from '@ngx-translate/core';
 import { AdminProjectsComponent } from './components/admin-projects/admin-projects.component';
 import { AdminCampaignsComponent } from './components/admin-campaigns/admin-campaigns.component';
 import { AdminEmailsComponent } from './components/admin-emails/admin-emails.component';
@@ -17,9 +26,6 @@ import { AdminUserDetailsComponent } from './components/admin-users/admin-user-d
 import { AdminProjectsListComponent } from './components/admin-projects-list/admin-projects-list.component';
 import { AdminProjectsDetailsComponent } from './components/admin-projects/admin-project-details/admin-project-details.component';
 import { AdminPresetComponent } from './components/admin-preset/admin-preset.component';
-import { AdminPresetsModule } from './components/admin-preset/admin-presets/admin-presets.module';
-import { AdminQuestionsModule } from './components/admin-preset/admin-questions/admin-questions.module';
-import { AdminSectionsModule } from './components/admin-preset/admin-sections/admin-sections.module';
 import { AdminCampaignComponent } from './components/admin-campaigns/admin-campaign/admin-campaign.component';
 import { AdminCampaignDetailsComponent } from './components/admin-campaigns/admin-campaign-details/admin-campaign-details.component';
 import { AdminCampaignProsComponent } from './components/admin-campaigns/admin-campaign-pros/admin-campaign-pros.component';
@@ -27,7 +33,8 @@ import { AdminCampaignHistoryComponent } from './components/admin-campaigns/admi
 import { AdminCampaignSearchComponent } from './components/admin-campaigns/admin-campaign-search/admin-campaign-search.component';
 import { AdminCampaignAnswersComponent } from './components/admin-campaigns/admin-campaign-answers/admin-campaign-answers.component';
 import { AdminAnswersListComponent } from './components/admin-answers-list/admin-answers-list.component';
-import { AdminSearchModule } from './components/admin-search/admin-search.module';
+
+//Pipes
 import { DateFormatPipe } from '../../pipes/DateFormatPipe';
 
 @NgModule({
