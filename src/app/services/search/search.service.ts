9458484071d6494601069e07108a4389ad7e3ffc
BastienScanu <bscanu@umi.us>
2018-02-21 15:18:18 +0100

    recherche des mails des pros

--- src/app/services/search/search.service.ts
+++ src/app/services/search/search.service.ts
@@ -51,4 +51,10 @@ export class SearchService {
       .map((res: Response) => res.json())
       .catch((error: Response) => Observable.throw(error.text()));
   }
+
+  public searchMails(config: any): Observable<any> {
+    return this._http.post('/search/searchMails', config)
+      .map((res: Response) => res.json())
+      .catch((error: Response) => Observable.throw(error.text()));
+  }
 }
