64a51cfec7b4d4bdfec693099c58cbf5de321e58
BastienScanu <bscanu@umi.us>
2018-03-05 16:22:35 +0100

    affichage "module en pause" ou "en fonctionnement"

--- src/app/modules/shared/components/shared-search-history/shared-search-history.component.ts
+++ src/app/modules/shared/components/shared-search-history/shared-search-history.component.ts
@@ -46,18 +46,19 @@ export class SharedSearchHistoryComponent implements OnInit {
     }
     this.loadHistory();
   }
-  
+
   public loadHistory() {
     this._searchService.getRequests(this._config)
       .first()
       .subscribe(result => {
         this._requests = result.requests;
+        this._paused = result._metadata.paused;
         if (result._metadata) {
           this._total = result._metadata.totalCount;
         }
       });
   }
-  
+
   public getChildren (request: any) {
     if (!request.loaded) {
       this._searchService.getRequests({
