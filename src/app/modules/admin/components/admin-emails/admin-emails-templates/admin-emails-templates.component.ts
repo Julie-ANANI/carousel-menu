dbb28d746ce553fecf4477185157f7a132bd0af0
BastienScanu <bscanu@umi.us>
2018-03-27 11:17:04 +0200

    affichage du menu

--- src/app/modules/admin/components/admin-emails/admin-emails-templates/admin-emails-templates.component.ts
+++ src/app/modules/admin/components/admin-emails/admin-emails-templates/admin-emails-templates.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TemplatesService } from '../../../../../services/templates/templates.service';
+import { EmailScenario } from '../../../../../models/email-scenario';
+import { EmailSignature } from '../../../../../models/email-signature';
 
 @Component({
   selector: 'app-admin-email-templates',
   templateUrl: 'admin-emails-templates.component.html',
   styleUrls: ['admin-emails-templates.component.scss']
 })
-export class AdminEmailTemplatesComponent {
-  
-  constructor() { }
+export class AdminEmailTemplatesComponent implements OnInit {
+
+  private _scenarios: Array<EmailScenario>;
+  private _signatures: Array<EmailSignature>;
+
+  constructor(private _templatesService: TemplatesService) {}
+
+  ngOnInit() {
+    this._templatesService.getAll({limit: 0}).first().subscribe((scenarios: any) => {
+      this._scenarios = scenarios.result;
+    });
+    this._templatesService.getAllSignatures({limit: 0}).first().subscribe((signatures: any) => {
+      this._signatures = signatures.result;
+    });
+  }
+
+  get scenarios(): Array<EmailScenario> { return this._scenarios; }
+  get signatures(): Array<EmailSignature> { return this._signatures; }
 }
