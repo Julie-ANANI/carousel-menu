99f11b4af1a1921d05e7cd9c43e6380b95b3e83a
BastienScanu <bscanu@umi.us>
2018-03-27 11:18:19 +0200

    interface de création de scénario de mails/signature

--- src/app/modules/admin/components/admin-emails/admin-emails-templates/admin-new-scenario/admin-new-scenario.component.ts
+++ src/app/modules/admin/components/admin-emails/admin-emails-templates/admin-new-scenario/admin-new-scenario.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { TemplatesService } from '../../../../../../services/templates/templates.service';
 
 @Component({
   selector: 'app-admin-new-scenario',
@@ -6,6 +8,27 @@ import { Component } from '@angular/core';
   styleUrls: ['admin-new-scenario.component.scss']
 })
 export class AdminNewScenarioComponent {
-  
-  constructor() { }
+
+  private _newScenarioName: string = null;
+  private _newSignatureName: string = null;
+
+  constructor(private _templatesService: TemplatesService,
+              private _router: Router) { }
+
+  public createScenario() {
+    this._templatesService.create({name: this._newScenarioName}).first().subscribe(newScenario => {
+      this._router.navigate(['/admin/emails/templates/scenario/' + newScenario._id])
+    });
+  }
+
+  public createSignature() {
+    this._templatesService.createSignature({name: this._newSignatureName}).first().subscribe(newSignature => {
+      this._router.navigate(['/admin/emails/templates/signature/' + newSignature._id])
+    });
+  }
+
+  get newScenarioName(): string { return this._newScenarioName; }
+  get newSignatureName(): string { return this._newSignatureName; }
+  set newScenarioName(name: string) { this._newScenarioName = name; }
+  set newSignatureName(name: string) { this._newSignatureName = name; }
 }
