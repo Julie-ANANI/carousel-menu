ebcce2de516c3de64a01c5504e3197ce61287ac1
marcvincenti <mvincenti@umi.us>
2018-04-03 11:46:18 +0200

    sort lists by number of occurences.

--- src/app/modules/shared/components/shared-market-report/components/shared-market-item-list-component/shared-market-item-list.component.ts
+++ src/app/modules/shared/components/shared-market-report/components/shared-market-item-list-component/shared-market-item-list.component.ts
@@ -1,7 +1,7 @@
 /**
  * Created by juandavidcruzgomez on 11/09/2017.
  */
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, OnInit, EventEmitter } from '@angular/core';
 import * as _ from 'lodash';
 import { Answer } from '../../../../../../models/answer';
 
@@ -11,13 +11,24 @@ import { Answer } from '../../../../../../models/answer';
   styleUrls: ['shared-market-item-list.component.scss']
 })
 
-export class SharedMarketItemListComponent {
+export class SharedMarketItemListComponent implements OnInit {
 
   @Input() public list: any;
   @Input() public answers: Array<Answer>;
   @Output() modalAnswerChange = new EventEmitter<any>();
 
   constructor() { }
+
+  ngOnInit() {
+    this.list = this.list
+      .sort((a: any, b: any) => {
+        if ((b.count || 1) - (a.count || 1) === 0) {
+          return b.value.length - a.value.length;
+        } else {
+          return (b.count || 1) - (a.count || 1);
+        }
+      });
+  }
 
   public getAnswers(commentsList: Array<any>): Array<Answer> {
     if (this.answers) {
