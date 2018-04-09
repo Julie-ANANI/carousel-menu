6ce217de411a348a5698fe1de6abff0d672ccf2e
BastienScanu <bscanu@umi.us>
2018-04-09 10:52:18 +0200

    $event.preventDefault()

--- src/app/modules/shared/components/shared-project-edit/shared-project-edit.component.ts
+++ src/app/modules/shared/components/shared-project-edit/shared-project-edit.component.ts
@@ -71,7 +71,7 @@ export class SharedProjectEditComponent implements OnInit, OnDestroy, ComponentC
 
     this.formData.patchValue(this._project);
 
-    if (!this.isAdmin && !this.canEdit) {
+    if (!this.canEdit) {
       this.formData.disable();
     }
     for (const innovationCard of this._project.innovationCards) {
@@ -109,19 +109,19 @@ export class SharedProjectEditComponent implements OnInit, OnDestroy, ComponentC
    * @param callback
    */
   public save(callback: () => void): void {
-    if (this.canEdit || this.isAdmin) {
+    if (this.canEdit) {
       this._innovationService
         .save(this._project._id, this.formData.value)
         .first()
         .subscribe((data: Innovation) => {
-        this.lastSavedDate = new Date(data.updated);
-        this.shouldSave = false;
-        if (callback) {
-          callback();
-        }
-      }, err => {
-        this._notificationsService.error('ERROR.PROJECT.UNFORBIDDEN', err);
-      });
+          this.lastSavedDate = new Date(data.updated);
+          this.shouldSave = false;
+          if (callback) {
+            callback();
+          }
+        }, err => {
+          this._notificationsService.error('ERROR.PROJECT.UNFORBIDDEN', err);
+        });
     } else {
       this._notificationsService.error('ERROR.PROJECT.UNFORBIDDEN', 'ERROR.CANT_EDIT');
     }
@@ -141,35 +141,36 @@ export class SharedProjectEditComponent implements OnInit, OnDestroy, ComponentC
       }
     };
     return _inputConfig[type] || {
-      placeholder: 'Input',
-      initialData: ''
-    };
+        placeholder: 'Input',
+        initialData: ''
+      };
   }
 
   private _newInnovationCardFormBuilderGroup (data: InnovCard): any {
     return this._formBuilder.group({
-      id: [{value: data._id, disabled: (!this.isAdmin && !this.canEdit)}, Validators.required],
-      title: [{value: data.title, disabled: (!this.isAdmin && !this.canEdit)}, Validators.required],
-      summary: [{value: data.summary, disabled: (!this.isAdmin && !this.canEdit)}, Validators.required],
-      problem: [{value: data.problem, disabled: (!this.isAdmin && !this.canEdit)}, Validators.required],
-      solution: [{value: data.solution, disabled: (!this.isAdmin && !this.canEdit)}, Validators.required],
-      advantages: [{value: data.advantages, disabled: (!this.isAdmin && !this.canEdit)}],
-      lang: [{value: data.lang, disabled: (!this.isAdmin && !this.canEdit)}, Validators.required],
-      principal: [{value: data.principal, disabled: (!this.isAdmin && !this.canEdit)}, Validators.required],
+      id: [{value: data._id, disabled: !this.canEdit}, Validators.required],
+      title: [{value: data.title, disabled: !this.canEdit}, Validators.required],
+      summary: [{value: data.summary, disabled: !this.canEdit}, Validators.required],
+      problem: [{value: data.problem, disabled: !this.canEdit}, Validators.required],
+      solution: [{value: data.solution, disabled: !this.canEdit}, Validators.required],
+      advantages: [{value: data.advantages, disabled: !this.canEdit}],
+      lang: [{value: data.lang, disabled: !this.canEdit}, Validators.required],
+      principal: [{value: data.principal, disabled: !this.canEdit}, Validators.required],
       // media: [{value: data.media, disabled: !this.canEdit}, Validators.required] // On ne les gère plus dans le reactive form
     });
   }
 
-  public createInnovationCard(): void {
+  public createInnovationCard(event: Event): void {
+    event.preventDefault();
     if (this.canEdit) {
       if (this._project.innovationCards.length < 2 && this._project.innovationCards.length !== 0) {
         this._innovationService.createInnovationCard(this._project._id, {
           lang: this._project.innovationCards[0].lang === 'en' ? 'fr' : 'en' // Pour l'instant il n'y a que deux langues
         }).first()
           .subscribe((data: InnovCard) => {
-          this._addInnovationCardWithData(data);
-          this._project.innovationCards.push(data);
-        });
+            this._addInnovationCardWithData(data);
+            this._project.innovationCards.push(data);
+          });
       }
     }
   }
@@ -189,7 +190,8 @@ export class SharedProjectEditComponent implements OnInit, OnDestroy, ComponentC
     card['advantages'] = event.value;
   }
 
-  public setAsPrincipal (innovationCardId: string): void {
+  public setAsPrincipal (event: Event, innovationCardId: string): void {
+    event.preventDefault();
     const innovationCards = this.formData.get('innovationCards').value;
     for (const innovationCard of innovationCards) {
       innovationCard.principal = innovationCard.id === innovationCardId;
@@ -197,7 +199,8 @@ export class SharedProjectEditComponent implements OnInit, OnDestroy, ComponentC
     this.formData.get('innovationCards').setValue(innovationCards);
   }
 
-  public submitProjectToValidation (): void {
+  public submitProjectToValidation(event: Event): void {
+    event.preventDefault();
     this.save(() => {
       this._innovationService.submitProjectToValidation(this._project._id).first().subscribe(_ => {
         this._router.navigate(['../']);
@@ -214,18 +217,19 @@ export class SharedProjectEditComponent implements OnInit, OnDestroy, ComponentC
       .first()
       .subscribe((res: Innovation) => {
         this._project = res;
-    });
+      });
   }
 
   public newOnlineVideoToAdd (videoInfos: Video): void {
     this._innovationService.addNewMediaVideoToInnovationCard(this._project._id, this._project.innovationCards[this.innovationCardEditingIndex]._id, videoInfos)
       .first()
       .subscribe(res => {
-      this._project = res;
-    });
+        this._project = res;
+      });
   }
 
-  public setMediaAsPrimary (media: Media): void {
+  public setMediaAsPrimary (event: Event, media: Media): void {
+    event.preventDefault();
     this._innovationService.setPrincipalMediaOfInnovationCard(this._project._id, this._project.innovationCards[this.innovationCardEditingIndex]._id, media._id)
       .first()
       .subscribe((res: Innovation) => {
@@ -233,7 +237,8 @@ export class SharedProjectEditComponent implements OnInit, OnDestroy, ComponentC
       });
   }
 
-  public deleteMedia (media: Media): void {
+  public deleteMedia(event: Event, media: Media): void {
+    event.preventDefault();
     this._innovationService.deleteMediaOfInnovationCard(this._project._id, this._project.innovationCards[this.innovationCardEditingIndex]._id, media._id)
       .first()
       .subscribe((res: Innovation) => {
@@ -256,27 +261,30 @@ export class SharedProjectEditComponent implements OnInit, OnDestroy, ComponentC
   public getModel (): any {
     return {
       lang: 'en',
-      jobType: 'innovationCard',
+      jobType: 'innovation',
       labels: 'EXPORT.INNOVATION.CARD',
       pdfDataseedFunction: this.dataBuilder()
     };
   }
 
-  public validateProject (): void {
+  public validateProject(event: Event): void {
+    event.preventDefault();
     this._innovationService.validate(this._project._id).first().subscribe(_ => {
       this._notificationsService.success('Projet validé', 'Le projet a bien été validé');
       this._router.navigate(['/admin']);
     });
   }
 
-  public askRevision (): void {
+  public askRevision(event: Event): void {
+    event.preventDefault();
     this._innovationService.askRevision(this._project._id).first().subscribe(_ => {
       this._notificationsService.success('Projet en révision', 'Le projet a été passé en status de révision, veuillez avertir le propriétaire des chagements à effectuer');
       this._router.navigate(['/admin']);
     });
   }
 
-  public addCollaborators (): void {
+  public addCollaborators (event: Event): void {
+    event.preventDefault();
     if (this.collaborators_emails !== '') {
       this._innovationService.inviteCollaborators(this._project._id, this.collaborators_emails).first().subscribe((data: any) => {
         if (data.usersAdded.length || data.invitationsToSend.length || data.invitationsToSendAgain.length) {
@@ -294,7 +302,8 @@ export class SharedProjectEditComponent implements OnInit, OnDestroy, ComponentC
     }
   }
 
-  public removeCollaborator (collaborator: User): void {
+  public removeCollaborator (event: Event, collaborator: User): void {
+    event.preventDefault();
     this._innovationService.removeCollaborator(this._project._id, collaborator).first().subscribe((collaborators: Array<User>) => {
       this.project.collaborators = collaborators;
     });
@@ -319,7 +328,7 @@ export class SharedProjectEditComponent implements OnInit, OnDestroy, ComponentC
   }
 
   get domSanitizer() { return this._domSanitizer; }
-  get canEdit (): boolean { return this._project && (this._project.status === 'EDITING'); }
+  get canEdit (): boolean { return this._project && (this._project.status === 'EDITING' || this.isAdmin); }
   get dateFormat(): string { return this._translateService.currentLang === 'fr' ? 'dd/MM/y' : 'y/MM/dd'; }
   get project(): Innovation { return this._project; }
   get isAdmin(): boolean { return (this._authService.adminLevel & 3) === 3; }
