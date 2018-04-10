8821d79b83b6f23feee44619c4a863966413808a
BastienScanu <bscanu@umi.us>
2018-04-10 16:13:22 +0200

    édition des innovation cards côté admin

--- src/app/modules/admin/components/admin-project/admin-project-cards/admin-project-cards.component.ts
+++ src/app/modules/admin/components/admin-project/admin-project-cards/admin-project-cards.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Innovation } from '../../../../../models/innovation';
+import { InnovationService } from '../../../../../services/innovation/innovation.service';
+import { TranslateNotificationsService } from '../../../../../services/notifications/notifications.service';
 
 @Component({
   selector: 'app-admin-project-cards',
@@ -10,12 +12,39 @@ import { Innovation } from '../../../../../models/innovation';
 export class AdminProjectCardsComponent implements OnInit {
 
   private _project: Innovation;
-
-  constructor(private _activatedRoute: ActivatedRoute) {}
+  private _formData: any = {};
+  public shouldSave = false; // To prevent leaving page
+  public lastSavedDate: Date;
+  
+  constructor(private _activatedRoute: ActivatedRoute,
+              private _innovationService: InnovationService,
+              private _notificationsService: TranslateNotificationsService) {}
 
   ngOnInit(): void {
     this._project = this._activatedRoute.snapshot.parent.data['innovation'];
   }
-  
+
+  public save(event: Event): void {
+    event.preventDefault();
+    this._innovationService
+      .save(this._project._id, this._formData)
+      .first()
+      .subscribe((data: Innovation) => {
+        this.lastSavedDate = new Date(data.updated);
+        this.shouldSave = false;
+      }, err => {
+        this._notificationsService.error('ERROR.PROJECT.UNFORBIDDEN', err);
+      });
+  }
+
+  public updateCards(event: any) {
+    this.shouldSave = true;
+    this._formData = event;
+  }
+
+  public updateProject(event: Innovation) {
+    this._project = event;
+  }
+
   get project() { return this._project; }
 }
