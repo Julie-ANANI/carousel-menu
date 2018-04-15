4e85d5afae3eda1c2dcba5e96eeaccb9c1bbfa91
abhisheksaini <saini.abhishek@hotmail.com>
2018-04-15 11:48:52 +0200

    implemented the service of client discover page to show innovation that are public and status is done

--- src/app/modules/client/components/client-discover/client-discover.component.ts
+++ src/app/modules/client/components/client-discover/client-discover.component.ts
@@ -1,29 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+
+// Services
 import { TranslateTitleService } from '../../../../services/title/title.service';
 import { InnovationService } from '../../../../services/innovation/innovation.service';
+
+
+// Models
 import { Innovation } from '../../../../models/innovation';
+import { InnovCard } from '../../../../models/innov-card';
 
 @Component({
   selector: 'app-client-discover',
   templateUrl: './client-discover.component.html',
   styleUrls: ['./client-discover.component.scss']
 })
+
 export class ClientDiscoverComponent implements OnInit {
 
-  /*private _innovationTag: String = '';*/
- // private _innovationSearchTag: String = 'null'; /* Search input box innovation tag */
   private innovations: Array<Innovation>;
+  private innovationCards: InnovCard[] = [];
+  private innovationLang: string;
   private totalInnovations: number;
   private innovationCardId: object;
-  // private innovationCards: any;
-  private innovationCardTitle: any;
+
   private _config = {
     fields: '',
     limit: 0,
     offset: 0,
     search: {
       isPublic: 1,
-      status: 'DONE',
+      status: 'DONE'
     },
     sort: {
       created: -1
@@ -48,21 +54,36 @@ export class ClientDiscoverComponent implements OnInit {
       this.totalInnovations = innovations._metadata.totalCount;
       this.innovations.forEach((items) => {
         if (items.innovationCards.length === 2) {
-
+          // english innovation
           this.innovationCardId = items.innovationCards[1];
-          this.getInnovation(this.innovationCardId);
+          this.innovationLang = 'fr';
+          this.getInnovationCard(this.innovationCardId);
         } else {
+          // only innovation present
           this.innovationCardId = items.innovationCards[0];
+          this.innovationLang = items.innovationCards[0].lang;
+          this.getInnovationCard(this.innovationCardId);
         }
       })
     });
   }
 
-  getInnovation(id: any) {
+  getInnovationCard(id: any) {
     this._innovationService.getInnovationCard(id).subscribe(result => {
-      this.innovationCardTitle = result.title;
-      console.log(this.innovationCardTitle);
+      this.innovationCards.push(result);
     });
+
+    // console.log('first' + this.innovationCards);
   }
 
+  set innovationCard(value: Array<any>) {
+    this.innovationCards = value;
+  }
+
+  get innovationCard(): Array<any> {
+    return this.innovationCards;
+  }
 }
+
+
+
