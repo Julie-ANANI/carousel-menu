b10096ef73b403054fb964971d4b7f569f9a840c
juandavidcruzgomez <jdcruz-gomez@unitedmotionideas.com>
2018-04-20 14:39:18 +0200

    logo style et permissions

--- src/app/modules/admin/components/admin-project/admin-project.component.ts
+++ src/app/modules/admin/components/admin-project/admin-project.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { TranslateTitleService } from '../../../../services/title/title.service';
 import { Innovation } from '../../../../models/innovation';
 import { ActivatedRoute } from '@angular/router';
+import { AuthService } from '../../../../services/auth/auth.service';
 
 @Component({
   selector: 'app-admin-project',
@@ -14,11 +15,21 @@ export class AdminProjectComponent implements OnInit {
   private _tabs: Array<string> = ['settings', 'cards', 'campaigns', 'synthesis'];
 
   constructor(private _activatedRoute: ActivatedRoute,
-              private _titleService: TranslateTitleService) {}
+              private _titleService: TranslateTitleService,
+              private _authService: AuthService) {}
 
   ngOnInit(): void {
     this._titleService.setTitle('MY_PROJECTS.TITLE');
     this._project = this._activatedRoute.snapshot.data['innovation'];
+  }
+
+  get authorizedTabs(): Array<string> {
+    const adminLevel = this._authService.adminLevel;
+    if(adminLevel>2) {
+      return this._tabs;
+    } else {
+      return ['settings', 'cards', 'synthesis'];
+    }
   }
 
   public get project(): Innovation { return this._project; }
