62667e73280390aed671f993d366a2df387e80e8
marcvincenti <mvincenti@umi.us>
2018-04-25 16:48:25 +0200

    Add tabs structure to client-project module.

--- src/app/modules/client/client.module.ts
+++ src/app/modules/client/client.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
 import { SharedModule } from '../shared/shared.module';
+import { ClientProjectModule } from './components/client-project/client-project.module';
 import { SharedMarketReportModule } from '../shared/components/shared-market-report/shared-market-report.module';
 import { ClientRoutingModule } from './client-routing.module';
 import { ClientComponent } from './client.component';
 import { ClientMyAccountComponent } from './components/client-my-account/client-my-account.component';
 import { ClientDiscoverComponent } from './components/client-discover/client-discover.component';
 import { ClientDiscoverDescriptionComponent } from './components/client-discover-description/client-discover-description.component';
-import { ClientProjectComponent } from './components/client-project/client-project.component';
 import { ClientMyProjectsComponent } from './components/client-my-projects/client-my-projects.component';
 import { ClientLoginComponent } from './components/client-login/client-login.component';
 import { ClientSignupComponent } from './components/client-signup/client-signup.component';
@@ -25,6 +25,7 @@ import { DiscoverSummaryPipe } from '../../pipes/DiscoverSummaryPipe';
 @NgModule({
   imports: [
     CommonModule,
+    ClientProjectModule,
     ClientRoutingModule,
     SharedModule,
     TranslateModule.forChild(),
@@ -36,7 +37,6 @@ import { DiscoverSummaryPipe } from '../../pipes/DiscoverSummaryPipe';
     ClientMyAccountComponent,
     ClientMyProjectsComponent,
     ClientProjectEditComponent,
-    ClientProjectComponent,
     ClientDiscoverComponent,
     ClientDiscoverDescriptionComponent,
     ClientWelcomeComponent,
--- src/app/modules/client/components/client-project/i18n/en.ts
+++ src/app/modules/client/components/client-project/i18n/en.ts
@@ -1,5 +1,10 @@
+import { EXPLORATION  } from '../components/exploration/i18n/en';
+import { SETUP } from '../components/setup/i18n/en';
+
 export const PROJECT_MODULE = {
-  'SETUP': 'Setup Project',
-  'EXPLORATION': 'Market Exploration',
-  'SYNTHESIS': 'Synthesis'
+  'SETUP_TAB': 'Setup Project',
+  'EXPLORATION_TAB': 'Market Exploration',
+  'SYNTHESIS_TAB': 'Synthesis',
+  'EXPLORATION': EXPLORATION,
+  'SETUP': SETUP
 };
