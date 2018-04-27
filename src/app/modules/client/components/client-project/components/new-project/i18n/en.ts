50b4707acb0316dacedff09b0ba9b278fcfe5d38
marcvincenti <mvincenti@umi.us>
2018-04-27 09:51:50 +0200

    Update new-project component.

--- src/app/modules/client/components/client-project/components/new-project/i18n/en.ts
+++ src/app/modules/client/components/client-project/components/new-project/i18n/en.ts
@@ -1,13 +1,10 @@
 export const NEW_PROJECT = {
-  'TITLE': 'Before to start…',
   'DESCRIPTION': {
     'LINE1': 'In which language would you like to start your project?',
     'LINE2': 'You will be able to translate it in another language later.'
   },
-  'DOWNLOAD': 'Download your project anytime',
-  'ANONYMOUS': 'Remain anonymous',
-  'PRIVATE': 'Nothing will be published at this stage',
   'FORM_NOT_VALID': 'Please answer to all questions',
-  'UNDERSTAND': 'I understand',
-  'COMPANY_NAME_LABEL': 'Give a name to your project'
+  'CREATE': 'Create my project',
+  'COMPANY_NAME_LABEL': 'Give a name to your project',
+  'INNOVATION_TYPE_LABEL': 'Choose your offer'
 };
--- src/app/modules/client/components/client-project/components/new-project/i18n/fr.ts
+++ src/app/modules/client/components/client-project/components/new-project/i18n/fr.ts
@@ -1,13 +1,10 @@
 export const NEW_PROJECT = {
-  'TITLE': 'Avant de commencer…',
   'DESCRIPTION': {
     'LINE1': 'Dans quelle langue souhaitez-vous démarrer votre projet ?',
     'LINE2': 'Vous pourrez traduire votre projet dans l\'autre langue à tout moment.'
   },
-  'DOWNLOAD': 'Télécharger votre projet à tout moment',
-  'ANONYMOUS': 'Anonymat garanti',
-  'PRIVATE': 'Rien ne sera publié à ce stade',
   'FORM_NOT_VALID': 'Merci de répondre à toutes les questions',
-  'UNDERSTAND': 'J\'ai compris',
-  'COMPANY_NAME_LABEL': 'Donnez un nom à votre projet'
+  'CREATE': 'Créer mon projet',
+  'COMPANY_NAME_LABEL': 'Donnez un nom à votre projet',
+  'INNOVATION_TYPE_LABEL': 'Choisissez votre offre'
 };
