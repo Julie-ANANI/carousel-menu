833da322e73476abe8506946e9c16d99fe6b9b08
marcvincenti <mvincenti@umi.us>
2018-04-27 14:54:57 +0200

    Add survey component & remove old synthesis client-component.

--- src/app/modules/client/components/client-project/components/setup/i18n/en.ts
+++ src/app/modules/client/components/client-project/components/setup/i18n/en.ts
@@ -1,7 +1,10 @@
+import { SURVEY } from '../components/survey/i18n/en';
+
 export const SETUP = {
   'TITLE': 'All infos about your project',
   'SUBMIT_PROJECT': 'Submit project',
   'PITCH_TAB': 'Pitch',
   'TARGETING_TAB': 'Targeting',
-  'SURVEY_TAB': 'Survey'
+  'SURVEY_TAB': 'Survey',
+  'SURVEY': SURVEY
 };
