0973a818ad2da141141a1866b63f5e498537e166
marcvincenti <mvincenti@umi.us>
2018-04-30 16:31:32 +0200

    Load an uri in survey tab if quiz exist.

--- src/app/modules/client/components/client-project/components/setup/components/survey/survey.component.ts
+++ src/app/modules/client/components/client-project/components/setup/components/survey/survey.component.ts
@@ -1,15 +1,27 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 import { Innovation } from '../../../../../../../../models/innovation';
+import { environment } from '../../../../../../../../../environments/environment';
 
 @Component({
   selector: 'app-project-survey',
   templateUrl: 'survey.component.html',
   styleUrls: ['survey.component.scss']
 })
-export class SurveyComponent {
+export class SurveyComponent implements OnInit {
 
   @Input() project: Innovation;
 
-  constructor() {}
+  private _url: string;
+
+  constructor(private translateService: TranslateService) {}
+
+  ngOnInit() {
+    if (this.project.quizId) {
+      this._url = environment.quizUrl + '/quiz/' + this.project.quizId + '/preview?lang=' + this.translateService.currentLang;
+    }
+  }
+
+  get url() { return this._url; }
 
 }
