27e35d0173095578bee190bb36708c041cf116dd
marcvincenti <mvincenti@umi.us>
2018-05-03 10:26:52 +0200

    analytics on market exploration (client-side).

--- src/app/modules/client/components/client-project/components/exploration/exploration.component.ts
+++ src/app/modules/client/components/client-project/components/exploration/exploration.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { AnswerService } from '../../../../../../services/answer/answer.service';
+import { InnovationService } from '../../../../../../services/innovation/innovation.service';
 import { TranslateNotificationsService } from '../../../../../../services/notifications/notifications.service';
 import { Answer } from '../../../../../../models/answer';
 import { Clearbit } from '../../../../../../models/clearbit';
@@ -18,12 +19,14 @@ export class ExplorationProjectComponent implements OnInit {
 
   private _contactUrl: string;
   private _answers: Array<Answer>;
+  private _campaignsStats: {nbPros: number, nbProsSent: number, nbProsOpened: number, nbProsClicked: number};
   private _companies: Array<Clearbit>;
   private _countries: Array<string>;
   private _questions: Array<Question>;
   private _modalAnswer: Answer;
 
-  constructor(private answerService: AnswerService, private notificationService: TranslateNotificationsService) {}
+  constructor(private answerService: AnswerService, private innovationService: InnovationService,
+              private notificationService: TranslateNotificationsService) {}
 
   ngOnInit() {
     this._contactUrl = encodeURI('mailto:contact@umi.us?subject=' + this.project.name);
@@ -48,6 +51,22 @@ export class ExplorationProjectComponent implements OnInit {
       }, (error) => {
         this.notificationService.error('ERROR.ERROR', error.message);
       });
+    this._campaignsStats = {nbPros: 0, nbProsSent: 0, nbProsOpened: 0, nbProsClicked: 0};
+    this.innovationService
+      .campaigns(this.project._id)
+      .first()
+      .subscribe((results) => {
+        if (results && Array.isArray(results.result)) {
+          results.result.forEach((campaign) => {
+            this._campaignsStats.nbPros += (campaign.stats.campaign.nbProfessionals || 0);
+            this._campaignsStats.nbProsSent += (campaign.stats.mail.totalPros || 0);
+            this._campaignsStats.nbProsOpened += (campaign.stats.mail.statuses.opened || 0);
+            this._campaignsStats.nbProsClicked += (campaign.stats.mail.statuses.clicked || 0);
+          });
+        }
+      }, (error) => {
+        this.notificationService.error('ERROR.ERROR', error.message);
+      });
     this._questions = [];
     if (this.project.preset && Array.isArray(this.project.preset.sections)) {
       this.project.preset.sections.forEach((section: Section) => {
@@ -61,6 +80,7 @@ export class ExplorationProjectComponent implements OnInit {
   }
 
   get answers() { return this._answers; }
+  get campaignStats() { return this._campaignsStats; }
   get companies() { return this._companies; }
   get countries() { return this._countries; }
   get contactUrl() { return this._contactUrl; }
