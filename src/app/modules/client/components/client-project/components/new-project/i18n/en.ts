2bde23946ad3fe0f4ef0589bdcab59594057fb8b
abhisheksaini <saini.abhishek@hotmail.com>
2018-05-15 16:04:32 +0200

    Added the new variables.

--- src/app/modules/client/components/client-project/components/new-project/i18n/en.ts
+++ src/app/modules/client/components/client-project/components/new-project/i18n/en.ts
@@ -10,5 +10,22 @@ export const NEW_PROJECT = {
   'DOWNLOAD': 'Download your project anytime',
   'ANONYMOUS': 'Remain anonymous',
   'PRIVATE': 'Nothing will be published at this stage',
-  'BOT': 'Ready to begin a new project?'
+  'BOT': 'Ready to begin a new project?',
+  'PROJECT_NAME': 'Project name is required.',
+  'APPS': {
+    'LINE1': 'Multiply application fields',
+    'LINE2': 'Address new markets and industries',
+    'LINE3': 'Turn R&D from a cost center to a profit center'
+  },
+  'INSIGHTS': {
+    'LINE1': 'Validate your market potential',
+    'LINE2': 'Sharp perfect project-market fit',
+    'LINE3': 'Make the right decision: Go, No-Go, Adjust'
+  },
+  'LEADS': {
+    'LINE1': 'Access full contact details',
+    'LINE2': 'Close deals in your markets',
+    'LINE3': 'Get in touch with potential prospects on demand'
+  }
+
 };
--- src/app/modules/client/components/client-project/components/new-project/i18n/fr.ts
+++ src/app/modules/client/components/client-project/components/new-project/i18n/fr.ts
@@ -7,8 +7,24 @@ export const NEW_PROJECT = {
   'CREATE': 'Créer mon projet',
   'COMPANY_NAME_LABEL': 'Donnez un nom à votre projet',
   'INNOVATION_TYPE_LABEL': 'Choisissez votre offre',
-  'DOWNLOAD': 'Download your project anytime',
-  'ANONYMOUS': 'Remain anonymous',
-  'PRIVATE': 'Nothing will be published at this stage',
-  'BOT': 'Prêt à commencer un nouveau projet ?'
+  'DOWNLOAD': 'Téléchargez votre projet à tout moment',
+  'ANONYMOUS': 'Anonymat garanti',
+  'PRIVATE': 'Rien n\'est publié à ce stade' ,
+  'BOT': 'Prêt à commencer un nouveau projet ?',
+  'PROJECT_NAME': 'Le nom du projet est requis.',
+  'APPS': {
+    'LINE1': 'Multiplier les champs d\'application',
+    'LINE2': 'Aborder de nouveaux marchés et industries',
+    'LINE3': 'Transformez la R & D d\'un centre de coûts en centre de profit'
+  },
+  'INSIGHTS': {
+    'LINE1': 'Validez votre potentiel de marché',
+    'LINE2': 'Ajustement parfait parfait du marché du projet',
+    'LINE3': 'Prenez la bonne décision: allez, allez-y, ajustez'
+  },
+  'LEADS': {
+  'LINE1': 'Accédez aux coordonnées complètes',
+  'LINE2': 'Des offres spéciales sur vos marchés',
+  'LINE3': 'Entrez en contact avec des prospects potentiels sur demande'
+}
 };
