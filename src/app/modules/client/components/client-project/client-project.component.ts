e1a1e678ed5116c1acfa0b379157b4ba1b6fdad4
abhisheksaini <saini.abhishek@hotmail.com>
2018-05-17 13:46:09 +0200

    Updated the UI.

--- src/app/modules/client/components/client-project/client-project.component.ts
+++ src/app/modules/client/components/client-project/client-project.component.ts
@@ -11,9 +11,11 @@ const DEFAULT_PAGE = 'setup';
   templateUrl: './client-project.component.html',
   styleUrls: ['./client-project.component.scss']
 })
+
 export class ClientProjectComponent implements OnInit {
 
   @Input() project: Innovation;
+  private _imgType: string;
 
   private _currentPage: string;
 
@@ -36,29 +38,47 @@ export class ClientProjectComponent implements OnInit {
     this._router.routeReuseStrategy.shouldReuseRoute = function() {
       return false;
     };
+
   }
 
   ngOnInit() {
     const url = this._router.routerState.snapshot.url.split('/');
+
     this._currentPage = url ? url[3] || DEFAULT_PAGE : DEFAULT_PAGE;
+
     if (!this.project) {
       this.project = this._activatedRoute.snapshot.data['innovation'];
     }
+
+    // Getting the project type
+    if (this.project.type === 'leads') {
+      this._imgType = 'https://res.cloudinary.com/umi/image/upload/v1526375000/app/default-images/get-leads.svg';
+    } else if (this.project.type === 'apps') {
+      this._imgType = 'https://res.cloudinary.com/umi/image/upload/v1526375000/app/default-images/get-apps.svg';
+    } else {
+      this._imgType = 'https://res.cloudinary.com/umi/image/upload/v1526375000/app/default-images/get-insights.svg';
+    }
+
   }
 
   public validCollaboratorsList(): boolean {
     let validCount = 0;
+
     const split = this.collaborators_emails.split(/[\s,;:]/g).filter(val => val !== '');
+
     split.forEach(mail => {
       if (mail.match(emailRegEx)) {
         validCount++;
       }
     });
+
     return validCount > 0 && validCount === split.length;
+
   }
 
   public addCollaborators (event: Event): void {
     event.preventDefault();
+
     if (this.collaborators_emails !== '') {
       this.innovationService.inviteCollaborators(this.project._id, this.collaborators_emails).first().subscribe((data: any) => {
         if (data.usersAdded.length || data.invitationsToSend.length || data.invitationsToSendAgain.length) {
@@ -69,13 +89,21 @@ export class ClientProjectComponent implements OnInit {
           if (data.usersAdded.length) {
             this.project.collaborators = this.project.collaborators.concat(data.usersAdded);
           }
+
         }
         this.collaborators_emails = '';
         this.displayAddCollaboratorsModal = false;
       });
     }
+
   }
 
-  get currentPage() { return this._currentPage; }
+  get currentPage() {
+    return this._currentPage;
+  }
+
+  get imgType(): string {
+    return this._imgType;
+  }
 
 }
--- src/app/modules/client/components/client-project/i18n/fr.ts
+++ src/app/modules/client/components/client-project/i18n/fr.ts
@@ -6,12 +6,15 @@ import { SETUP } from '../components/setup/i18n/fr';
 
 export const PROJECT_MODULE = {
   'ADD_COLLABORATORS': 'Ajouter des collaborateurs',
+  'COLLABORATORS': 'collaborateurs',
   'ADD_COLLABORATORS_MODAL': {
     'TITLE': 'Ajout de collaborateurs à ce projet',
     'CONTENT': 'Veuillez écrire les adresses e-mail des collaborateurs dont vous souhaitez que nous autorisions l\'accès à la gestion de votre projet ci-dessous :',
     'USERS_ADDED': 'collaborateur(s) a/ont été ajouté(s)',
-    'TO_SEND_EMAIL': 'adresse(s) e-mail(s) ne correspond(ent) à aucun compte dans notre application. Pour les/l\'ajouter comme collabora-teur-trice-s à ce projet, vous devez leur/lui envoyer une invitation en cliquant sur le bouton ci-dessous :',
-    'TO_RESEND_EMAIL': 'adresse(s) e-mail(s) ne correspond(ent) à aucun compte dans notre application et ont peut-être déjà reçu une invitation de vôtre part. Pour les/l\'ajouter comme collabora-teur-trice-s à ce projet, vous devez leur/lui envoyer une invitation en cliquant sur le bouton'
+    'TO_SEND_EMAIL': 'adresse(s) e-mail(s) ne correspond(ent) à aucun compte dans notre application. Pour les/l\'ajouter comme collabora-teur-trice-s à ce projet, ' +
+    'vous devez leur/lui envoyer une invitation en cliquant sur le bouton ci-dessous :',
+    'TO_RESEND_EMAIL': 'adresse(s) e-mail(s) ne correspond(ent) à aucun compte dans notre application et ont peut-être déjà reçu une invitation de vôtre part. Pour ' +
+    'les/l\'ajouter comme collabora-teur-trice-s à ce projet, vous devez leur/lui envoyer une invitation en cliquant sur le bouton'
   },
   'SETUP_TAB': 'Setup Project',
   'EXPLORATION_TAB': 'Market Exploration',
