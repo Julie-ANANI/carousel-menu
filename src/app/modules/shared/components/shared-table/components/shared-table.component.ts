03a8c5c3b0e8affbf0f7a1b9340bd46e0b612272
valentinmarco <valentin.marco@etu.univ-grenoble-alpes.fr>
2018-06-12 10:28:03 +0200

    Suite de la création du tableau générique : ajout de la pagination et du tri des colonnes

--- src/app/modules/shared/components/shared-table/components/shared-table.component.ts
+++ src/app/modules/shared/components/shared-table/components/shared-table.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {Table} from '../interfaces/table';
 import {Row} from '../interfaces/row';
 
@@ -9,9 +9,15 @@ import {Row} from '../interfaces/row';
 })
 export class SharedTableComponent {
 
+  @Input() set config(value: any) {
+    this.loadConfig(value);
+  }
+
   @Input() set data(value: Table) {
     this.loadData(value);
   }
+
+  @Output() configChange: EventEmitter<any> = new EventEmitter<any>();
 
   private _title = 'Résultats';
   private _content: Row[] = [];
@@ -20,18 +26,39 @@ export class SharedTableComponent {
   private _columnsNames: string[] = [];
   private _selectedRows = 0;
 
+  private _config: any = null;
+
   constructor() {}
 
-  loadData(value: Table): void
-  {
+  loadData(value: Table): void  {
     this._title = value._title;
-    for (const c of value._content)
-    {
-      this._content.push({_isSelected: false, _content: c})
+
+    // Si le tableau contient plus de ligne que la limit, on le vide
+    // (c'est le cas lorsque l'on passe de 50 lignes par pages à 10 lignes par pages par exemple)
+    if (this._content.length > this._config.limit) {
+      this._content = [];
     }
+
+    // Pour chaque valeur qu'on souhaite insérer, on va chercher si elle n'est pas déjà dans le tableau content
+    // Si ce n'est pas le cas on insère une nouvelle Row dans content
+    value._content.forEach(value1 => {
+      if (!this._content.find(value2 => JSON.stringify(value2._content) === JSON.stringify(value1))) {
+        this._content.push({_isSelected: false, _content: value1})
+      }
+    });
+
     this._total = value._total;
     this._columns = value._columns;
     this._columnsNames = value._columnsNames;
+  }
+
+  loadConfig(value: any): void {
+    this._config = value;
+  }
+
+  changeConfig(value: any): void {
+    this._config = value;
+    this.configChange.emit(this._config);
   }
 
   getRowsKeys(): string[]
@@ -64,18 +91,20 @@ export class SharedTableComponent {
     return this._columnsNames;
   }
 
+  get config(): any {
+    return this._config;
+  }
+
   selectRow(key: string): void {
     this._content[key]._isSelected = !(this._content[key]._isSelected);
     this._selectedRows++;
   }
 
-  isSelected(row: Row): boolean
-  {
+  isSelected(row: Row): boolean {
     return row._isSelected;
   }
 
-  selectAll(e: any): void
-  {
+  selectAll(e: any): void  {
       this._content.forEach(value => { value._isSelected = e.srcElement.checked; });
   }
 
