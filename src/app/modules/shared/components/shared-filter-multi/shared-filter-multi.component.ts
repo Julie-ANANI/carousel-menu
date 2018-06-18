deec22418be0d7cafd73fa49106f241b73f4739e
valentinmarco <valentin.marco@etu.univ-grenoble-alpes.fr>
2018-06-18 16:05:12 +0200

    Suite de la création du tableau générique : ajout d'un filtre sur différentes colonnes de type string - Création d'un filtre pour les booléens

--- src/app/modules/shared/components/shared-filter-multi/shared-filter-multi.component.ts
+++ src/app/modules/shared/components/shared-filter-multi/shared-filter-multi.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
+import {Column} from '../shared-table/models/column';
+import {Types} from '../shared-table/models/types';
 
 @Component({
   selector: 'sqFilterMulti',
@@ -9,65 +11,101 @@ export class SharedFilterMultiComponent {
 
   @Input() config: any;
   @Output() configChange = new EventEmitter <any>();
-  @Input() set props(value: string[]) {
+  @Input() set props(value: Column[]) {
     this.loadProps(value);
   }
 
-  private _props: string[]  = [];
-  private _currentProp = '';
+  private _currentTextProp: Column = {_attr: '', _name: '', _type: 'TEXT'};
+  private _textProps: Column[] = [];
+  private _otherProps: Column[] = [];
 
   constructor() {}
 
-  filter(event: any) {
+  filterText(event: any) {
 
-    this._props.forEach(value1 =>
+    this._textProps.forEach(value1 =>
     {
-      if (this.config.search[value1] && value1 !== this._currentProp) {
-        delete this.config.search[value1]
+      if (this.config.search[value1._attr] && value1._attr !== this._currentTextProp._attr) {
+        delete this.config.search[value1._attr]
       }
     });
 
     const value = (<HTMLInputElement> event.srcElement).value;
-    if (this.config.search[this._currentProp] && value === '') {
-      delete this.config.search[this._currentProp];
+    if (this.config.search[this._currentTextProp._attr] && value === '') {
+      delete this.config.search[this._currentTextProp._attr];
       this.configChange.emit(this.config);
     } else if (value !== '') {
-      this.config.search[this._currentProp] = value;
+      this.config.search[this._currentTextProp._attr] = value;
       this.configChange.emit(this.config);
     }
   }
 
-  loadProps(value: string[]) {
-    if (value) {
+  filterOther(event: any, prop: Column) {
+    if (this.config[prop._attr] === null) {
+      delete this.config[prop._attr];
+    }
+    this.configChange.emit(this.config);
+  }
 
-      if (value.indexOf('firstName') !== -1 && value.indexOf('lastName') !== -1) {
-        value.splice(value.indexOf('lastName'), 1);
+  textIsSelected(prop: Column): boolean {
+    return this._currentTextProp._attr === prop._attr;
+  }
+
+  loadProps(value: Column[]) {
+    if (value) {
+      if (value.find(value1 => value1._attr.toLowerCase() === 'firstname') &&
+        value.find(value1 => value1._attr.toLowerCase() === 'lastname')) {
+        value = value.filter(value1 => value1._attr.toLowerCase() !== 'lastname');
       }
-      this._props = value;
-      if (!this._currentProp) {
-        this._currentProp = this._props[0];
+      this._textProps = value.filter(value1 => value1._type === 'TEXT');
+      if (this._currentTextProp._attr === '' && this._textProps.length > 0) {
+        this._currentTextProp = this._textProps[0];
       }
+
+      this._otherProps = value.filter(value1 => value1._type !== 'TEXT');
     }
   }
 
-  get props(): string[] {
-      return this._props;
+  get currentTextProp(): Column {
+    return this._currentTextProp;
   }
 
-  get currentProp(): string {
-    return this._currentProp;
+  get textProps() {
+    return this._textProps;
   }
 
-  changeCurrentProp(prop: any) {
-    this._currentProp = prop.srcElement.value;
+  get otherProps() {
+    return this._otherProps;
   }
 
-  getProp(prop: string): string {
+  getType(column: Column): Types {
+    let typeKey = 'TEXT';
+    for (const typesKey in Types) {
+      if (column._type === typesKey) {
+        typeKey = typesKey;
+      }
+    }
+    return Types[typeKey];
+  }
+
+  getAttr(column: Column) {
+    return column._attr;
+  }
+
+  getName(column: Column) {
+    return column._name;
+  }
+
+  changeCurrentTextProp(prop: any) {
+    this._currentTextProp = this._textProps.find(value => value._attr === prop.srcElement.value);
+  }
+
+  getTextProp(prop: Column): string {
     if (prop) {
-      if (prop === 'firstName' || prop === 'lastName') {
+      if (prop._attr.toLowerCase() === 'firstname' || prop._attr.toLowerCase() === 'lastname') {
         return 'Name';
       }
-      return prop.charAt(0).toUpperCase() + prop.slice(1);
+      return prop._name.charAt(0).toUpperCase() + prop._name.slice(1).toLowerCase();
     }
   }
 }
