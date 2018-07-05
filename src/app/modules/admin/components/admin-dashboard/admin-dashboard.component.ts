cd696ba4086cafd6922f87b21df9850b2b0a96f5
theorabut <trabut@umi.us>
2018-07-05 11:11:49 +0200

    Dashbooooaarrd

--- src/app/modules/admin/components/admin-dashboard/admin-dashboard.component.ts
+++ src/app/modules/admin/components/admin-dashboard/admin-dashboard.component.ts
@@ -6,6 +6,8 @@ import { SearchService } from '../../../../services/search/search.service';
 import { User } from '../../../../models/user.model';
 import { Subject } from 'rxjs/Subject';
 
+
+
 @Component({
   selector: 'app-admin-dashboard',
   templateUrl: './admin-dashboard.component.html',
@@ -16,7 +18,27 @@ export class AdminDashboardComponent implements OnInit {
   public operators: Array<User> = [];
   public operatorId = '';
 
+  private _dateNow = new Date();
+
   public nbDaysOfStats = 1;
+
+
+
+  private _weekBatches: {
+    monday: Array<any>,
+    tuesday: Array<any>,
+    wednesday: Array<any>,
+    thursday: Array<any>,
+    friday: Array<any>
+  } = {
+    monday: [],
+    tuesday: [],
+    wednesday: [],
+    thursday: [],
+    friday: []
+  };
+
+
 
   public operatorData: {
     nbProjectsToValidate: number,
@@ -46,6 +68,7 @@ export class AdminDashboardComponent implements OnInit {
               private _authService: AuthService) { }
 
   ngOnInit(): void {
+
     this._titleService.setTitle('Admin Dashboard');
 
     if (this._authService.user && this._authService.user.isOperator) {
@@ -57,6 +80,9 @@ export class AdminDashboardComponent implements OnInit {
     this._dashboardService.getOperatorData(this.operatorId).first().subscribe((operatorData) => this.operatorData = operatorData);
 
     this.getPeriodStats();
+
+
+    this.getWeek();
   }
 
   public newOperatorSelected(operatorId: string) {
@@ -76,11 +102,46 @@ export class AdminDashboardComponent implements OnInit {
     });
   }
 
+  public getWeek() {
+    const now = Date.now();
+    this._dateNow = new Date(now);
+    console.log(this._dateNow);
+    this._dashboardService.getNextDateSend(this._dateNow.toString()).first().subscribe( (batches: Array<any>) => {
+        console.log(batches);
+        this._weekBatches.monday = batches[0];
+        this._weekBatches.tuesday = batches[1];
+        this._weekBatches.wednesday = batches[2];
+        this._weekBatches.thursday = batches[3];
+        this._weekBatches.friday = batches[4];
+    });
+  }
+
+  public getNextWeek() {
+    this._dateNow.setDate(this._dateNow.getDate() + 7);
+    this._dashboardService.getNextDateSend(this._dateNow.toString()).first().subscribe((batches: Array<any>) => {
+      console.log(batches);
+    });
+  }
+
+  public lastNextWeek() {
+    this._dateNow.setDate(this._dateNow.getDate() - 7);
+    this._dashboardService.getNextDateSend(this._dateNow.toString()).first().subscribe((batches: Array<any>) => {
+      console.log(batches);
+    });
+  }
+
+
   get refreshNeededEmitter(): Subject<any> {
     return this._refreshNeededEmitter;
   }
 
   get adminLevel(): number {
     return this._authService.adminLevel;
+  }
+
+
+
+  get weekBatches() : any {
+    return this._weekBatches;
   }
 }
--- src/app/services/dashboard/dashboard.service.ts
+++ src/app/services/dashboard/dashboard.service.ts
@@ -20,4 +20,11 @@ export class DashboardService {
       .catch((error: Response) => Observable.throw(error.text()));
   }
 
+  public getNextDateSend(date: String): Observable<any> {
+      console.log(date);
+      return this._http.post('/dashboard/nextMails', {date: date})
+        .map((res: Response) => res.json())
+        .catch((error: Response) => Observable.throw(error.text()));
+  }
+
 }
