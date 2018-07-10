31114f7bf8fa01c3dc858ef644931814850b6b20
BastienScanu <bscanu@umi.us>
2018-07-10 17:41:34 +0200

    correction estimation du nombre de requêtes qui seront utilisées pour une recherche

--- src/app/modules/shared/components/shared-search-pros/shared-search-pros.component.ts
+++ src/app/modules/shared/components/shared-search-pros/shared-search-pros.component.ts
@@ -123,7 +123,7 @@ export class SharedSearchProsComponent implements OnInit {
 
   public estimateNumberOfGoogleRequests() {
     const numberOfSearches = this._params.keywords.split("\n").length;
-    let numberOfRequests = 1;
+    let numberOfRequests = 0;
     const selectedCountries = this._params.countries;
     let smartCountries = 0;
     if (this._params.options.smart || this._params.options.regions) {
@@ -139,7 +139,7 @@ export class SharedSearchProsComponent implements OnInit {
       });
     }
     if (!this._params.options.smart) {
-      numberOfRequests += (selectedCountries.length - smartCountries);
+      numberOfRequests += ((selectedCountries.length || 1) - smartCountries);
     }
     this._estimatedNumberOfGoogleRequests = numberOfSearches * numberOfRequests * this._params.count / 10;
   }
