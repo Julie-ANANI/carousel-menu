b502d75b06378110f11e88709f797ea1f7dfdc5e
marcvincenti <mvincenti@umi.us>
2018-07-12 15:30:11 +0200

    Filter Tags on answer level.

--- src/app/modules/shared/components/shared-market-report/components/pro-tag/pro-tag.component.ts
+++ src/app/modules/shared/components/shared-market-report/components/pro-tag/pro-tag.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Answer } from '../../../../../../models/answer';
+import { Filter } from '../../models/filter';
+import { Tag } from '../../../../../../models/tag';
 
 @Component({
   selector: 'app-pro-tag',
@@ -10,13 +12,25 @@ import { Answer } from '../../../../../../models/answer';
 export class ProfessionalTagComponent {
 
   @Input() public answer: Answer;
+
   @Output() modalAnswerChange = new EventEmitter<any>();
+  @Output() addFilter = new EventEmitter<Filter>();
 
   constructor() { }
 
   public seeAnswer(event: Event, answer: Answer) {
     event.preventDefault();
     this.modalAnswerChange.emit(answer);
+  }
+
+  public newFilter(event: Event, tag: Tag) {
+    event.preventDefault();
+    this.addFilter.emit({
+      status: 'TAG',
+      questionTitle: tag.label,
+      value: tag._id
+    });
+
   }
 
 }
