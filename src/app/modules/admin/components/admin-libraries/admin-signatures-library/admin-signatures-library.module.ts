644c201ac5142f337ef1074be4eff4d6908b3ad6
BastienScanu <bscanu@umi.us>
2018-07-17 17:19:31 +0200

    page signatures

--- src/app/modules/admin/components/admin-libraries/admin-signatures-library/admin-signatures-library.module.ts
+++ src/app/modules/admin/components/admin-libraries/admin-signatures-library/admin-signatures-library.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
 import { AdminSignaturesLibraryComponent } from "./admin-signatures-library.component";
-import { RouterModule } from "@angular/router";
 
 
 @NgModule({
   imports: [
     CommonModule,
-    TranslateModule.forChild(),
-    RouterModule
+    TranslateModule.forChild()
   ],
   declarations: [
     AdminSignaturesLibraryComponent
