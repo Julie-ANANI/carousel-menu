14e12ae09d7ac4bceb3cb0f2054ffd8858f59a9f
juandavidcruzgomez <jdcruz-gomez@unitedmotionideas.com>
2018-07-17 17:28:54 +0200

    404 page redirection

--- src/app/modules/base/components/not-found-page/not-found-page.component.ts
+++ src/app/modules/base/components/not-found-page/not-found-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TranslateTitleService } from '../../../../services/title/title.service';
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-not-found-page',
@@ -9,9 +10,17 @@ import { TranslateTitleService } from '../../../../services/title/title.service'
 
 export class NotFoundPageComponent implements OnInit {
 
-  constructor(private translateTitleService: TranslateTitleService) {}
+  constructor(private translateTitleService: TranslateTitleService,
+              private _router: Router) {}
 
   ngOnInit(): void {
     this.translateTitleService.setTitle('404.PAGE_NOT_FOUND');
+
+    /**
+     * Go somewhere else
+     */
+    setTimeout(()=>{
+      this._router.navigate(['/'] );
+    }, 2000);
   }
 }
