93a3ce5e6edca55905d78179860619fb25bb648a
BastienScanu <bscanu@umi.us>
2018-07-23 15:15:41 +0200

    on utilise le nouveau module d'édition de workflows dans les campagnes

--- src/app/modules/admin/components/admin-campaigns/admin-campaigns.module.ts
+++ src/app/modules/admin/components/admin-campaigns/admin-campaigns.module.ts
@@ -9,7 +9,6 @@ import { SharedAnswerListModule } from '../../../shared/components/shared-answer
 import { SharedSearchHistoryModule} from '../../../shared/components/shared-search-history/search-history.module';
 import { SharedSearchProsModule } from '../../../shared/components/shared-search-pros/shared-search-pros.module';
 import { SharedSearchResultsModule } from '../../../shared/components/shared-search-results/search-results.module';
-import { SharedEditScenarioModule } from '../../../shared/components/shared-edit-scenario/shared-edit-scenario.module';
 import { PipeModule } from '../../../../pipe/pipe.module';
 import { SidebarModule } from '../../../sidebar/sidebar.module';
 import { AdminCampaignsComponent } from './admin-campaigns.component';
@@ -24,6 +23,7 @@ import { AdminCampaignSearchResultsComponent } from './admin-campaign-search-res
 import { AdminCampaignTemplatesComponent } from './admin-campaign-templates/admin-campaign-templates.component';
 import { SharedTableModule } from '../../../table/table.module';
 import { AdminCampaignAbtestingComponent } from './admin-campaign-abtesting/admin-campaign-abtesting.component';
+import { AdminEditWorkflowModule } from "../admin-edit-workflow/admin-edit-workflow.module";
 
 @NgModule({
   imports: [
@@ -35,12 +35,12 @@ import { AdminCampaignAbtestingComponent } from './admin-campaign-abtesting/admi
     SharedSearchResultsModule,
     SharedAnswerListModule,
     SharedSearchHistoryModule,
-    SharedEditScenarioModule,
     RouterModule,
     TranslateModule.forChild(),
     SidebarModule,
     SharedTableModule,
-    PipeModule
+    PipeModule,
+    AdminEditWorkflowModule
   ],
   declarations: [
     AdminCampaignsComponent,
