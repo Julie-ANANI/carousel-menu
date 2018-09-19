5ba1bb468ece8939c10e63d8b0bc4a970a4f8d7c
abhisheksaini <saini.abhishek@hotmail.com>
2018-09-19 15:22:08 +0200

    finished new header.

--- src/app/modules/base/components/header/header.component.ts
+++ src/app/modules/base/components/header/header.component.ts
@@ -4,20 +4,50 @@ import { Location } from '@angular/common';
 import { AuthService } from '../../../../services/auth/auth.service';
 import { UserService } from '../../../../services/user/user.service';
 import { User } from '../../../../models/user.model';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  animations: [
+    trigger('animateWrapper', [
+      state('false', style({
+        display: 'none',
+        opacity: 0,
+        background: 'none'
+      })),
+      state('true', style({
+        display: 'block',
+        opacity: 1,
+        background: 'rgba(0,0,0,0.65)'
+      })),
+      transition('false => true', animate('.5ms ease-in-out')),
+      transition('true => false', animate('700.5ms ease-in-out'))
+    ]),
+    trigger('animateContent', [
+      state('false', style({
+        transform: 'translateX(100%)'
+      })),
+      state('true', style({
+        transform: 'translateX(0)'
+      })),
+      transition('false <=> true', animate('700ms ease-in-out'))
+    ])
+  ]
 })
 
 export class HeaderComponent implements OnInit, OnDestroy {
 
-  private _backOfficeValue: boolean; // if true then display back office menu options.
-  private _displayMenuOptions: boolean; // on small devices if true then display menu options.
+  private _backOfficeValue = false; // if true then display back office menu options.
+
   private _displayNavbar: boolean; // if true that means user is authenticated so display navbar.
-  user: User;
-  profilePic: string;
+
+  private _user: User; // we are storing the user values to display the its data.
+
+  private _displayMobileMenu = false; // display menu items when on mobile.
+
+  private _profilePic: string;
 
   constructor(private _authService: AuthService,
               private _location: Location,
@@ -26,15 +56,13 @@ export class HeaderComponent implements OnInit, OnDestroy {
   ngOnInit() {
     this.userInfo();
     this._displayNavbar = this._authService.isAuthenticated;
-    this._displayMenuOptions = false;
-    this._backOfficeValue = false;
     this._backOfficeValue = this._location.path().slice(0, 6) === '/admin';
   }
 
   private userInfo() {
     this.userService.getSelf().first().subscribe((res: User) => {
-      this.user = res;
-      this.profilePic = res.profilePic ? res.profilePic.url || '' : '';
+      this._user = res;
+      this._profilePic = res.profilePic ? res.profilePic.url || '' : '';
     });
   }
 
@@ -52,18 +80,19 @@ export class HeaderComponent implements OnInit, OnDestroy {
     this._backOfficeValue = !this._backOfficeValue;
   }
 
-  // to toggle the value of menu options display.
-  toggleMenuState() {
-    this._displayMenuOptions = !this._displayMenuOptions;
+  toggleMobileMenu(event: Event) {
+    if (event.target['id'] === 'm-wrapper' || event.target['id'] === 'hamburger-icon') {
+      this._displayMobileMenu = !this._displayMobileMenu;
+    }
   }
 
   // set the menu display value false when click on link
   onClickLink() {
-    this._displayMenuOptions = false;
+    this._displayMobileMenu = false;
   }
 
   hasProfilePic(): boolean {
-    return !!this.profilePic && this.profilePic !== '';
+    return !!this._profilePic && this._profilePic !== '';
   }
 
   get backOfficeValue(): boolean {
@@ -74,12 +103,20 @@ export class HeaderComponent implements OnInit, OnDestroy {
     return this._authService;
   }
 
-  get displayMenuOptions(): boolean {
-    return this._displayMenuOptions;
-  }
-
   get displayNavbar(): boolean {
     return this._displayNavbar;
+  }
+
+  get user(): User {
+    return this._user;
+  }
+
+  get displayMobileMenu(): boolean {
+    return this._displayMobileMenu;
+  }
+
+  get profilePic(): string {
+    return this._profilePic;
   }
 
   ngOnDestroy(): void {
