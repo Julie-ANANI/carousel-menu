fca5c1823e9e9b680aca09f2e630eb9bd09b4d0c
juandavidcruzgomez <jdcruz-gomez@unitedmotionideas.com>
2018-10-08 12:08:21 +0200

    Logos and other things

--- src/environments/vendor1/environment.vendor1.dev.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: 'https://dev.umi.us/api',
-  companyName: 'Some UMI vendor',
-  companyShortName: 'Vendor dev',
-  domain: 'vendor1',
-  quizURl: 'http://localhost:4200',
-  background: 'https://res.cloudinary.com/umi/image/upload/v1524662280/app/default-images/sign-background.jpg'
-};
