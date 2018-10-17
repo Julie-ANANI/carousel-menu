925315b884dfa76494c7e44106f957af0d93f8fc
juandavidcruzgomez <jdcruz-gomez@unitedmotionideas.com>
2018-10-17 09:46:25 +0200

    Salveo env

--- src/environments/salveo/environment.salveo.prod.ts
+++ src/environments/salveo/environment.salveo.prod.ts
@@ -1,16 +1,16 @@
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:3000/api',
-  clientUrl: 'http://localhost:4200',
+  production: true,
+  apiUrl: 'https://appv3.umi.us/api',
+  clientUrl: 'https://multivalente.umi.us',
   companyName: 'Multivalente',
   companyShortName: 'Multivalente',
   domain: 'multivalente',
-  innovationUrl: 'http://localhost:4200',
-  quizUrl: 'http://localhost:4200',
+  innovationUrl: 'https://multivalente.umi.us',
+  quizUrl: 'https://quiz.umi.us',
   companyURL: 'www.multivalente.com',
   logoURL: 'https://res.cloudinary.com/umi/image/upload/v1528460832/whitelabel/multivalente/logo-multivalente-3.png',
   logoSynthURL: 'https://res.cloudinary.com/umi/image/upload/v1529571635/whitelabel/multivalente/logo-multivalente-bg.png',
   background: 'https://res.cloudinary.com/umi/image/upload/v1524662280/app/default-images/sign-background.jpg',
-  secureCookie: false,
+  secureCookie: true,
   cookieTime: 48 * 3600 * 1000
 };
