2390ab458e7f83abaccfad8d9c74122d45071de3
abhisheksaini <saini.abhishek@hotmail.com>
2018-11-29 16:27:53 +0100

    project setupe in progress... added current page ...

--- src/app/modules/user/client/components/project/components/setup/setup.component.ts
+++ src/app/modules/user/client/components/project/components/setup/setup.component.ts
@@ -3,6 +3,7 @@ import {Innovation} from '../../../../../../../models/innovation';
 import {ScrollService} from '../../../../../../../services/scroll/scroll.service';
 import {takeUntil} from 'rxjs/operators';
 import {Subject} from 'rxjs';
+import {Router} from '@angular/router';
 
 
 // const DEFAULT_TAB = 'targeting';
@@ -27,16 +28,31 @@ export class SetupComponent implements OnInit, OnDestroy {
 
   ngUnsubscribe: Subject<any> = new Subject();
 
-  constructor(private scrollService: ScrollService) { }
+  currentPage: string;
+
+  constructor(private scrollService: ScrollService,
+              private router: Router) { }
 
   ngOnInit() {
     this.scrollService.getScrollValue().pipe(takeUntil(this.ngUnsubscribe)).subscribe((value) => {
       this.scrollOn = value > 50;
       console.log(this.scrollOn);
     });
+
+    const url = this.router.routerState.snapshot.url.split('/');
+    this.currentPage = url.length > 0 ? url[5] : 'targeting';
+
+
     console.log(this.innovation);
 
   }
+
+
+  setCurrentTab(event: Event, value: string) {
+    event.preventDefault();
+    this.currentPage = value;
+  }
+
 
   ngOnDestroy(): void {
     this.ngUnsubscribe.next();
