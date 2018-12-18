917e3dd0de6b487d6bfe69238f1986a154ca1a0f
abhisheksaini <saini.abhishek@hotmail.com>
2018-12-18 09:56:33 +0100

    Added progress bar in app component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component, HostListener, Inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { Component, HostListener, Inject, OnDestroy, OnInit, PLATFORM_ID } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
 import { AuthService } from './services/auth/auth.service';
 import { TranslateService, initTranslation } from './i18n/i18n';
 import { TranslateNotificationsService } from './services/notifications/notifications.service';
 import { NavigationEnd, Router } from '@angular/router';
 import { ScrollService } from './services/scroll/scroll.service';
+import { LoaderService } from './services/loader/loader.service';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -12,7 +15,11 @@ import { ScrollService } from './services/scroll/scroll.service';
   templateUrl: './app.component.html'
 })
 
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
+
+  private _displayLoader = false;
+
+  private _ngUnsubscribe: Subject<any> = new Subject();
 
   notificationsOptions = {
     position: ['bottom', 'right'],
@@ -30,7 +37,8 @@ export class AppComponent implements OnInit {
               private authService: AuthService,
               private translateNotificationsService: TranslateNotificationsService,
               private router: Router,
-              private scrollService: ScrollService) {}
+              private scrollService: ScrollService,
+              private loaderService: LoaderService) {}
 
   ngOnInit(): void {
 
@@ -53,6 +61,16 @@ export class AppComponent implements OnInit {
       );
     }
 
+    this.loaderService.isLoading$.pipe(takeUntil(this._ngUnsubscribe)).subscribe((isLoading: boolean) => {
+      // Bug corrigé avec setTimeout :
+      // https://stackoverflow.com/questions/38930183/angular2-expression-has-changed-after-it-was-checked-binding-to-div-width-wi
+      setTimeout((_: void) => {
+        this._displayLoader = isLoading;
+      });
+    });
+
+    this.loaderService.stopLoading();
+
   }
 
 
@@ -62,16 +80,21 @@ export class AppComponent implements OnInit {
   }
 
 
+  get displayLoader(): boolean {
+    return this._displayLoader;
+  }
+
+  get ngUnsubscribe(): Subject<any> {
+    return this._ngUnsubscribe;
+  }
+
+  ngOnDestroy(): void {
+    this._ngUnsubscribe.next();
+    this._ngUnsubscribe.complete();
+  }
+
 }
 
-
-/*this.loaderService.isLoading$.pipe(takeUntil(this.ngUnsubscribe)).subscribe((isLoading: boolean) => {
-       // Bug corrigé avec setTimeout :
-       // https://stackoverflow.com/questions/38930183/angular2-expression-has-changed-after-it-was-checked-binding-to-div-width-wi
-       setTimeout((_: void) => {
-         this._displayLoader = isLoading;
-       });
-     });*/
 
 /***
  * This is to listen the click event on the page.
