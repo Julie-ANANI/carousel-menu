a7d32be09c0cc5c9b7709e9fcc3d70f10102b8f5
juandavidcruzgomez <jdcruz-gomez@unitedmotionideas.com>
2019-01-25 16:14:33 +0100

    callback

--- src/app/modules/authentication/authentication.component.ts
+++ src/app/modules/authentication/authentication.component.ts
@@ -13,13 +13,18 @@ export class AuthenticationComponent {
   constructor(private _router: Router,
               private _activatedRoute: ActivatedRoute,
               private _authService: AuthService) {
+    console.trace("In the constructor");
     this._activatedRoute.queryParams.subscribe(result=>{
+      console.log(`Activated route: ${this._activatedRoute}`);
       if(result['error']) {
         //Something wrong happened
         console.error(result['error_description'])
       } else if(result['code']) {
+        console.trace("Trying to recover the linkedin code");
         this._authService.linkedInFetchToken(result['code'], environment.domain)
           .subscribe(result=>{
+            console.trace("Good, we have everything and we may be authenticated");
+            console.log(JSON.stringify(result));
             this._router.navigate(['/project']);
           }, err=>{
             console.error(err);
@@ -28,6 +33,7 @@ export class AuthenticationComponent {
         //This is the first call
       } else if(result['access_token']) {
         //This is the second one, authenticate in the API
+        console.trace("What??");
         this._router.navigate(['/login']);
       } else {
         //There's some error...
