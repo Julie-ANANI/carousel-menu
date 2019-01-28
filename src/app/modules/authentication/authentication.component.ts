84d749fb9aff296736f15e9f5199604594ed674c
juandavidcruzgomez <jdcruz-gomez@unitedmotionideas.com>
2019-01-28 08:22:26 +0100

    routing

--- src/app/modules/authentication/authentication.component.ts
+++ src/app/modules/authentication/authentication.component.ts
@@ -18,35 +18,40 @@ export class AuthenticationComponent implements OnInit {
 
   ngOnInit(): void {
     console.log("I'm in the on init");
+    let whereToGo = null;
     this._activatedRoute.queryParams.subscribe(result=>{
       console.log(`Activated route: ${this._activatedRoute}`);
-      if(result['error']) {
-        //Something wrong happened
-        console.error(result['error_description'])
-      } else if(result['code']) {
+      if(result['code']) {
         console.trace("Trying to recover the linkedin code");
         this._authService.linkedInFetchToken(result['code'], environment.domain)
           .subscribe(result=>{
             console.trace("Good, we have everything and we may be authenticated");
             console.log(JSON.stringify(result));
-            this._router.navigate(['/project']);
+            this._router.navigate(['/project']).then(_=>{console.log("OK")}, err=>{console.error(err)});
           }, err=>{
             console.error(err);
-            this._router.navigate(['/login']);
+            this._router.navigate(['/login']).then(_=>{console.log("OK")}, err=>{console.error(err)});;
           });
         //This is the first call
+      } else if(result['error']) {
+        //Something wrong happened
+        console.error(result['error_description']);
+        whereToGo = this._router.navigate(['/login']);
       } else if(result['access_token']) {
         //This is the second one, authenticate in the API
         console.trace("What??");
-        this._router.navigate(['/login']);
+        whereToGo =  this._router.navigate(['/login']);
       } else {
         //There's some error...
         console.error(result['error_description'])
-        this._router.navigate(['/login']);
+        whereToGo = this._router.navigate(['/login']);
+      }
+      if(whereToGo) {
+        whereToGo.then(_=>{console.log("OK")}, err=>{console.error(err)});
       }
     }, err=>{
       console.error(err);
-      this._router.navigate(['/login']);
+      this._router.navigate(['/login']).then(_=>{console.log("OK")}, err=>{console.error(err)});
     });
   }
 }
