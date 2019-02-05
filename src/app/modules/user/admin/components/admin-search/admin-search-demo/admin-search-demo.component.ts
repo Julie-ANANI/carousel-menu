38f60bb274cde50a9f40f2c79f1a81a453f2aff2
BastienScanu <bscanu@umi.us>
2019-02-05 18:11:17 +0100

    demo tab for the search module

---
+++ src/app/modules/user/admin/components/admin-search/admin-search-demo/admin-search-demo.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-search-demo',
+  templateUrl: './admin-search-demo.component.html',
+  styleUrls: ['./admin-search-demo.component.scss']
+})
+export class AdminSearchDemoComponent {}
