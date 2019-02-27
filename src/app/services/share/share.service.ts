fa97a159eb84cab8830352b2b8d3b3755c168d51
abhisheksaini <saini.abhishek@hotmail.com>
2019-02-27 16:35:38 +0100

    Updated share services.

--- src/app/services/share/share.service.ts
+++ src/app/services/share/share.service.ts
@@ -1,9 +1,7 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
-import { Innovation } from '../../models/innovation';
 import { InnovCard } from '../../models/innov-card';
 import { Observable } from 'rxjs/Observable';
-import * as _ from 'lodash';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable()
@@ -11,62 +9,64 @@ export class ShareService {
 
   constructor(private _http: HttpClient) {}
 
-  private _getShareUrl (project: Innovation): string {
-    return encodeURIComponent(`${environment.clientUrl}/projects/${project._id}`);
+
+  private _getShareUrl (innovationCard: InnovCard): string {
+    return encodeURIComponent(`${environment.clientUrl}/discover/${innovationCard.innovation_reference}/${innovationCard.lang}`);
   }
 
-  private _getTitle (project: Innovation, lang: string): string {
-    const innovationCard = _.find(project.innovationCards, (card: InnovCard) => card['lang'] === lang);
-    if (innovationCard) {
-      return encodeURIComponent(innovationCard['title'] || '');
-    }
-    return '';
+
+  private _getTitle (innovationCard: InnovCard): string {
+    return innovationCard.title || '';
   }
 
-  private _getSummary (project: Innovation, lang: string): string {
-    const innovationCard = _.find(project.innovationCards, (card: InnovCard) => card['lang'] === lang);
-    if (innovationCard) {
-      return encodeURIComponent(innovationCard['summary'] || '');
-    }
-    return '';
+
+  private _getSummary (innovationCard: InnovCard): string {
+    return innovationCard.summary || '';
   }
 
-  public shareSynthesis(projectId: string): Observable<any> {
+
+  shareSynthesis(projectId: string): Observable<any> {
     return this._http.post('/sharing', {id: projectId, type: 'synthesis'});
   }
 
-  public linkedinProjectShareLink (project: Innovation, lang: string): string {
-    lang = lang || 'en';
+
+  linkedinProjectShareLink (innovationCard: InnovCard): string {
     return 'https://www.linkedin.com/shareArticle' +
       '?mini=true' +
-      '&url=' + this._getShareUrl(project) +
-      '&title=' + this._getTitle(project, lang) +
-      '&summary=' + this._getSummary(project, lang) +
+      '&url=' + this._getShareUrl(innovationCard) +
+      '&title=' + this._getTitle(innovationCard) +
+      '&summary=' + this._getSummary(innovationCard) +
       '&source=' + 'UMI';
   }
 
-  public twitterProjectShareLink (project: Innovation, lang: string): string {
-    lang = lang || 'en';
+
+  twitterProjectShareLink (innovationCard: InnovCard): string {
     const twitterAccount = 'uMotionIdeas';
     return 'http://twitter.com/home' +
-      '?status=' + this._getTitle(project, lang) + '%20' + this._getShareUrl(project) + '%20%40' + twitterAccount;
+      '?status=' + this._getTitle(innovationCard) + '%20' + this._getShareUrl(innovationCard) + '%20%40' + twitterAccount;
   }
 
-  public facebookProjectShareLink (project: Innovation): string {
+
+  facebookProjectShareLink (innovationCard: InnovCard): string {
     return 'https://www.facebook.com/dialog/feed' +
       '?app_id=' + '1172496952780763' +
       '&version=' + 'v2.12' +
       'display=popup' +
-      '&link=' + this._getShareUrl(project);
+      '&link=' + this._getShareUrl(innovationCard);
   }
 
-  public googleProjectShareLink (project: Innovation, lang: string): string {
+  /*public googleProjectShareLink (project: Innovation, lang: string): string {
     lang = lang || 'en';
-    return 'https://plus.google.com/share?url=' + this._getShareUrl(project) + '&text=' + this._getSummary(project, lang);
+    return 'https://plus.google.com/share?url=' + this._getShareUrl(project, lang) + '&text=' + this._getSummary(project, lang);
+  }*/
+
+
+  mailProjectShareLink (innovationCard: InnovCard): string {
+
+    const message = encodeURI(`Please add your message here.\r\n\r\n-------------------------------------\r\nInnovation Details: \r\n\r\nURL - ${environment.clientUrl}/discover/${innovationCard.innovation_reference}/${innovationCard.lang}\r\n\r\nTitle - ${this._getTitle(innovationCard)}\r\n\r\nSummary - ${this._getSummary(innovationCard)}`);
+
+    return `mailto:?subject=Interesting Innovation - ${this._getTitle(innovationCard)}&body=${message}`;
+
   }
 
-  public mailProjectShareLink (project: Innovation, lang: string): string {
-    lang = lang || 'en';
-    return `mailto:?body=${this._getSummary(project, lang)}%0A${this._getShareUrl(project)}&subject=${this._getTitle(project, lang)}`;
-  }
 }
