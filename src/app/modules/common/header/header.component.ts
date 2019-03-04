a85a178bf33fc50766eb4c16c77ba4a572cea0f4
juandavidcruzgomez <jdcruz-gomez@unitedmotionideas.com>
2019-03-04 17:46:22 +0100

    mergemerge

--- src/app/modules/common/header/header.component.ts
+++ src/app/modules/common/header/header.component.ts
@@ -1,126 +1,72 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { environment } from "../../../../environments/environment";
-import { Location } from '@angular/common';
+import { environment} from '../../../../environments/environment';
 import { AuthService } from '../../../services/auth/auth.service';
-import { UserService } from '../../../services/user/user.service';
-import { User } from '../../../models/user.model';
-import { animate, state, style, transition, trigger } from '@angular/animations';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss'],
-  animations: [
-    trigger('animateWrapper', [
-      state('false', style({
-        display: 'none',
-        opacity: 0,
-        background: 'none'
-      })),
-      state('true', style({
-        display: 'block',
-        opacity: 1,
-        background: 'rgba(0,0,0,0.65)'
-      })),
-      transition('false => true', animate('.5ms ease-in-out')),
-      transition('true => false', animate('700.5ms ease-in-out'))
-    ]),
-    trigger('animateContent', [
-      state('false', style({
-        transform: 'translateX(100%)'
-      })),
-      state('true', style({
-        transform: 'translateX(0)'
-      })),
-      transition('false <=> true', animate('700ms ease-in-out'))
-    ])
-  ]
+  styleUrls: ['./header.component.scss']
 })
 
 export class HeaderComponent implements OnInit, OnDestroy {
 
-  private _backOfficeValue = false; // if true then display back office menu options.
+  private _backOfficeValue: boolean; // if true, then display back office menu options.
 
-  private _displayNavbar: boolean; // if true that means user is authenticated so display navbar.
-
-  private _user: User; // we are storing the user values to display the its data.
-
-  private _displayMobileMenu = false; // display menu items when on mobile.
-
-  private _profilePic: string;
+  private _displayMenuOptions: boolean; // on small devices if true then display menu options.
 
   constructor(private _authService: AuthService,
-              private _location: Location,
-              private userService: UserService) {}
+              private location: Location) { }
 
   ngOnInit() {
-    this.userInfo();
-    this._displayNavbar = this._authService.isAuthenticated;
-    this._backOfficeValue = this._location.path().slice(0, 6) === '/admin';
+    this._displayMenuOptions = false;
+    this._backOfficeValue = false;
+    this._backOfficeValue = this.location.path().slice(5, 11) === '/admin';
   }
 
-  private userInfo() {
-    this.userService.getSelf().first().subscribe((res: User) => {
-      this._user = res;
-      this._profilePic = res.profilePic ? res.profilePic.url || '' : '';
-    });
-  }
 
-  getLogo(): string {
-    // return `logo-${ environment.domain || 'umi.us'}.png`;
-    return environment.logoURL;
-  }
-
-  canShow(reqLevel: number): boolean {
-    return reqLevel && ( this._authService.adminLevel & reqLevel) === reqLevel;
-  }
-
-  // to toggle the back office value.
+  /***
+   * to toggle the back office value.
+   */
   toggleBackOfficeState() {
     this._backOfficeValue = !this._backOfficeValue;
   }
 
-  toggleMobileMenu(event: Event) {
-    if (event.target['id'] === 'm-wrapper' || event.target['id'] === 'hamburger-icon') {
-      this._displayMobileMenu = !this._displayMobileMenu;
-    }
+  /***
+   * to toggle the value of menu options display.
+   */
+  toggleMenuState() {
+    this._displayMenuOptions = !this._displayMenuOptions;
   }
 
-  // set the menu display value false when click on link
+  /***
+   * set the menu display value false when click on link
+   */
   onClickLink() {
-    this._displayMobileMenu = false;
+    this._displayMenuOptions = false;
   }
 
-  hasProfilePic(): boolean {
-    return !!this._profilePic && this._profilePic !== '';
+  canShow(reqLevel: number): boolean {
+    return reqLevel && ( this.authService.adminLevel & reqLevel) === reqLevel;
   }
 
   get backOfficeValue(): boolean {
     return this._backOfficeValue;
   }
 
+  getLogo(): string {
+    return environment.logoURL;
+  }
+
   get authService(): AuthService {
     return this._authService;
   }
 
-  get displayNavbar(): boolean {
-    return this._displayNavbar;
-  }
-
-  get user(): User {
-    return this._user;
-  }
-
-  get displayMobileMenu(): boolean {
-    return this._displayMobileMenu;
-  }
-
-  get profilePic(): string {
-    return this._profilePic;
+  get displayMenuOptions(): boolean {
+    return this._displayMenuOptions;
   }
 
   ngOnDestroy(): void {
-    this._displayNavbar = false;
     this._backOfficeValue = false;
   }
 
