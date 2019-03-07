baab74370823327926750745dae60d1cc22c0439
BastienScanu <bscanu@umi.us>
2019-03-07 11:43:06 +0100

    tooltip on hover

--- src/app/modules/user/admin/components/admin-search/admin-search-map/admin-search-map.component.ts
+++ src/app/modules/user/admin/components/admin-search/admin-search-map/admin-search-map.component.ts
@@ -12,6 +12,11 @@ export class AdminSearchMapComponent implements OnInit{
   @Input() countriesData: Array<any> = [];
   @Output() onCountryClick = new EventEmitter<any>();
 
+  private _hoveredCountry: string = null;
+  private _boxStyle: any = {
+    opacity: 0
+  };
+
   constructor() {}
 
   ngOnInit() {}
@@ -25,12 +30,26 @@ export class AdminSearchMapComponent implements OnInit{
   public hoverCountry(event: Event) {
     event.preventDefault();
     const country = event.srcElement.className.baseVal.slice(-2);
-    console.log(country);
+    this._hoveredCountry = country;
+    this._boxStyle = {
+      top: `${event.pageY - 220}px`,
+      left: `${event.pageX - 200}px`
+    };
   }
 
   public leaveHover(event: Event) {
     event.preventDefault();
-    const country = event.srcElement.className.baseVal.slice(-2);
-    console.log(country);
+    this._hoveredCountry = null;
+    this._boxStyle = {
+      opacity: 0
+    };
+  }
+
+  get hoveredCountry(): string {
+    return this._hoveredCountry;
+  }
+
+  get boxStyle(): any {
+    return this._boxStyle;
   }
 }
