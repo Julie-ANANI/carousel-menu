638d95dec8906e33b9a9dac7ef0abfac02526c46
marcvincenti <mvincenti@umi.us>
2019-03-22 15:21:17 +0100

    propagate new tag filter

--- src/app/modules/shared/components/shared-market-report/components/pro-tag/pro-tag.component.ts
+++ src/app/modules/shared/components/shared-market-report/components/pro-tag/pro-tag.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 import { FilterService } from '../../services/filters.service';
+import { TagsService } from '../../services/tags.service';
 import { Answer } from '../../../../../../models/answer';
 import { Tag } from '../../../../../../models/tag';
 
@@ -12,17 +13,18 @@ import { Tag } from '../../../../../../models/tag';
 export class ProfessionalTagComponent implements OnInit {
 
   @Input() answer: Answer;
-  @Input() tagId: string;
+  @Input() questionId: string;
 
   @Output() modalAnswerChange = new EventEmitter<any>();
 
   private _tags: Array<Tag>;
 
-  constructor(private filterService: FilterService) {}
+  constructor(private filterService: FilterService,
+              private tagService: TagsService) {}
 
   ngOnInit() {
-    if (this.tagId) {
-      this._tags = this.answer.answerTags[this.tagId];
+    if (this.questionId) {
+      this._tags = this.answer.answerTags[this.questionId];
     } else {
       this._tags = this.answer.tags;
     }
@@ -35,12 +37,15 @@ export class ProfessionalTagComponent implements OnInit {
 
   public newFilter(event: Event, tag: Tag) {
     event.preventDefault();
-
-    this.filterService.addFilter({
-      status: 'TAG',
-      questionId: this.tagId,
-      value: tag._id
-    });
+    if (this.questionId) {
+      this.filterService.addFilter({
+        status: 'TAG',
+        questionId: this.questionId,
+        value: tag._id
+      });
+    } else {
+      this.tagService.checkTag(tag._id, false);
+    }
   }
 
   get tags() {
