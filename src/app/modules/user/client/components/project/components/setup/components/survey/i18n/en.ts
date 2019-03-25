99b29f273c53fca76b980c13dd0059de40cc9cb1
abhisheksaini <saini.abhishek@hotmail.com>
2019-03-25 15:30:08 +0100

    updated and checked survey component.

--- src/app/modules/user/client/components/project/components/setup/components/survey/i18n/en.ts
+++ src/app/modules/user/client/components/project/components/setup/components/survey/i18n/en.ts
@@ -2,6 +2,6 @@ export const SURVEY = {
 
   'NO_QUIZ': 'Our team will launch the study once your project has been validated.',
 
-  'DONT_SHARE_QUIZ': 'Visit this link to see the questionnaire that will be sent to market professionals. Do not share this link which is available as an example only.'
+  'DONT_SHARE_QUIZ': 'Visit this link to see the questionnaire that will be sent to market professionals. Do not share this link this is available as an example only.'
 
 };
