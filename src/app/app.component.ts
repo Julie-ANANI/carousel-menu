43b6c3bbafc010e77b169ac7a01ef6c283f661e1
juandavidcruzgomez <jdcruz-gomez@unitedmotionideas.com>
2019-03-26 15:15:24 +0100

    https swell

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -109,7 +109,11 @@ export class AppComponent implements OnInit {
       //linkElement.setAttribute('src', '/swellrt-beta.js');
       linkElement.setAttribute('src', `http://localhost:9898/${SWELL_CONTEXT}/${SWELL_JS_MODULE}`);
     } else {
-      linkElement.setAttribute('src', `https://swellrt.umi.us:9899/${SWELL_CONTEXT}/${SWELL_JS_MODULE}`);
+      if(environment.apiUrl === 'https://dev.umi.us') {
+        linkElement.setAttribute('src', `https://swellrtdev.umi.us:9899/${SWELL_CONTEXT}/${SWELL_JS_MODULE}`);
+      } else {
+        linkElement.setAttribute('src', `https://swellrt.umi.us:9899/${SWELL_CONTEXT}/${SWELL_JS_MODULE}`);
+      }
     }
     document.head.appendChild( linkElement );
   }
