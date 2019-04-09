4a1fc01808abe75fe9d3c6693039d71281e5f91c
abhisheksaini <saini.abhishek@hotmail.com>
2019-04-09 18:09:16 +0200

    showcase-answer is in progress...

--- src/app/modules/demo/components/showcase/components/showcase-answers/showcase-answers.component.ts
+++ src/app/modules/demo/components/showcase/components/showcase-answers/showcase-answers.component.ts
@@ -1,36 +1,70 @@
 import { Component, Input } from '@angular/core';
-import {TagStats} from '../../../../../../models/tag-stats';
-import {Answer} from '../../../../../../models/answer';
-import {AnswerService} from '../../../../../../services/answer/answer.service';
+import { TagStats } from '../../../../../../models/tag-stats';
+import { Answer } from '../../../../../../models/answer';
+import { AnswerService } from '../../../../../../services/answer/answer.service';
+import { AuthService } from '../../../../../../services/auth/auth.service';
 
 @Component({
   selector: 'app-showcase-answers',
   templateUrl: './showcase-answers.component.html',
+  styleUrls: ['./showcase-answers.component.scss']
 })
 
 export class ShowcaseAnswersComponent {
 
-  @Input() totalAnswers: number;
+  @Input() set totalAnswers(value: number) {
+    this.count = value;
+  }
 
   @Input() set tagsStats(value: Array<TagStats>) {
     const tags_id = value.map((st) => st.tag._id);
+
     if (tags_id.length > 0) {
-      this.answerService.getStarsAnswer(tags_id).subscribe((next) => {
-        if (Array.isArray(next.result)) {
-          this._answers = next.result;
-          this._topAnswers = next.result.slice(0, 6);
+      this._answerService.getStarsAnswer(tags_id).subscribe((response) => {
+        if (Array.isArray(response.result)) {
+          this._answers = response.result;
+          this._topAnswers = response.result.slice(0, 6);
+
+          this._topAnswers = this._topAnswers.map((answer) => {
+            answer.isLoading = true;
+            return answer;
+          });
+
+          this._topAnswers.forEach((answer, index) => {
+            this._stopLoading(answer, index);
+          });
+
         }
       });
     } else {
       this._topAnswers = [];
     }
+
   }
-  public openAnswersModal = false;
 
   private _answers: Array<Answer> = [];
-  private _topAnswers: Array<Answer> = [];
 
-  constructor(private answerService: AnswerService) {}
+  private _topAnswers: Array<any> = [];
+
+  count: number;
+
+  adminPass: boolean = false;
+
+  constructor(private _answerService: AnswerService,
+              private _authService: AuthService) {
+
+    this.adminPass = this._authService.adminLevel > 2
+
+  }
+
+
+  private _stopLoading(answer: any, index: number) {
+    setTimeout(() => {
+      answer.isLoading = false;
+      this._topAnswers[index] = answer;
+    }, Math.floor(Math.random() * 2000) + 1000);
+  }
+
 
   get answers() {
     return this._answers;
