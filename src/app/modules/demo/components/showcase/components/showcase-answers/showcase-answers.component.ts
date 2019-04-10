67ef748ea255f2f56853bccd2710a278beed11d7
abhisheksaini <saini.abhishek@hotmail.com>
2019-04-10 17:10:20 +0200

    modify the showcase-answer and v1

--- src/app/modules/demo/components/showcase/components/showcase-answers/showcase-answers.component.ts
+++ src/app/modules/demo/components/showcase/components/showcase-answers/showcase-answers.component.ts
@@ -1,8 +1,9 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { TagStats } from '../../../../../../models/tag-stats';
-import { Answer } from '../../../../../../models/answer';
 import { AnswerService } from '../../../../../../services/answer/answer.service';
 import { AuthService } from '../../../../../../services/auth/auth.service';
+import { TranslateNotificationsService } from '../../../../../../services/notifications/notifications.service';
+import { Answer } from '../../../../../../models/answer';
 
 @Component({
   selector: 'app-showcase-answers',
@@ -10,10 +11,10 @@ import { AuthService } from '../../../../../../services/auth/auth.service';
   styleUrls: ['./showcase-answers.component.scss']
 })
 
-export class ShowcaseAnswersComponent {
+export class ShowcaseAnswersComponent implements OnInit {
 
   @Input() set totalAnswers(value: number) {
-    this.count = value;
+    this._count = value;
   }
 
   @Input() set tagsStats(value: Array<TagStats>) {
@@ -23,17 +24,8 @@ export class ShowcaseAnswersComponent {
       this._answerService.getStarsAnswer(tags_id).subscribe((response) => {
         if (Array.isArray(response.result)) {
           this._answers = response.result;
-          this._topAnswers = response.result.slice(0, 6);
-
-          this._topAnswers = this._topAnswers.map((answer) => {
-            answer.isLoading = true;
-            return answer;
-          });
-
-          this._topAnswers.forEach((answer, index) => {
-            this._stopLoading(answer, index);
-          });
-
+          this._selectedAnswers = response.result.slice(0, 6);
+          this._startLoading(this._selectedAnswers);
         }
       });
     } else {
@@ -44,27 +36,83 @@ export class ShowcaseAnswersComponent {
 
   private _answers: Array<Answer> = [];
 
-  private _topAnswers: Array<any> = [];
+  private _topAnswers: Array<Answer> = [];
 
-  count: number;
+  private _count: number;
 
-  adminPass: boolean = false;
+  private _adminPass: boolean = false;
+
+  private _modalShow: boolean = false;
+
+  private _selectedAnswers: Array<Answer> = [];
 
   constructor(private _answerService: AnswerService,
-              private _authService: AuthService) {
+              private _authService: AuthService,
+              private _translateNotificationsService: TranslateNotificationsService) {
 
-    this.adminPass = this._authService.adminLevel > 2
+    this._adminPass = this._authService.adminLevel > 2
 
   }
 
 
-  private _stopLoading(answer: any, index: number) {
-    setTimeout(() => {
-      answer.isLoading = false;
-      this._topAnswers[index] = answer;
-    }, Math.floor(Math.random() * 2000) + 1000);
+  ngOnInit(): void {
   }
 
+
+  private _startLoading(answers: Array<Answer>) {
+
+    this._topAnswers = answers.map((answer) => {
+      answer.isLoading = true;
+      return answer;
+    });
+
+    this._stopLoading(this._topAnswers);
+
+  }
+
+
+  private _stopLoading(answers: Array<Answer>) {
+    if (answers.length) {
+      answers.forEach((answer, index) => {
+        setTimeout(() => {
+          answer.isLoading = false;
+          this._topAnswers[index] = answer;
+        }, Math.floor(Math.random() * 2000) + 1000);
+      });
+    }
+  }
+
+
+  public openModal(event: Event) {
+    event.preventDefault();
+    this._modalShow = true;
+  }
+
+
+  public activeAnswer(value: Answer) {
+    const index = this._selectedAnswers.findIndex((item: Answer) => item._id === value._id);
+    return index !== -1;
+  }
+
+
+  public onChangeAnswer(event: Event, answer: Answer) {
+    if (event.target['checked']) {
+      if (this._selectedAnswers.length < 6) {
+        this._selectedAnswers.push(answer);
+      } else {
+        this._translateNotificationsService.error('ERROR.ERROR_EN', 'You can only select 6 answers.');
+      }
+    } else {
+      this._selectedAnswers = this._selectedAnswers.filter((item: Answer) => item._id !== answer._id);
+    }
+  }
+
+
+  public onClickApply(event: Event) {
+    event.preventDefault();
+    this._startLoading(this._selectedAnswers);
+    this._modalShow = false;
+  }
 
   get answers() {
     return this._answers;
@@ -72,6 +120,26 @@ export class ShowcaseAnswersComponent {
 
   get topAnswers() {
     return this._topAnswers;
+  }
+
+  get modalShow(): boolean {
+    return this._modalShow;
+  }
+
+  set modalShow(value: boolean) {
+    this._modalShow = value;
+  }
+
+  get count(): number {
+    return this._count;
+  }
+
+  get adminPass(): boolean {
+    return this._adminPass;
+  }
+
+  get selectedAnswers(): Array<Answer> {
+    return this._selectedAnswers;
   }
 
 }
