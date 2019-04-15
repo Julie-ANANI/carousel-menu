9e485879b2bfca4ed7580248cf77a7c8bc082f29
abhisheksaini <saini.abhishek@hotmail.com>
2019-04-15 09:15:09 +0200

    modify and nxt version of header.

--- src/app/modules/common/header/header.component.ts
+++ src/app/modules/common/header/header.component.ts
@@ -1,17 +1,53 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, HostListener, OnDestroy } from '@angular/core';
 import { environment} from '../../../../environments/environment';
 import { AuthService } from '../../../services/auth/auth.service';
 import { Location } from '@angular/common';
 import { UserService } from '../../../services/user/user.service';
 import { User } from '../../../models/user.model';
+import { Header } from './interface/header';
+import { initTranslation, TranslateService } from '../../../i18n/i18n';
+import { CookieService } from 'ngx-cookie';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { SidebarInterface } from '../../sidebar/interfaces/sidebar-interface';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  animations: [
+
+    trigger(('animateSidebar'), [
+      state('inactive', style({
+        opacity: 0,
+        transform: 'translateX(-100%)'
+      })),
+      state('active', style({
+        opacity: 1,
+        transform: 'translateX(0)'
+      })),
+      transition('inactive => active', animate('400ms ease-in')),
+      transition('active => inactive', animate('700ms ease-out')),
+    ]),
+
+    trigger(('animateSidebarBackdrop'), [
+      state('inactive', style({
+        display: 'none',
+        opacity: 0,
+        background: 'none'
+      })),
+      state('active', style({
+        display: 'block',
+        opacity: 1,
+        background: 'rgba(0,0,0,0.75)',
+      })),
+      transition('inactive => active', animate('.5ms ease-in-out')),
+      transition('active => inactive', animate('700ms ease-in-out')),
+    ])
+
+  ]
 })
 
-export class HeaderComponent implements OnInit, OnDestroy {
+export class HeaderComponent implements OnDestroy {
 
   private _backOfficeValue: boolean = false; // if true, then display back office menu options.
 
@@ -19,30 +55,112 @@ export class HeaderComponent implements OnInit, OnDestroy {
 
   private _profilePicture: string;
 
-  userInitial: string;
+  private _userInitial: string;
 
-  email: string;
+  private _email: string;
 
-  name: string;
+  private _name: string;
+
+  private _currentLang: string;
+
+  private _flag: string;
+
+  private _sidebarValues: SidebarInterface = {};
+
+  private _clientRoutes: Array<Header> = [
+    { pageName: 'HEADER.DISCOVER', pageLink: '/discover', trackingClass: 'gtm-menu-discover' },
+    { pageName: 'HEADER.SHARED_REPORTS', pageLink: '/user/synthesis', trackingClass: 'gtm-menu-my-projects' },
+    { pageName: 'HEADER.MY_PROJECTS', pageLink: '/user/projects', trackingClass: 'gtm-menu-my-projects' },
+    ];
+
+  private _adminRoutes: Array<Header> = [
+    { pageName: 'Dashboard', pageLink: '/user/admin', adminLevel: 1 },
+    { pageName: 'Users', pageLink: '/user/admin/users', adminLevel: 1 },
+    { pageName: 'Professionals', pageLink: '/user/admin/professionals', adminLevel: 3 },
+    { pageName: 'Projects', pageLink: '/user/admin/projects', adminLevel: 1 },
+    { pageName: 'Libraries', pageLink: '/user/admin/libraries', adminLevel: 3 },
+    { pageName: 'Monitoring', pageLink: '/user/admin/monitoring', adminLevel: 3 },
+    { pageName: 'Settings', pageLink: '/user/admin/settings', adminLevel: 3 }
+  ];
 
   constructor(private _authService: AuthService,
               private _location: Location,
-              private _userService: UserService) {
+              private _userService: UserService,
+              private _translateService: TranslateService,
+              private _cookieService: CookieService) {
 
     this._userService.getSelf().subscribe((response: User) => {
-      this.userInitial = response.firstName && response.lastName ? `${response.firstName.slice(0, 1)}${response.lastName.slice(0, 1)}` :
-        response.firstName.slice(0, 2);
-      this.email = response.email;
-      this.name = response.name;
+
+      this._userInitial = response.firstName && response.lastName ?
+        `${response.firstName.slice(0, 1)}${response.lastName.slice(0, 1)}` : response.firstName.slice(0, 2);
+
+      this._email = response.email;
+      this._name = response.name;
       this._profilePicture = response.profilePic ? response.profilePic.url || '' : '';
+
     });
 
-    this._backOfficeValue = this._location.path().slice(5, 11) === '/admin';
+    this._initializeVariables();
 
   }
 
-  ngOnInit() {
 
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    if (window.innerWidth > 840) {
+      this._sidebarValues.animate_state = 'inactive';
+    }
+  }
+
+
+  /***
+   * initializing all the variables.
+   * @private
+   */
+  private _initializeVariables() {
+    initTranslation(this._translateService);
+    this._currentLang = this._translateService.currentLang;
+    this._setFlag();
+    this._backOfficeValue = this._location.path().slice(5, 11) === '/admin';
+    this._sidebarValues.animate_state = 'inactive';
+  }
+
+
+  /***
+   * based on the current lang show flag.
+   * @private
+   */
+  private _setFlag() {
+    this._flag = this._currentLang === 'en' ? 'US' : 'FR';
+  }
+
+
+  /***
+   * set the lang as current lang.
+   * @param lang
+   */
+  public setLang(lang: string) {
+
+    if (lang === 'fr') {
+      this._propagateTranslation(lang);
+      this._currentLang = lang;
+    } else {
+      this._propagateTranslation(lang);
+      this._currentLang = lang;
+    }
+
+    this._setFlag();
+
+  }
+
+
+  /***
+   * Setting the lang and the cookie.
+   * @param {string} lang
+   */
+  private _propagateTranslation(lang: string) {
+    this._cookieService.put('user_lang', lang || 'en');
+    this._translateService.use(lang || 'en');
   }
 
 
@@ -59,6 +177,12 @@ export class HeaderComponent implements OnInit, OnDestroy {
    */
   public toggleMenuState() {
     this._displayMenuOptions = !this._displayMenuOptions;
+    this._sidebarValues.animate_state = this._sidebarValues.animate_state === 'inactive' ? 'active' : 'inactive';
+  }
+
+
+  public closeSidebar() {
+    this._sidebarValues.animate_state = 'inactive';
   }
 
 
@@ -79,6 +203,7 @@ export class HeaderComponent implements OnInit, OnDestroy {
     return environment.logoURL;
   }
 
+
   public getCompany(): string {
     return environment.companyShortName;
   }
@@ -88,9 +213,11 @@ export class HeaderComponent implements OnInit, OnDestroy {
     return !!this._profilePicture && this._profilePicture !== '';
   }
 
+
   public isMainDomain(): boolean {
     return environment.domain === 'umi';
   }
+
 
   get backOfficeValue(): boolean {
     return this._backOfficeValue;
@@ -106,6 +233,38 @@ export class HeaderComponent implements OnInit, OnDestroy {
 
   get profilePicture(): string {
     return this._profilePicture;
+  }
+
+  get userInitial(): string {
+    return this._userInitial;
+  }
+
+  get email(): string {
+    return this._email;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  get currentLang(): string {
+    return this._currentLang;
+  }
+
+  get flag(): string {
+    return this._flag;
+  }
+
+  get sidebarValues(): SidebarInterface {
+    return this._sidebarValues;
+  }
+
+  get clientRoutes(): Array<Header> {
+    return this._clientRoutes;
+  }
+
+  get adminRoutes(): Array<Header> {
+    return this._adminRoutes;
   }
 
   ngOnDestroy(): void {
