7c95bc24b7cd3b866519a54677f7a093600c88d6
abhisheksaini <saini.abhishek@hotmail.com>
2019-04-17 17:13:52 +0200

    added search-input template 2 and move the first search-input

---
+++ src/app/modules/utility-components/search-inputs/search-template-2/search-input-2.component.ts
@@ -0,0 +1,162 @@
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import 'rxjs/Rx';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-search-input-2',
+  templateUrl: './search-input-2.component.html',
+  styleUrls: ['./search-input-2.component.scss']
+})
+
+export class SearchInput2Component implements OnInit, OnDestroy {
+
+  @Output() finalValueEmit = new EventEmitter<any>(); // this is the final value that we send to the parent. By default we send only the string type.
+
+  @Input() suggestions: any; // values that we get from the parent component. By default it is Array<{'text': string}> type.
+
+  @Input() set searchActive(value) {
+    this._searchActive = !!value;
+  }
+
+  private _searchField: FormControl; // declare the FormControl as properties of our components.
+
+  private _displaySuggestion = true;
+
+  private _crossIcon = false;
+
+  private _searchActive = false;
+
+  private _currentFocus = -1;
+
+  private _localSuggestions: any = [];
+
+  private _ngUnsubscribe: Subject<any> = new Subject();
+
+  constructor() { }
+
+  ngOnInit() {
+    this._searchField = new FormControl(); // create the form control.
+
+    this._searchField.valueChanges.pipe(takeUntil(this._ngUnsubscribe)).subscribe(input => {
+      this._crossIcon = input !== '';
+      this._displaySuggestion = true;
+      this.searchLocally(input);
+    });
+
+  }
+
+  private searchLocally(value: string) {
+    this._localSuggestions = [];
+
+    if (value !== null) {
+      this.suggestions.forEach((items: any) => {
+        const index = this._localSuggestions.findIndex((item: any) => item.text === items.text);
+        const temp = items.text.toLowerCase().includes(value.toLowerCase());
+        if (temp) {
+          if (index === -1) {
+            this._localSuggestions.push(items);
+          }
+        }
+      });
+    }
+
+  }
+
+
+  onValueSelect(value: any) {
+    this._searchField.setValue(value.text);
+    this.finalValueEmit.emit(value.text);
+    this._displaySuggestion = false;
+    this._crossIcon = true;
+  }
+
+
+  clearInput(event: Event) {
+    event.preventDefault();
+    this._searchField.reset();
+    this._crossIcon = false;
+    this.finalValueEmit.emit('');
+  }
+
+
+  closeSearch(event: Event) {
+    event.preventDefault();
+    this._searchField.reset();
+    this._crossIcon = false;
+    this.searchActive = !this.searchActive;
+
+    if (this.searchActive === false) {
+      this.finalValueEmit.emit('');
+    }
+
+  }
+
+  onKeyboardPress(event: Event) {
+    this._displaySuggestion = true;
+
+    if (event['key'] === 40 || event['code'] === 'ArrowDown') {
+      this._currentFocus++;
+      this.setFocus(this._currentFocus);
+    } else if (event['key'] === 38 || event['code'] === 'ArrowUp') {
+      this._currentFocus--;
+      this.setFocus(this._currentFocus);
+    } else if (event['key'] === 13 || event['code'] === 'Enter') {
+      event.preventDefault();
+
+      if (this._currentFocus > -1) {
+        if (this._localSuggestions[this._currentFocus].text !== '') {
+          this._searchField.setValue(this._localSuggestions[this._currentFocus].text);
+          this._currentFocus = -1;
+        }
+      }
+
+      this.finalValueEmit.emit(this._searchField.value);
+      this._displaySuggestion = false;
+    }
+
+  }
+
+
+  private setFocus(value: number) {
+    if (value >= this._localSuggestions.length) {
+      this._currentFocus = 0;
+    }
+
+    if (value < 0) {
+      this._currentFocus = this._localSuggestions.length - 1;
+    }
+
+  }
+
+  get searchField(): FormControl {
+    return this._searchField;
+  }
+
+  get displaySuggestion(): boolean {
+    return this._displaySuggestion;
+  }
+
+  get crossIcon(): boolean {
+    return this._crossIcon;
+  }
+
+  get searchActive(): boolean {
+    return this._searchActive;
+  }
+
+  get currentFocus(): number {
+    return this._currentFocus;
+  }
+
+  get localSuggestions(): any {
+    return this._localSuggestions;
+  }
+
+  ngOnDestroy(): void {
+    this._ngUnsubscribe.next();
+    this._ngUnsubscribe.complete();
+  }
+
+}
---
+++ src/app/modules/utility-components/search-inputs/search-template-2/search-input-2.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { SearchInput2Component } from './search-input-2.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    TranslateModule,
+    ReactiveFormsModule,
+    FormsModule
+  ],
+  declarations: [
+    SearchInput2Component
+  ],
+  exports: [
+    SearchInput2Component
+  ]
+})
+
+export class SearchInput2Module {}
