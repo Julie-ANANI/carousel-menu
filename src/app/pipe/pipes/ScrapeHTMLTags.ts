e72c71672b50eec361d8d4eb1d84d7dfe3a4df9b
marcvincenti <mvincenti@umi.us>
2019-04-25 16:49:02 +0200

    patch regex universal

--- src/app/pipe/pipes/ScrapeHTMLTags.ts
+++ src/app/pipe/pipes/ScrapeHTMLTags.ts
@@ -6,24 +6,12 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 export class ScrapeHTMLTags implements PipeTransform {
 
-  decodeEntities (str: string) {
-    const el = document.createElement('textarea');
-
-    el.innerHTML = str
-      .replace(/<script[^>]*>([\S\s]*?)<\/script>/gmi, '')
-      .replace(/<\/?\w(?:[^"'>]|"[^"]*"|'[^']*')*>/gmi, '');
-
-    return el.value;
-  }
-
   transform (value: any): any {
 
-    const regex = /<\/?[\w\s="/.':;#-\/?]+>/gi;
-
-    value = this.decodeEntities(value);
+    const regex = /<\/?\w(?:[^"'>]|"[^"]*"|'[^']*')*>/gi;
 
     if (value) {
-      return value.replace(regex, "");
+      return value.replace(regex, '');
     } else {
       return value;
     }
