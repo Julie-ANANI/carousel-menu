d3cfbd3ef9c359faa513ec1327d4c4de01321d0f
marcvincenti <mvincenti@umi.us>
2019-04-25 15:50:15 +0200

    add sentry in prod mode

--- src/app/handlers/error-handler.ts
+++ src/app/handlers/error-handler.ts
@@ -1,6 +1,12 @@
 import { ErrorHandler, Injectable, Injector } from '@angular/core';
 import { HttpErrorResponse } from '@angular/common/http';
 import { ErrorService } from '../services/error/error.service';
+import { environment } from '../../environments/environment';
+import * as Sentry from '@sentry/browser';
+
+Sentry.init({
+  dsn: 'https://d86e9ff2dfbb40eab9632f0a3a599757@sentry.io/1315751'
+});
 
 @Injectable()
 export class GlobalErrorHandler implements ErrorHandler {
@@ -9,8 +15,13 @@ export class GlobalErrorHandler implements ErrorHandler {
 
   handleError(error: Error | HttpErrorResponse) {
 
-    const errorService = this.injector.get(ErrorService);
-    errorService.handleError(error);
+    if (environment.production) {
+      const eventId = Sentry.captureException(error);
+      Sentry.showReportDialog({ eventId });
+    } else {
+      const errorService = this.injector.get(ErrorService);
+      errorService.handleError(error);
+    }
 
     throw error;
 
