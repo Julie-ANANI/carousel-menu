9a5c833c9cbd1a552ea9a43465ff6d7f90af1e32
marcvincenti <mvincenti@umi.us>
2019-04-29 15:52:05 +0200

    do not need to log errors (we already have the console.err with context on page)

--- src/app/services/error/error.service.ts
+++ src/app/services/error/error.service.ts
@@ -2,7 +2,6 @@ import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
 import { Router } from '@angular/router';
 import { HttpErrorResponse } from '@angular/common/http';
 import { AuthService } from '../auth/auth.service';
-import { TranslateNotificationsService } from '../notifications/notifications.service';
 import { environment } from '../../../environments/environment';
 import { version } from '../../../environments/version';
 import * as Sentry from '@sentry/browser';
@@ -18,8 +17,7 @@ export class ErrorService {
 
   constructor(@Inject(PLATFORM_ID) protected platformId: Object,
               private router: Router,
-              private auth: AuthService,
-              private translateNotificationService: TranslateNotificationsService) {}
+              private auth: AuthService) {}
 
   public handleError(error: Error | HttpErrorResponse) {
 
@@ -33,7 +31,6 @@ export class ErrorService {
           username: user ? `${user.firstName} ${user.lastName}` : '',
           id: user ? user.id : ''
         });
-
         scope.setTag('route', environment.clientUrl + this.router.url);
 
         const eventId = Sentry.captureException(error);
@@ -44,48 +41,10 @@ export class ErrorService {
 
       });
 
-    } else {
-
-      this.logError(error);
-
     }
 
     console.error(error);
 
-  }
-
-  private logError(error: Error | HttpErrorResponse): void {
-    if (error instanceof HttpErrorResponse) {
-      // Server or connection error happened
-      if (!navigator.onLine) {
-        // Handle offline error
-        this.translateNotificationService.error('ERROR.ERROR', 'ERROR.NO_CONNECTION');
-      } else {
-        // Handle Http Error (error.status === 403, 404...)
-        console.log('----- Server Error -----');
-        const domain = environment.domain;
-        const time = new Date().toISOString();
-        const route = environment.clientUrl + this.router.url;
-        const user = this.auth.user ? this.auth.user.firstName + ' ' + this.auth.user.lastName : '';
-        const type = error.name;
-        const message = error.message || error.toString();
-        const status = error.status;
-        const url = error.url;
-        console.log({domain, time, route, user, type, message, status, url});
-        console.log('----- /Server Error -----');
-      }
-    } else {
-      // Handle Client Error (Angular Error, ReferenceError...)
-      console.log('----- Client Error -----');
-      const domain = environment.domain;
-      const time = new Date().toISOString();
-      const route = environment.clientUrl + this.router.url;
-      const user = this.auth.user ? this.auth.user.firstName + ' ' + this.auth.user.lastName : '';
-      const type = error.name;
-      const message = error.message || error.toString();
-      console.log({domain, time, route, user, type, message});
-      console.log('----- /Client Error -----');
-    }
   }
 
 }
