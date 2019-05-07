621d3e5625bcb059f9cbca1304bbd5426b3b8c9f
abhisheksaini <saini.abhishek@hotmail.com>
2019-05-07 14:27:14 +0200

    fix the bug of photo upload zone.

--- src/app/modules/shared/components/shared-upload-zone-photo/shared-upload-zone-photo.component.ts
+++ src/app/modules/shared/components/shared-upload-zone-photo/shared-upload-zone-photo.component.ts
@@ -11,21 +11,23 @@ import { TranslateNotificationsService } from '../../../../services/notification
 
 export class SharedUploadZonePhotoComponent {
 
-  public loading = false;
-
   @Input() uri: string;
 
   @Output() cbFn: EventEmitter <any> = new EventEmitter();
 
   @ViewChild('fileInput') fileInput: any;
 
-  constructor(private notificationsService: TranslateNotificationsService,
-              private http: HttpClient) {}
+  loading = false;
+
+  constructor(private _translateNotificationsService: TranslateNotificationsService,
+              private _httpClient: HttpClient) { }
+
 
   public dropped(event: any) {
     if (this.loading === false) {
       if (event.files) {
         this.loading = true;
+
         for (const droppedFile of event.files) {
           // Is it a file?
           if (droppedFile.fileEntry && droppedFile.fileEntry.isFile) {
@@ -38,25 +40,29 @@ export class SharedUploadZonePhotoComponent {
         }
       } else if (event.target.files) {
         this.loading = true;
+
         for (const file of event.target.files) {
           this.uploadFile(file);
         }
+
       }
     }
   }
 
+
   private uploadFile = (file: File) => {
     const formData = new FormData();
     formData.append('file', file, file.name);
-    this.http.post(this.uri, formData)
-      .subscribe((data: any) => {
-        // Sanitized logo returned from backend
-        this.loading = false;
-        this.cbFn.emit(data);
-      }, (err: any) => {
-        this.notificationsService.error('ERROR.ERROR', err);
-        this.loading = false;
-      });
+
+    this._httpClient.post(this.uri, formData).subscribe((data: any) => {
+      // Sanitized logo returned from backend.
+      this.loading = false;
+      this.cbFn.emit(data);
+      }, () => {
+      this.loading = false;
+      this._translateNotificationsService.error('ERROR.ERROR', 'ERROR.SERVER_ERROR');
+    });
+
   }
 
 }
