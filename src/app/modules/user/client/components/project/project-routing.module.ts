42b9ce098127fa6f2c1a1886b57d1e5a368bba1b
marcvincenti <mvincenti@umi.us>
2019-05-13 17:35:07 +0200

    we already loaded all components as directives, we don't want to load them twice.

--- src/app/modules/user/client/components/project/project-routing.module.ts
+++ src/app/modules/user/client/components/project/project-routing.module.ts
@@ -4,13 +4,6 @@ import { Routes, RouterModule } from '@angular/router';
 import { InnovationResolver } from '../../../../../resolvers/innovation.resolver';
 
 import { ProjectComponent } from './project.component';
-import { SetupComponent } from './components/setup/setup.component';
-import { ExplorationComponent } from './components/exploration/exploration.component';
-import { SurveyComponent } from './components/setup/components/survey/survey.component';
-import { SharedMarketReportComponent } from '../../../../shared/components/shared-market-report/shared-market-report.component';
-import { SharedProjectEditCardsComponent } from '../../../../shared/components/shared-project-edit-cards-component/shared-project-edit-cards.component';
-import { SharedProjectSettingsComponent } from '../../../../shared/components/shared-project-settings-component/shared-project-settings.component';
-
 
 const projectRoutes: Routes = [
   {
@@ -19,23 +12,14 @@ const projectRoutes: Routes = [
     resolve: { innovation : InnovationResolver },
     runGuardsAndResolvers: 'always',
     children: [
-      {
-        path: 'exploration',
-        component: ExplorationComponent,
-        pathMatch: 'full'
-      },
-      {
-        path: 'synthesis',
-        component: SharedMarketReportComponent,
-        pathMatch: 'full'
-      },
+      { path: 'exploration', pathMatch: 'full' },
+      { path: 'synthesis', pathMatch: 'full' },
       {
         path: 'setup',
-        component: SetupComponent,
         children: [
-          { path: 'survey', component: SurveyComponent, pathMatch: 'full' },
-          { path: 'pitch', component: SharedProjectEditCardsComponent, pathMatch: 'full' },
-          { path: 'targeting', component: SharedProjectSettingsComponent, pathMatch: 'full' },
+          { path: 'survey', pathMatch: 'full' },
+          { path: 'pitch', pathMatch: 'full' },
+          { path: 'targeting', pathMatch: 'full' },
           { path: '', redirectTo: 'targeting', pathMatch: 'full' }
         ]
       },
