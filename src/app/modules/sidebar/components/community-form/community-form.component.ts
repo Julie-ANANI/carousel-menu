bc1d650f127ab83792677c94615e9619b56fadb8
abhisheksaini <saini.abhishek@hotmail.com>
2019-05-20 15:43:31 +0200

    added the functionality to add the professional as an ambassador but still remains to call the service to do so.

--- src/app/modules/sidebar/components/community-form/community-form.component.ts
+++ src/app/modules/sidebar/components/community-form/community-form.component.ts
@@ -1,10 +1,16 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ProfessionalsService } from '../../../../services/professionals/professionals.service';
+import { TranslateNotificationsService } from '../../../../services/notifications/notifications.service';
+import { Router } from '@angular/router';
+import { Professional } from '../../../../models/professional';
 
 @Component({
   selector: 'community-form',
   templateUrl: './community-form.component.html',
   styleUrls: ['./community-form.component.scss']
 })
+
 export class CommunityFormComponent implements OnInit {
 
   @Input() set context(value: any) {
@@ -13,7 +19,9 @@ export class CommunityFormComponent implements OnInit {
 
   @Input() set sidebarState(value: string) {
     if (value === undefined || value === 'active') {
-
+      this._buildForm();
+      this._lookingProfessional = false;
+      this._addingProfessional = false;
     }
   }
 
@@ -41,10 +49,30 @@ export class CommunityFormComponent implements OnInit {
 
   private _innovationId: string;
 
-  constructor() { }
+  private _form: FormGroup;
+
+  private _lookingProfessional: boolean;
+
+  private _addingProfessional: boolean;
+
+  private _professional: Professional;
+
+  constructor(private _formBuilder: FormBuilder,
+              private _professionalService: ProfessionalsService,
+              private _router: Router,
+              private _translateNotificationsService: TranslateNotificationsService) { }
 
   ngOnInit() {
     console.log(this._parentCb && typeof this._parentCb === 'function');
+  }
+
+
+  private _buildForm() {
+    this._form = this._formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      firstName: ['', [Validators.required]],
+      lastName: ['', [Validators.required]]
+    });
   }
 
 
@@ -65,6 +93,62 @@ export class CommunityFormComponent implements OnInit {
   }
 
 
+  public onEmailChange() {
+    const email = this._form.get('email').value;
+    const config: any = {
+      fields: 'firstName lastName ambassador',
+      limit: '1',
+      offset: '0',
+      search: '{}',
+      email: email,
+      sort: '{ "created": -1 }'
+    };
+
+    if (email && this._form.get('email').valid) {
+
+      this._lookingProfessional = true;
+
+      this._professionalService.getAll(config).subscribe((response) => {
+        if (response && response.result && response.result.length > 0) {
+          this._professional = response.result[0];
+          this._form.patchValue(response.result[0]);
+        }
+      }, () => {
+        this._translateNotificationsService.error('ERROR.ERROR', 'ERROR.CANNOT_REACH');
+      }, () => {
+        this._lookingProfessional = false;
+      });
+
+    }
+
+  }
+
+
+  public addAmbassador() {
+    if (this._form.valid) {
+      this._addingProfessional = true;
+
+      if (this._professional) {
+        this._professional.ambassador.is = true;
+        this._professionalService.save(this._professional._id, this._professional).subscribe(() => {
+          this.goToAmbassador();
+        }, () => {
+          this._translateNotificationsService.error('ERROR.ERROR', 'ERROR.CANNOT_REACH');
+        });
+      } else {
+
+      }
+
+    }
+  }
+
+
+  public goToAmbassador() {
+    this._router.navigate([`user/admin/community/members/${this._professional._id}`]);
+  }
+
+
+
   get actionType() {
     return this._actionType;
   }
@@ -79,6 +163,22 @@ export class CommunityFormComponent implements OnInit {
 
   get innovationId(): string {
     return this._innovationId;
+  }
+
+  get form(): FormGroup {
+    return this._form;
+  }
+
+  get lookingProfessional(): boolean {
+    return this._lookingProfessional;
+  }
+
+  get addingProfessional(): boolean {
+    return this._addingProfessional;
+  }
+
+  get professional(): Professional {
+    return this._professional;
   }
 
 }
