ff771cb57b3827ddbdc5a0c5e0e46ed92fa7263e
abhisheksaini <saini.abhishek@hotmail.com>
2019-06-03 17:06:51 +0200

    added msg when no professional found on filtering. Some other modifications.

--- src/app/modules/demo/components/search-tool/i18n/fr.ts
+++ src/app/modules/demo/components/search-tool/i18n/fr.ts
@@ -2,6 +2,12 @@ export const SEARCH_DEMO = {
 
   'SOME_PROS': 'Quelques-uns des professionnels',
 
-  'PROFESSIONALS': 'PROFESSIONNELS TROUVÉS'
+  'PROFESSIONALS': 'PROFESSIONNELS TROUVÉS',
+
+  'NO_RESULT_HEADING': 'Aucun résultat trouvé',
+
+  'NO_RESULT_TEXT': 'Essayez de sélectionner un autre pays pour trouver les professionnels que vous recherchez.',
+
+  'RESULTS_FOR': 'résultats pour '
 
 };
