ce27c1ce354ebd47a268c5f713d9d4639db988c2
abhisheksaini <saini.abhishek@hotmail.com>
2019-06-12 17:21:20 +0200

    better call.

--- src/app/modules/shared/components/shared-sort/shared-sort.component.ts
+++ src/app/modules/shared/components/shared-sort/shared-sort.component.ts
@@ -8,47 +8,72 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 export class SharedSortComponent {
 
-  @Input() set config(value: any) {
-    this._config = value;
+  @Input() set sortConfig(value: string) {
+    this._sort = value;
+    this._getCurrentKeyValue();
   }
 
   @Input() set property(value: string) {
     this._property = value;
   }
 
-  @Output() configChangeSort = new EventEmitter <any>();
+  @Output() sortConfigChange: EventEmitter<string> = new EventEmitter<string>();
 
   private _property: string;
 
-  private _config: any;
+  private _sort: string;
+
+  private _currentKey: string;
+
+  private _currentKeyValue: number;
 
   constructor() {}
+
+  private _getCurrentKeyValue() {
+    const currentSort = this.currentSort;
+
+    for (let sortKey of Object.keys(currentSort)) {
+      this._currentKey = sortKey;
+      this._currentKeyValue = currentSort[sortKey];
+    }
+
+  }
 
   public sort(event: Event): void {
     event.preventDefault();
 
-    const previousOrder = this._config.sort[this._property] || 0;
-    const newOrder = previousOrder === 1 ? -1 : 1;
-    this._config.sort = {};
+    let newOrder;
 
-    if (newOrder === 1 || newOrder === -1) {
-      this._config.sort[this._property] = newOrder;
+    if (this._currentKey === this._property) {
+      newOrder = this._currentKeyValue === 1 ? -1 : 1;
+    } else {
+      newOrder = -1;
+      this._currentKey = this._property;
     }
 
-    this.configChangeSort.emit(this._config);
+    const newSort = { [this._currentKey]: newOrder };
+    this.sortConfigChange.emit(JSON.stringify(newSort));
 
   }
 
+  get currentSort() {
+    return !!this._sort ? JSON.parse(this._sort) : {};
+  }
+
   get order(): number {
-    return this._config.sort[this._property] || 0;
+    return this._currentKey === this._property ? this._currentKeyValue : 0;
   }
 
   get property(): string {
     return this._property;
   }
 
-  get config(): any {
-    return this._config;
+  get currentKey(): string {
+    return this._currentKey;
+  }
+
+  get currentKeyValue(): number {
+    return this._currentKeyValue;
   }
 
 }
