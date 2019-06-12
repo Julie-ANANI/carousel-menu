1ac6853194b45973abf64f50e619181ec2229699
abhisheksaini <saini.abhishek@hotmail.com>
2019-06-12 18:14:16 +0200

    added pagination template 2 component.

---
+++ src/app/modules/utility-components/paginations/pagination-template-2/pagination-template-2.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pagination-2',
+  templateUrl: './pagination-template-2.component.html',
+  styleUrls: ['./pagination-template-2.component.scss']
+})
+
+export class PaginationTemplate2Component implements OnInit {
+
+  constructor() {}
+
+  ngOnInit() { }
+
+}
