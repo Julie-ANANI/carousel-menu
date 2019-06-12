26fd3fe41da6c3abc8054f61ebd32fb26133622a
abhisheksaini <saini.abhishek@hotmail.com>
2019-06-12 18:14:52 +0200

    marked attributes to remove.

--- src/app/modules/table/models/table.ts
+++ src/app/modules/table/models/table.ts
@@ -3,20 +3,21 @@
 export interface Table {
   readonly _selector?: string;
   readonly _title?: string;
-  readonly _isHeadable?: boolean;
+  readonly _isHeadable?: boolean; //Todo remove
   readonly _isSelectable?: boolean;
   readonly _isEditable?: boolean;
-  readonly _isNoTitle?: boolean;
+  readonly _isNoTitle?: boolean; //Todo remove
   readonly _isTitle?: boolean;
   readonly _isShowable?: boolean;
   readonly _isDeletable?: boolean;
   readonly _isFiltrable?: boolean;
-  readonly _isLocal ?: boolean;
-  readonly _isNotPaginable?: boolean;
-  readonly _reloadColumns?: boolean;
+  readonly _isLocal?: boolean; //Todo remove
+  readonly _isNotPaginable?: boolean; //Todo remove
+  readonly _reloadColumns?: boolean; //Todo remove
   readonly _columns: Column[];
-  readonly _actions?: string[];
+  readonly _actions?: string[]; //Todo remove
   readonly _editIndex?: number;
+  readonly _isPaginable?: boolean;
   readonly _editButtonLabel?: string;
   readonly _buttons?: Array<{ _icon?: string, _label: string }>;
   _content: any[];
