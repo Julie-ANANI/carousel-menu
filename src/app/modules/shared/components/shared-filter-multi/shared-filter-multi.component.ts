1c4d15e25f28be029dd7514707fb61ceea93ec81
abhisheksaini <saini.abhishek@hotmail.com>
2019-06-12 10:19:30 +0200

    updated the filter and search.

--- src/app/modules/shared/components/shared-filter-multi/shared-filter-multi.component.ts
+++ src/app/modules/shared/components/shared-filter-multi/shared-filter-multi.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Column, types } from '../../../table/models/column';
 import { Choice } from '../../../table/models/choice';
+import { Config } from '../../../../models/config';
 
 @Component({
   selector: 'app-shared-multi-filter',
@@ -10,13 +11,19 @@ import { Choice } from '../../../table/models/choice';
 
 export class SharedFilterMultiComponent {
 
-  @Input() config: any;
-
-  @Input() set props(value: Column[]) {
-    this.loadProps(value);
+  @Input() set searchConfig(value: Config) {
+    this._searchConfig = value;
   }
 
-  @Output() configChange = new EventEmitter <any>();
+  @Input() set props(value: Column[]) {
+    this._loadProps(value);
+  }
+
+  @Input() set tableView(value: boolean) {
+    this._tableView = value;
+  }
+
+  @Output() onConfigUpdation = new EventEmitter<Config>();
 
   private _currentTextProp: Column = {_attrs: [''], _name: '', _type: 'TEXT'};
 
@@ -24,40 +31,13 @@ export class SharedFilterMultiComponent {
 
   private _otherProps: Column[] = [];
 
+  private _tableView: boolean;
+
+  private _searchConfig: Config = {};
+
   constructor() {}
 
-  // For values linked with config
-  filterText(event: any) {
-    this.config.offset = '0';
-    let value = event.value || '';
-      if (value === '') {
-        this.config.search = '{}';
-        this.configChange.emit(this.config);
-      } else {
-        //Detect an "special" query...
-        value = value.split(',');
-        let _search = {};
-        value.forEach((queryStr, idx)=>{
-          _search[this._currentTextProp._attrs[idx]] = encodeURIComponent(queryStr.trim());
-        });
-        this.config.search = JSON.stringify(_search);
-        this.configChange.emit(this.config);
-      }
-  }
-
-  filterOther(prop: Column) {
-    this.config.offset = 0;
-    if (this.config[prop._attrs[0]] === null) {
-        delete this.config[prop._attrs[0]];
-    }
-    this.configChange.emit(this.config);
-  }
-
-  textIsSelected(prop: Column): boolean {
-    return this._currentTextProp._attrs[0] === prop._attrs[0];
-  }
-
-  loadProps(value: Column[]) {
+  private _loadProps(value: Column[]) {
     if (value) {
       this._textProps = value.filter(value1 =>
         (this.getType(value1) === 'TEXT' || this.getType(value1) === 'COUNTRY') && (this.isFiltrable(value1)));
@@ -70,16 +50,62 @@ export class SharedFilterMultiComponent {
     }
   }
 
-  get currentTextProp(): Column {
-    return this._currentTextProp;
+  public textIsSelected(prop: Column): boolean {
+    return this._currentTextProp._attrs[0] === prop._attrs[0];
   }
 
-  get textProps() {
-    return this._textProps;
+  public isFiltrable(column: Column): boolean {
+    return column._isFiltrable === undefined ? true : column._isFiltrable;
   }
 
-  get otherProps() {
-    return this._otherProps;
+  public onChangeTextProp(prop: any) {
+    this._currentTextProp = this._textProps.find(value => value._attrs[0] === prop.target['value']);
+  }
+
+  // For values linked with config
+  public onSearch(event: any) {
+
+    let value = event.value || '';
+
+      if (value === '') {
+        this._searchConfig.search = '{}';
+        this.onConfigUpdation.emit(this._searchConfig);
+      } else {
+        //Detect an "special" query...
+        value = value.split(',');
+        let _search = {};
+
+        value.forEach((queryStr, index) => {
+          _search[this._currentTextProp._attrs[index]] = encodeURIComponent(queryStr.trim());
+        });
+
+        this._searchConfig.search = JSON.stringify(_search);
+        this.onConfigUpdation.emit(this._searchConfig);
+
+      }
+  }
+
+  public onOtherFilter(prop: Column, event: Event) {
+    const attr: string = prop._attrs[0];
+    let currentFilter = {};
+    currentFilter[attr] = event.target['value'];
+
+    if (this._searchConfig.filter) {
+      this._searchConfig.filter.forEach((filter, index) => {
+        for (let key in JSON.parse(filter) ) {
+          if (key === attr) {
+            this._searchConfig.filter[index] = JSON.stringify(currentFilter);
+          } else {
+            this._searchConfig.filter.push(JSON.stringify(currentFilter));
+          }
+        }
+      });
+    } else {
+      this._searchConfig.filter = [JSON.stringify(currentFilter)]
+    }
+
+    this.onConfigUpdation.emit(this._searchConfig);
+
   }
 
   getType(column: Column): types {
@@ -106,12 +132,24 @@ export class SharedFilterMultiComponent {
     return choice._alias || choice._name;
   }
 
-  isFiltrable(column: Column): boolean {
-    return column._isFiltrable === undefined ? true : column._isFiltrable;
+  get currentTextProp(): Column {
+    return this._currentTextProp;
   }
 
-  changeCurrentTextProp(prop: any) {
-    this._currentTextProp = this._textProps.find(value => value._attrs[0] === prop.srcElement.value);
+  get textProps() {
+    return this._textProps;
+  }
+
+  get otherProps() {
+    return this._otherProps;
+  }
+
+  get tableView(): boolean {
+    return this._tableView;
+  }
+
+  get searchConfig(): Config {
+    return this._searchConfig;
   }
 
 }
