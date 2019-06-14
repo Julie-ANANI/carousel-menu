215224fc34d0e99741d435d9dcc45c1f51e62d0f
abhisheksaini <saini.abhishek@hotmail.com>
2019-06-14 17:22:26 +0200

    added navigation in pagination.

--- src/app/modules/utility-components/paginations/pagination-template-2/pagination-template-2.component.ts
+++ src/app/modules/utility-components/paginations/pagination-template-2/pagination-template-2.component.ts
@@ -20,7 +20,11 @@ export class PaginationTemplate2Component implements OnInit {
 
   private _pagination: Pagination;
 
-  parPages: Array<number> = [ 10, 25, 50, 100 ];
+  private _parPages: Array<number> = [ 10, 25, 50, 100 ];
+
+  private _startPageNumber: number;
+
+  private _endPageNumber: number;
 
   constructor() {}
 
@@ -28,6 +32,8 @@ export class PaginationTemplate2Component implements OnInit {
     this._pagination.currentPage = 1;
     this._pagination.previousPage = 0;
     this._pagination.nextPage = 2;
+    this._startPageNumber = 0;
+    this._endPageNumber = 11;
   }
 
   private _calculatePage() {
@@ -42,12 +48,67 @@ export class PaginationTemplate2Component implements OnInit {
 
   }
 
+  public navigatePage(pageNumber: number) {
+    const mediumNumber = this._pagination.paginatorNumber[this._endPageNumber - 1] - 5;
+
+    if (pageNumber > mediumNumber) {
+      this.moveForward(pageNumber, mediumNumber);
+    } else {
+      this.moveBackward(pageNumber, mediumNumber);
+    }
+
+    this._pagination.currentPage = pageNumber;
+
+  }
+
+  public moveForward(pageNumber: number, mediumNumber: number) {
+    const diff = pageNumber - mediumNumber;
+
+    if (this._endPageNumber + diff < this._pagination.paginatorNumber.length) {
+      this._endPageNumber += diff;
+    } else {
+      this._endPageNumber = this._pagination.paginatorNumber.length;
+    }
+
+    this._startPageNumber = this._endPageNumber - 11;
+
+  }
+
+  public moveBackward(pageNumber: number, mediumNumber:number) {
+    const diff = mediumNumber - pageNumber;
+
+    if (this._startPageNumber - diff > 0) {
+      this._startPageNumber -=diff;
+    } else {
+      this._startPageNumber = 0;
+    }
+
+    this._endPageNumber = this._startPageNumber + 11;
+
+  }
+
   public setPageNumber() {
 
   }
 
+  public onChangeParPage(value: number) {
+    this._pagination.parPage = value;
+  }
+
   get pagination(): Pagination {
     return this._pagination;
+  }
+
+  get parPages(): Array<number> {
+    return this._parPages;
+  }
+
+  get startPageNumber(): number {
+    return this._startPageNumber;
+  }
+
+  get endPageNumber(): number {
+    return this._endPageNumber;
   }
 
 }
