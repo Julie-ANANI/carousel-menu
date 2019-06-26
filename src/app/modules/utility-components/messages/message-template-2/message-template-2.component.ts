29ec324b9d54042fc10ab8547d51a962131d9807
abhisheksaini <saini.abhishek@hotmail.com>
2019-06-26 15:26:22 +0200

    added bounce effect.

--- src/app/modules/utility-components/messages/message-template-2/message-template-2.component.ts
+++ src/app/modules/utility-components/messages/message-template-2/message-template-2.component.ts
@@ -12,7 +12,13 @@ export class MessageTemplate2Component {
     this._imageSrc = value;
   }
 
+  @Input() set animation(value: boolean) {
+    this._animation = value;
+  }
+
   private _imageSrc = '';
+
+  private _animation: boolean;
 
   private _defaultSrc = 'https://res.cloudinary.com/umi/image/upload/v1542810328/app/default-images/bot/info.svg';
 
@@ -20,6 +26,10 @@ export class MessageTemplate2Component {
 
   get imageSrc(): string {
     return this._imageSrc;
+  }
+
+  get animation(): boolean {
+    return this._animation;
   }
 
   get defaultSrc(): string {
