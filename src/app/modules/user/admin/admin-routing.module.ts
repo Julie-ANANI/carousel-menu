2e1edf88c5d86581d2fc8d9e022cbfb4d9842fe9
abhisheksaini <saini.abhishek@hotmail.com>
2019-06-27 08:13:18 +0200

    added professional resolver for professional admin side.

--- src/app/modules/user/admin/admin-routing.module.ts
+++ src/app/modules/user/admin/admin-routing.module.ts
@@ -42,6 +42,7 @@ import { RequestResolver } from '../../../resolvers/request.resolver';
 import { ProfessionalResolver } from '../../../resolvers/professional.resolver';
 import { TagsSectorResolver } from '../../../resolvers/tags-sector-resolver';
 import { ProjectsResolver } from '../../../resolvers/admin/projects-resolver';
+import { ProfessionalsResolver } from '../../../resolvers/admin/professionals-resolver';
 
 
 const adminRoutes: Routes = [
@@ -64,7 +65,13 @@ const adminRoutes: Routes = [
       {
         path: 'professionals',
         children: [
-          { path: '', component: AdminProfessionalsComponent, pathMatch: 'full' }
+          {
+            path: '',
+            component: AdminProfessionalsComponent,
+            pathMatch: 'full',
+            resolve: { professionals : ProfessionalsResolver },
+            runGuardsAndResolvers: 'always',
+          }
         ]
       },
       {
