7fbc228494a423d2771c04e4f6c6dce35aaccea1
abhisheksaini <saini.abhishek@hotmail.com>
2019-07-01 11:52:56 +0200

    remove unused module.

--- src/app/modules/user/admin/components/admin-libraries/admin-presets/admin-presets.module.ts
+++ src/app/modules/user/admin/components/admin-libraries/admin-presets/admin-presets.module.ts
@@ -1,17 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { TranslateModule } from '@ngx-translate/core';
 
 import { AdminPresetsEditComponent } from './admin-presets-edit/admin-presets-edit.component';
 import { AdminPresetsListComponent } from './admin-presets-list/admin-presets-list.component';
 
-import { SharedFilterInputModule } from '../../../../../shared/components/shared-filter-input/shared-filter-input.module';
-import { SharedSortModule} from '../../../../../shared/components/shared-sort/shared-sort.module';
-import { SharedLoaderModule } from '../../../../../shared/components/shared-loader/shared-loader.module';
 import { SharedPresetModule } from '../../../../../shared/components/shared-preset/shared-preset.module';
-import { PipeModule } from '../../../../../../pipe/pipe.module';
 import { AdminProjectQuestionnaireModule } from '../../admin-project/admin-project-questionnaire/admin-project-questionnaire.module';
 import { ModalModule } from '../../../../../utility-components/modals/modal/modal.module';
 import { ErrorTemplate1Module } from '../../../../../utility-components/errors/error-template-1/error-template-1.module';
@@ -22,13 +18,8 @@ import { TableModule } from '../../../../../table/table.module';
   imports: [
     CommonModule,
     TranslateModule.forChild(),
-    SharedFilterInputModule,
-    SharedSortModule,
     TranslateModule.forChild(),
-    SharedLoaderModule,
-    PipeModule,
     RouterModule,
-    FormsModule,
     ReactiveFormsModule,
     AdminProjectQuestionnaireModule,
     SharedPresetModule,
