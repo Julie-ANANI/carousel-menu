6ec53b9805190e151528c068114b1ede9f2a9041
abhisheksaini <saini.abhishek@hotmail.com>
2019-07-01 11:44:26 +0200

    delete admin-presets-new component.

--- src/app/modules/user/admin/components/admin-libraries/admin-presets/admin-presets.module.ts
+++ src/app/modules/user/admin/components/admin-libraries/admin-presets/admin-presets.module.ts
@@ -6,7 +6,6 @@ import { TranslateModule } from '@ngx-translate/core';
 
 import { AdminPresetsEditComponent } from './admin-presets-edit/admin-presets-edit.component';
 import { AdminPresetsListComponent } from './admin-presets-list/admin-presets-list.component';
-import { AdminPresetsNewComponent } from './admin-presets-new/admin-presets-new.component';
 
 import { SharedFilterInputModule } from '../../../../../shared/components/shared-filter-input/shared-filter-input.module';
 import { SharedSortModule} from '../../../../../shared/components/shared-sort/shared-sort.module';
@@ -14,7 +13,6 @@ import { SharedLoaderModule } from '../../../../../shared/components/shared-load
 import { SharedPresetModule } from '../../../../../shared/components/shared-preset/shared-preset.module';
 import { PipeModule } from '../../../../../../pipe/pipe.module';
 import { AdminProjectQuestionnaireModule } from '../../admin-project/admin-project-questionnaire/admin-project-questionnaire.module';
-import { PaginationTemplate1Module } from '../../../../../utility-components/paginations/pagination-template-1/pagination-template-1.module';
 import { ModalModule } from '../../../../../utility-components/modals/modal/modal.module';
 import { ErrorTemplate1Module } from '../../../../../utility-components/errors/error-template-1/error-template-1.module';
 import { MessageTemplate2Module } from '../../../../../utility-components/messages/message-template-2/message-template-2.module';
@@ -34,7 +32,6 @@ import { TableModule } from '../../../../../table/table.module';
     ReactiveFormsModule,
     AdminProjectQuestionnaireModule,
     SharedPresetModule,
-    PaginationTemplate1Module,
     ModalModule,
     ErrorTemplate1Module,
     MessageTemplate2Module,
@@ -42,8 +39,7 @@ import { TableModule } from '../../../../../table/table.module';
   ],
   declarations: [
     AdminPresetsEditComponent,
-    AdminPresetsListComponent,
-    AdminPresetsNewComponent
+    AdminPresetsListComponent
   ]
 })
 
