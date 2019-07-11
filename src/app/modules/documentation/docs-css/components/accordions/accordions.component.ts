d28b087d9d52f5cc73627ac432bb5aa72fe3a90b
abhisheksaini <saini.abhishek@hotmail.com>
2019-07-11 12:25:26 +0200

    docs-css: overview and accordions component

---
+++ src/app/modules/documentation/docs-css/components/accordions/accordions.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-accordions',
+  templateUrl: './accordions.component.html'
+})
+
+export class AccordionsComponent{
+
+  constructor() {
+  }
+
+}
+
