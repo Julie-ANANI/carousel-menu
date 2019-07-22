dae49d6a24f16d3a07e0eb4eb3cc5475f107227a
marcvincenti <mvincenti@umi.us>
2019-07-22 18:00:21 +0200

    update login/signup wording

--- src/app/modules/common/signup/i18n/en.ts
+++ src/app/modules/common/signup/i18n/en.ts
@@ -1,14 +1,14 @@
 export const SIGN_UP = {
-  'HEADING': 'Sign Up',
+  'HEADING': 'Create an account',
 
-  'HEADING_UMI':'Ready to launch a market test?',
+  'HEADING_UMI': 'Ready to launch a market test?',
 
   'HAVE_ACCOUNT': 'Already have an account?',
 
-  'SIGN_UP_LINKEDIN': 'Connect with LinkedIn',
+  'SIGN_UP_LINKEDIN': 'Sign Up with LinkedIn',
 
   'SIGN_UP_EMAIL': 'Sign Up with Email Address',
 
-  'BUTTON': 'Sign In',
+  'BUTTON': 'Create an account',
 
 };
