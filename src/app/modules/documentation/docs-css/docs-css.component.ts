15f8ec65530fc8c1a008c7a1205c3a52794f6f19
abhisheksaini <saini.abhishek@hotmail.com>
2019-08-19 12:28:53 +0200

    updated the menu docs and also css.

--- src/app/modules/documentation/docs-css/docs-css.component.ts
+++ src/app/modules/documentation/docs-css/docs-css.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
+import { TranslateTitleService } from '../../../services/title/title.service';
 
 @Component({
   selector: 'app-docs-css',
@@ -11,7 +12,10 @@ export class DocsCssComponent {
 
   private _activeRoute: string;
 
-  constructor(private _router: Router) {
+  constructor(private _router: Router,
+              private _translateTitleService: TranslateTitleService) {
+
+    this._translateTitleService.setTitle('UMI | Documentation');
 
     this._router.events.subscribe((event) => {
       if (event instanceof NavigationEnd) {
