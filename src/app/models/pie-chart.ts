acf437c56a3429f1768938bb062b6c41066f4546
marcvincenti <mvincenti@umi.us>
2019-09-18 10:55:09 +0200

    DRY

--- src/app/models/pie-chart.ts
+++ src/app/models/pie-chart.ts
@@ -1,7 +1,7 @@
 export interface PieChart {
   data?: Array<number>;
   colors?: Array<string>;
-  labels: {[prop: string]: Array<string>};
+  labels: {[lang: string]: Array<string>};
   percentage?: number;
   labelPercentage?: Array<string>;
 }
