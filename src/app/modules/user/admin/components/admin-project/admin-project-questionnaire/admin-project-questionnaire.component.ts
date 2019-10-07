e70bc66d6b420297de58ebc321080f3bfb406075
abhisheksaini <saini.abhishek@hotmail.com>
2019-10-07 09:55:34 +0200

    not visible for prod for the moment. Templates questionnaire.

--- src/app/modules/user/admin/components/admin-project/admin-project-questionnaire/admin-project-questionnaire.component.ts
+++ src/app/modules/user/admin/components/admin-project/admin-project-questionnaire/admin-project-questionnaire.component.ts
@@ -62,6 +62,10 @@ export class AdminProjectQuestionnaireComponent {
     this.saveInnovation();
   }
 
+  get isVisible(): boolean {
+    return !environment.production;
+  }
+
   get innovation() {
     return this._innovation;
   }
