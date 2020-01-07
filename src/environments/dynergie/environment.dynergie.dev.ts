c3c0c06a125b7d2d1d26bb3884a948a0adbdff8f
juandavidcruzgomez <jdcruz-gomez@unitedmotionideas.com>
2020-01-07 17:00:27 +0100

    logo dynergie

--- src/environments/dynergie/environment.dynergie.dev.ts
+++ src/environments/dynergie/environment.dynergie.dev.ts
@@ -8,8 +8,9 @@ export const environment = {
   domain: 'dynergie',
   quizUrl: 'https://quizdev.umi.us',
   companyURL: 'www.dynergie.eu',
-  logoURL: 'https://res.cloudinary.com/umi/image/upload/v1513701711/app/logo-dynergie.png',
+  logoURL: 'https://res.cloudinary.com/umi/image/upload/app/logo-dynergie.png',
   background: 'https://res.cloudinary.com/umi/image/upload/v1524662280/app/default-images/sign-background.jpg',
+  logoSynthURL: 'https://res.cloudinary.com/umi/image/upload/app/logo-dynergie.png',
   commercialContact: 'contact@dynergie.eu',
   secureCookie: true,
   cookieTime: 48 * 3600 * 1000
