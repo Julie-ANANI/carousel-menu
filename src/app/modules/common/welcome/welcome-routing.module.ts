86d9a94c2d90fab39b99f143042347c8835e26a1
abhisheksaini <saini.abhishek@hotmail.com>
2020-01-07 11:16:59 +0100

    Welcome page

--- src/app/modules/common/welcome/welcome-routing.module.ts
+++ src/app/modules/common/welcome/welcome-routing.module.ts
@@ -1,18 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-// Components
 import { WelcomeComponent } from './welcome.component';
 
 const welcomeRoutes: Routes = [
-  { path: '', component: WelcomeComponent, pathMatch: 'full' }
+  {
+    path: '',
+    component: WelcomeComponent,
+    pathMatch: 'full'
+  }
 ];
 
 @NgModule({
   imports: [
     RouterModule.forChild(welcomeRoutes)
-  ],
-  providers: [
   ],
   exports: [
     RouterModule
