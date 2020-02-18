5975289b088df05bc05886e1e55bd14dcaf0a804
abhisheksaini <saini.abhishek@hotmail.com>
2020-02-18 15:50:34 +0100

    section model.

--- src/app/models/executive-report.ts
+++ src/app/models/executive-report.ts
@@ -1,3 +1,20 @@
+export interface ExecutiveReport {
+  readonly _id: string;
+  readonly completion: number
+  lang: 'en' | 'fr';
+  summary: string;
+  umiCommercial: string
+  owner: ExecutiveOwner;
+  objective: string;
+  targeting: ExecutiveTargeting;
+  professionals: ExecutiveProfessional;
+  sections: Array<ExecutiveSection>;
+  conclusion: string;
+  umiOperator: string;
+  //created: Date;
+  //updated: Date;
+}
+
 export interface ExecutiveOwner {
   name: string;
   email: string;
@@ -24,18 +41,51 @@ export interface ExecutiveConclusion {
   conclusion: string
 }
 
-export interface ExecutiveReport {
-  readonly _id: string;
-  readonly completion: number
-  lang: 'en' | 'fr';
-  summary: string;
-  umiCommercial: string
-  owner: ExecutiveOwner;
-  objective: string;
-  targeting: ExecutiveTargeting;
-  professionals: ExecutiveProfessional;
-  conclusion: string;
-  umiOperator: string;
-  //created: Date;
-  //updated: Date;
+export interface ExecutiveSection {
+  questionId: string;
+  label: string;
+  abstract: string;
+  questionType: 'PIE' | 'RANKING' | 'BAR' | 'QUOTE' | 'KPI';
+  content: SectionPie | SectionRanking | SectionBar | SectionQuote | SectionKpi;
+}
+
+export interface SectionKpi {
+  kpi: string;
+  legend: string;
+  examples: Array<string>;
+}
+
+export interface SectionQuote {
+  showQuotes: boolean;
+  quote: string;
+  contributor: {
+    name: string;
+    jobTitle: string;
+  };
+}
+
+export interface SectionBar {
+  showExamples: boolean;
+  values: Array< {
+    legend: string;
+    value: string;
+    example: Array<string>;
+  }>;
+}
+
+export interface SectionPie {
+  showPositive: boolean;
+  values: Array< {
+    p_index: string;
+    value: string;
+    label: string;
+  }>;
+}
+
+export interface SectionRanking {
+  values: Array< {
+    legend: string;
+    value: string;
+    color: string;
+  }>;
 }
