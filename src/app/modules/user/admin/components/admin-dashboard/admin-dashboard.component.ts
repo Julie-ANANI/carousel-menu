81ad1442d6b6a455bdb0b6d420222864faeef675
abhisheksaini <saini.abhishek@hotmail.com>
2020-04-21 11:22:32 +0200

    fixed the bug of batches name.

--- src/app/modules/user/admin/components/admin-dashboard/admin-dashboard.component.ts
+++ src/app/modules/user/admin/components/admin-dashboard/admin-dashboard.component.ts
@@ -2,15 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { TranslateTitleService } from '../../../../../services/title/title.service';
 import { DashboardService } from '../../../../../services/dashboard/dashboard.service';
 import { AuthService } from '../../../../../services/auth/auth.service';
-import { SearchService } from '../../../../../services/search/search.service';
 import { User } from '../../../../../models/user.model';
 import { Subject } from 'rxjs';
-import { InnovationService } from '../../../../../services/innovation/innovation.service';
-import { InnovCard } from '../../../../../models/innov-card';
-import { TranslateService } from '@ngx-translate/core';
-import { SidebarInterface } from '../../../../sidebars/interfaces/sidebar-interface';
-import { first } from 'rxjs/operators';
-
 
 @Component({
   selector: 'app-admin-dashboard',
@@ -26,19 +19,6 @@ export class AdminDashboardComponent implements OnInit {
 
   public operatorId = '';
 
-  private _dateNow = new Date();
-
-  public nbDaysOfStats = 1;
-
-  private _sidebarTemplateValue: SidebarInterface = {};
-
-  private _selectedInnovation: InnovCard;
-
-  private _selectedBatch: any;
-
-  private _weekBatches: Array<any> = [[], [], [], [], []];
-  // => [['DATE', batch, batch,..]...]
-
   operatorData: {
     nbProjectsToValidate: number,
     nbProjectsToTreat: number
@@ -47,26 +27,11 @@ export class AdminDashboardComponent implements OnInit {
     nbProjectsToTreat: null
   };
 
-  statistics: {
-    percentFoundPros: number | 'NA',
-    percentFoundEmails: number | 'NA',
-    percentOkEmails: number | 'NA',
-    percentReceivedEmails: number | 'NA'
-  } = {
-    percentFoundPros: null,
-    percentFoundEmails: null,
-    percentOkEmails: null,
-    percentReceivedEmails: null
-  };
-
   private _refreshNeededEmitter = new Subject<any>();
 
   constructor(private translateTitleService: TranslateTitleService,
               private dashboardService: DashboardService,
-              private searchService: SearchService,
-              private authService: AuthService,
-              private innovationService: InnovationService,
-              private translateService: TranslateService) { }
+              private authService: AuthService) { }
 
   ngOnInit() {
 
@@ -93,10 +58,6 @@ export class AdminDashboardComponent implements OnInit {
         .subscribe((operatorData: any) => this.operatorData = operatorData);
     }
 
-    this.getPeriodStats();
-
-    this.getWeek();
-
     this._spinnerDisplay = false;
 
   }
@@ -110,98 +71,6 @@ export class AdminDashboardComponent implements OnInit {
       .subscribe((operatorData: any) => this.operatorData = operatorData);
   }
 
-  public getPeriodStats() {
-    this.searchService
-      .getEmailStats(this.nbDaysOfStats)
-      .subscribe((stats: any) => {
-        const totalMails = stats.total && stats.total.domainNotFound && stats.total.found && stats.total.timeOut ? stats.total.domainNotFound + stats.total.found + stats.total.notFound + stats.total.timeOut : 'NA';
-        this.statistics.percentFoundEmails = totalMails && stats && stats.total && stats.total.found ? Math.round(stats.total.found / totalMails * 100) : 'NA';
-        this.statistics.percentFoundPros = 'NA';
-        this.statistics.percentOkEmails = stats && stats.total && stats.total.found && stats.total.confidence ? Math.round((stats.total.confidence['100'] || 0) / stats.total.found * 100) : 'NA';
-        this.statistics.percentReceivedEmails = 'NA';
-      });
-  }
-
-  getWeek() {
-    const now = Date.now();
-    this._dateNow = new Date(now);
-    this.dashboardService.getNextDateSend(this._dateNow.toString()).subscribe( (batches: Array<any>) => {
-        this._weekBatches = batches;
-    });
-  }
-
-  onClickLast() {
-    this._dateNow.setDate(this._dateNow.getDate() - 7);
-    this.dashboardService.getNextDateSend(this._dateNow.toString()).subscribe((batches: Array<any>) => {
-      this._weekBatches = batches;
-    });
-  }
-
-  onClickNext() {
-    this._dateNow.setDate(this._dateNow.getDate() + 7);
-    this.dashboardService.getNextDateSend(this._dateNow.toString()).subscribe((batches: Array<any>) => {
-      this._weekBatches = batches;
-    });
-  }
-
-  getState(b: any) {
-    const day = this._dateNow.getDay();
-    let Now = new Date(this._dateNow);
-    const beginWeek = new Date(Now.setDate(Now.getDate() - day));
-    Now = new Date(this._dateNow);
-    const endWeek = new Date(Now.setDate(Now.getDate() - day + 6));
-    const FM = new Date(b.firstMail);
-    const SM = new Date(b.secondMail);
-    const TM = new Date(b.thirdMail);
-    if ((beginWeek < FM) && (FM < endWeek)) {
-      return 0;
-    }
-    if ((beginWeek < SM) && (SM < endWeek)) {
-      return 1;
-    }
-    if ((beginWeek < TM) && (TM < endWeek)) {
-      return 2;
-    }
-  }
-
-  getDateString(d: any): string {
-    let result = '';
-    let day = d.split('/')[0];
-    let month = d.split('/')[1];
-
-    day = ('0' + day).slice(-2);
-
-    month = Number(month);
-    month ++;
-    month = month.toString();
-    month = ('0' + month).slice(-2);
-
-    if (this.translateService.currentLang === 'fr') {
-      result = day + '/' + month;
-    } else {
-      result = month + '/' + day;
-    }
-
-    return result;
-
-  }
-
-  showPreview(event: Event, batch: any) {
-    event.preventDefault();
-    this._selectedBatch = batch;
-    if (this._selectedBatch !== null) {
-      this.innovationService.getInnovationCard(this._selectedBatch.innovation.innovationCards[0].id).pipe(first()).subscribe((card: any) => {
-        this._selectedInnovation = card;
-        this._sidebarTemplateValue = {
-          animate_state: 'active',
-          title: 'SIDEBAR.TITLE.PREVIEW',
-          size: '726px'
-        };
-      });
-    }
-
-  }
-
   get refreshNeededEmitter(): Subject<any> {
     return this._refreshNeededEmitter;
   }
@@ -210,32 +79,8 @@ export class AdminDashboardComponent implements OnInit {
     return this.authService.adminLevel;
   }
 
-
-  get weekBatches(): any {
-    return this._weekBatches;
-  }
-
-  get selectedInnovation(): any {
-    return this._selectedInnovation;
-  }
-  get selectedBatch(): any {
-    return this._selectedBatch;
-  }
-
-  get dateNow(): any {
-    return this._dateNow;
-  }
-
   get spinnerDisplay(): boolean {
     return this._spinnerDisplay;
-  }
-
-  get sidebarTemplateValue(): SidebarInterface {
-    return this._sidebarTemplateValue;
-  }
-
-  set sidebarTemplateValue(value: SidebarInterface) {
-    this._sidebarTemplateValue = value;
   }
 
 }
--- src/app/modules/user/admin/components/admin-dashboard/admin-dashboard.module.ts
+++ src/app/modules/user/admin/components/admin-dashboard/admin-dashboard.module.ts
@@ -2,21 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TranslateModule } from '@ngx-translate/core';
-import { TableModule } from '../../../../table/table.module';
 import { PipeModule } from '../../../../../pipe/pipe.module';
-import { SidebarModule } from '../../../../sidebars/templates/sidebar/sidebar.module';
 import { AdminDashboardComponent } from './admin-dashboard.component';
 import { AdminMissionsListModule } from '../admin-missions-list/admin-missions-list.module';
-import { SidebarInnovationPreviewModule } from '../../../../sidebars/components/sidebar-innovation-preview/sidebar-innovation-preview.module';
 import { SharedLoaderModule } from '../../../../shared/components/shared-loader/shared-loader.module';
 
 @NgModule({
   imports: [
     CommonModule,
     ReactiveFormsModule,
-    SidebarModule,
-    SidebarInnovationPreviewModule,
-    TableModule,
     TranslateModule.forChild(),
     PipeModule,
     AdminMissionsListModule,
