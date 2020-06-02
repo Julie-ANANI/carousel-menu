941a25776a4226913d50adec5a1651b4d88746b1
juandavidcruzgomez <jdcruz-gomez@umi.us>
2020-06-02 15:09:21 +0200

    Market tests by default and remove dashboard de la liste

--- src/app/modules/common/header/header.component.ts
+++ src/app/modules/common/header/header.component.ts
@@ -60,11 +60,11 @@ export class HeaderComponent {
   private _showLangs: boolean = false;
 
   private _adminRoutes: Array<Header> = [
-    { pageName: 'Dashboard', pageLink: '/user/admin', adminLevel: 1 },
+    // { pageName: 'Dashboard', pageLink: '/user/admin', adminLevel: 1 },
+    { pageName: 'Market Tests', pageLink: '/user/admin/projects', adminLevel: 1 },
     { pageName: 'Users', pageLink: '/user/admin/users', adminLevel: 1 },
     { pageName: 'Community', pageLink: '/user/admin/community', adminLevel: 3 },
     { pageName: 'Professionals', pageLink: '/user/admin/professionals', adminLevel: 3 },
-    { pageName: 'Market Tests', pageLink: '/user/admin/projects', adminLevel: 1 },
     { pageName: 'Libraries', pageLink: '/user/admin/libraries', adminLevel: 3 },
     { pageName: 'Monitoring', pageLink: '/user/admin/monitoring', adminLevel: 3 },
     { pageName: 'Settings', pageLink: '/user/admin/settings', adminLevel: 3 },
