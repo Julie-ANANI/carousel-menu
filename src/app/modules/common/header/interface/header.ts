365536121c0953974a7df23708126dd043b2c2ab
abhisheksaini <saini.abhishek@hotmail.com>
2020-07-02 16:58:39 +0200

    header improvement

--- src/app/modules/common/header/interface/header.ts
+++
@@ -1,6 +0,0 @@
-export interface Header {
-  pageName?: string;
-  pageLink?: string;
-  trackingClass?: string;
-  adminLevel?: number;
-}
--- src/app/services/user/user-front.service.ts
+++ src/app/services/user/user-front.service.ts
@@ -31,4 +31,21 @@ export class UserFrontService {
     return user && user.company && user.company.logo || ''
   }
 
+  /***
+   * this function is to get the initial of the user.
+   * @param user | Contributor
+   */
+  public static initial(user: User): string {
+    if (user) {
+     if (user.firstName && user.lastName) {
+        return `${user.firstName.slice(0, 1)}${user.lastName.slice(0, 1)}`;
+      } else if (user.firstName) {
+        return `${user.firstName.slice(0, 2)}`;
+      } else if (user.lastName) {
+        return `${user.lastName.slice(0, 2)}`;
+      }
+    }
+    return '';
+  }
+
 }
