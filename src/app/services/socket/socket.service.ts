c9c95e2a9821c587aeef985fe409cf13dc820f2a
bastienscanu <bscanu@umi.us>
2020-10-27 13:56:29 +0100

    debug socket.io ?!

--- src/app/services/socket/socket.service.ts
+++ src/app/services/socket/socket.service.ts
@@ -13,7 +13,7 @@ export class SocketService {
   socket: SocketIOClient.Socket;
 
   constructor() {
-    this.socket = io(environment.apiUrl.replace('/api', ''), {path: `${environment.apiUrl}/socket-io`});
+    this.socket = io(environment.apiUrl.replace('/api', ''), {path: '/api/socket-io'});
   }
 
   listenToSocket(): Observable<any> {
