784f0eca279532fa6b5c7ddab25d98df1c7d73d6
abhisheksaini <saini.abhishek@hotmail.com>
2020-10-28 17:19:11 +0100

    revert description editor.

--- src/app/modules/user/admin/components/admin-project/admin-project-description/admin-project-description.module.ts
+++ src/app/modules/user/admin/components/admin-project/admin-project-description/admin-project-description.module.ts
@@ -8,7 +8,6 @@ import {SharedEditorTinymceModule} from '../../../../../shared/components/shared
 import {ModalEmptyModule} from '../../../../../utility/modals/modal-empty/modal-empty.module';
 import {SharedUploadZonePhotoModule} from '../../../../../shared/components/shared-upload-zone-photo/shared-upload-zone-photo.module';
 import {SharedUploadZoneVideoModule} from '../../../../../shared/components/shared-upload-zone-video/shared-upload-zone-video.module';
-import {SharedEditorsModule} from '../../../../../shared/components/shared-editors/shared-editors.module';
 
 @NgModule({
   imports: [
@@ -18,8 +17,7 @@ import {SharedEditorsModule} from '../../../../../shared/components/shared-edito
     SharedEditorTinymceModule,
     ModalEmptyModule,
     SharedUploadZonePhotoModule,
-    SharedUploadZoneVideoModule,
-    SharedEditorsModule
+    SharedUploadZoneVideoModule
   ],
   declarations: [
     AdminProjectDescriptionComponent,
