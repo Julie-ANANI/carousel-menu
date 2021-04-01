694f612cf1e91b6732fc474253df89f69194760f
SAINIAbhishek <saini.abhishek@hotmail.com>
2021-04-01 15:22:56 +0200

    will implement in the back.

--- src/app/services/community/community.service.ts
+++ src/app/services/community/community.service.ts
@@ -2,9 +2,6 @@ import { Injectable } from '@angular/core';
 import {Observable} from 'rxjs';
 import {CommunityCircle} from '../../models/community';
 import {HttpClient} from '@angular/common/http';
-import {environment} from '../../../environments/environment';
-
-const COMMUNITY_API = environment.communityApiUrl;
 
 @Injectable({providedIn: 'root'})
 export class CommunityService {
@@ -12,7 +9,7 @@ export class CommunityService {
   constructor(private _http: HttpClient) { }
 
   public getAllCircles(): Observable<Array<CommunityCircle>> {
-    return this._http.get<Array<CommunityCircle>>(`${COMMUNITY_API}/circle/?fields=name`);
+    return this._http.get<Array<CommunityCircle>>(`/community/circle`);
   }
 
 }
