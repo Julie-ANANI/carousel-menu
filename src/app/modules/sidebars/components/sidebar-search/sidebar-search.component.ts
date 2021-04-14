b5a5c7d4b91a6eed02cc9943e3b05dcfa9c83c41
WeiWANG0118 <wwang@umi.us>
2021-04-14 10:13:27 +0200

    collapse-advanced-setting: campaign-search, hide Index and Viadeo options which are not used

--- src/app/modules/sidebars/components/sidebar-search/sidebar-search.component.ts
+++ src/app/modules/sidebars/components/sidebar-search/sidebar-search.component.ts
@@ -1,25 +1,25 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { GeographySettings } from "../../../../models/innov-settings";
+import { GeographySettings } from '../../../../models/innov-settings';
 
 @Component({
   selector: 'app-sidebar-search',
   templateUrl: './sidebar-search.component.html',
-  styleUrls: ['./sidebar-search.component.scss']
+  styleUrls: ['./sidebar-search.component.scss'],
 })
-
 export class SidebarSearchComponent {
-
   @Input() isEditable = false;
 
   @Input() params: any = null;
 
   @Input() geography: GeographySettings = <GeographySettings>{};
 
-  @Output() paramsChange = new EventEmitter <any>();
+  @Output() paramsChange = new EventEmitter<any>();
 
-  @Output() geographyChange = new EventEmitter <any>();
+  @Output() geographyChange = new EventEmitter<any>();
 
   private _prosCount: Array<string> = ['10', '20', '30', '40', '50', '100'];
+
+  private _showToggleSearch = false;
 
   constructor() {}
 
@@ -35,5 +35,11 @@ export class SidebarSearchComponent {
     return this._prosCount;
   }
 
-}
+  get showToggleSearch(): boolean {
+    return this._showToggleSearch;
+  }
 
+  onClickToggle() {
+    this._showToggleSearch = !this._showToggleSearch;
+  }
+}
