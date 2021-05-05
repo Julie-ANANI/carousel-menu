8cfe2a27332e7346b16ff4d2a976a6149ee759f9
leachemoul <lea_chemoul@orange.fr>
2021-05-05 15:45:54 +0200

    :fr: Countries handle translations

--- src/app/modules/shared/components/shared-world-list/shared-world-list.module.ts
+++ src/app/modules/shared/components/shared-world-list/shared-world-list.module.ts
@@ -3,16 +3,18 @@ import { CommonModule } from '@angular/common';
 import { SharedWorldListComponent } from './shared-world-list.component';
 import {TranslateModule} from '@ngx-translate/core';
 import {SharedToggleListModule} from '../shared-toggle-list/shared-toggle-list.module';
+import {PipeModule} from '../../../../pipe/pipe.module';
 
 @NgModule({
   declarations: [SharedWorldListComponent],
   exports: [
     SharedWorldListComponent
   ],
-  imports: [
-    CommonModule,
-    TranslateModule,
-    SharedToggleListModule
-  ]
+    imports: [
+        CommonModule,
+        TranslateModule,
+        SharedToggleListModule,
+        PipeModule
+    ]
 })
 export class SharedWorldListModule { }
