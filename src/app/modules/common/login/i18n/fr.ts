684a8c7278f65a2ddb5931df463b96d7b6482539
SAINIAbhishek <saini.abhishek@hotmail.com>
2021-05-12 17:55:51 +0200

    🔥 re-design login page.

--- src/app/modules/common/login/i18n/fr.ts
+++ src/app/modules/common/login/i18n/fr.ts
@@ -3,16 +3,11 @@ export const LOG_IN = {
 
   'DOES_NOT_HAVE_ACCOUNT': 'Vous n\'avez pas de compte ?',
 
-  'LOG_IN_BTN': 'Connexion',
+  'LOG_IN_BTN': 'Se connecter',
 
   'HEADING': 'Bienvenue !',
 
   'LOG_IN_LINKEDIN': 'Se connecter avec LinkedIn',
-
-  'FORGOT_PASSWORD': {
-    'A': 'Avez-vous',
-    'B': 'oublié votre mot de passe ?'
-  },
 
   'SIGN_UP': 'Inscription'
 
--- src/environments/umi/environment.demo.ts
+++ src/environments/umi/environment.demo.ts
@@ -11,7 +11,7 @@ export const environment = {
   companyURL: 'https://www.umi.us',
   logoSynthURL: 'https://res.cloudinary.com/umi/image/upload/app/default-images/company-logo/logo-umi-trans-2020.png',
   logoURL: 'https://res.cloudinary.com/umi/image/upload/app/default-images/company-logo/logo-umi-animated-2020.gif',
-  background: 'https://res.cloudinary.com/umi/image/upload/c_mfit,w_1920/v1527583034/app/default-images/cover-image.jpg',
+  background: 'https://res.cloudinary.com/umi/image/upload/f_auto/app/default-images/backgrounds/umi-welcome-picture-2021.jpg',
   commercialContact: 'klegrand@umi.us',
   secureCookie: true,
   cookieTime: 3600 * 1000
