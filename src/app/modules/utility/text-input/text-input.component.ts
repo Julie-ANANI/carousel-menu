d84b86ca757186ed1a7d99c0fe0deb76bf314150
SAINIAbhishek <saini.abhishek@hotmail.com>
2021-05-21 14:44:55 +0200

    possibility of different field type.

--- src/app/modules/utility/text-input/text-input.component.ts
+++ src/app/modules/utility/text-input/text-input.component.ts
@@ -16,38 +16,11 @@ import {emailRegEx} from '../../../utils/regex';
 })
 export class TextInputComponent {
 
-  /**
-   * defines the different html styles.
-   * TYPE_1: has circle button on the input field. ex: Admin project settings component (Modal Publish to Community)
-   * TYPE_2: has normal button attached next to the input filed. Client new project last step component.
-   */
-  @Input() template: 'TYPE_1' | 'TYPE_2' = 'TYPE_1';
-
-  /**
-   * added text list.
-   * @param value
-   */
   @Input() set addedText(value: Array<string>) {
     this._addedText = value;
   }
 
   @Input() placeholder = 'COMMON.PLACEHOLDER.INPUT_LIST_DEFAULT';
-
-  /**
-   * top value of the btn add for the template TYPE_1.
-   * sometimes it's not in center from the top container of input.
-   */
-  @Input() btnAddTopValue = '';
-
-  /**
-   * input field text size.
-   */
-  @Input() inputTextSize = '16px';
-
-  /***
-   * true to make input field and button in center.
-   */
-  @Input() isFieldCenter = false;
 
   /**
    * error text color: text-white | text-primary ....
@@ -63,6 +36,11 @@ export class TextInputComponent {
    * to make the form field small.
    */
   @Input() isFormSmall = false;
+
+  /**
+   * input field text size.
+   */
+  @Input() inputTextSize = '16px';
 
   /**
    * provide the label bg color class.
@@ -88,18 +66,18 @@ export class TextInputComponent {
 
   private _addedText: Array<string> = [];
 
-  private _isEmailError = false;
+  isEmailError = false;
 
   constructor() { }
 
   public onAddValue() {
     if (this._text.value) {
       if ((this.fieldType === 'email' && this._text.value.match(emailRegEx)) || this.fieldType === 'text') {
-        this._isEmailError = false;
+        this.isEmailError = false;
         this.textEntered.emit(this._text.value);
         this._text.reset();
       } else if (this.fieldType === 'email' && !this._text.value.match(emailRegEx)) {
-        this._isEmailError = true;
+        this.isEmailError = true;
       }
     }
   }
@@ -115,10 +93,6 @@ export class TextInputComponent {
 
   get addedText(): Array<string> {
     return this._addedText;
-  }
-
-  get isEmailError(): boolean {
-    return this._isEmailError;
   }
 
 }
