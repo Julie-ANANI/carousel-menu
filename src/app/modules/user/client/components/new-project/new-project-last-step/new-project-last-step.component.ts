340adfc0acd4c5b151c8665bdb7152f801897e2f
SAINIAbhishek <saini.abhishek@hotmail.com>
2021-05-21 17:32:44 +0200

    😩 left with the date picker component.

--- src/app/modules/user/client/components/new-project/new-project-last-step/new-project-last-step.component.ts
+++ src/app/modules/user/client/components/new-project/new-project-last-step/new-project-last-step.component.ts
@@ -1,8 +1,6 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {TranslateService} from '@ngx-translate/core';
 import {Consent} from '../../../../../../models/innovation';
-import {CommonService} from '../../../../../../services/common/common.service';
-import {IMyDateModel} from 'angular-mydatepicker';
 
 @Component({
   selector: 'app-new-project-last-step',
@@ -25,8 +23,6 @@ export class NewProjectLastStepComponent {
 
   @Output() collaboratorsChange: EventEmitter<Array<string>> = new EventEmitter<Array<string>>();
 
-  @Output() restitutionDateChange: EventEmitter<Date> = new EventEmitter<Date>();
-
   private _projectLangs: Array<string> = ['en', 'fr'];
 
   private _projectLang = this.currentLang;
@@ -39,16 +35,8 @@ export class NewProjectLastStepComponent {
 
   private _collaborators: Array<string> = [];
 
-  private _restitutionDate = this._commonService.getFutureMonth();
+  constructor(private _translateService: TranslateService) { }
 
-  constructor(private _translateService: TranslateService,
-              private _commonService: CommonService) { }
-
-  /**
-   * when user select the lang of the project.
-   * @param event
-   * @param value
-   */
   public onChangeProjectLang(event: Event, value: string) {
     event.preventDefault();
     if (this._projectLang !== value) {
@@ -56,28 +44,16 @@ export class NewProjectLastStepComponent {
     }
   }
 
-  /**
-   * when the user writes the comment in the textarea of the restitution date.
-   * @param value
-   */
   public onChangeMilestoneComment(value: string) {
     this._milestoneDateComment = value;
     this.milestoneDateCommentChange.emit(this._milestoneDateComment);
   }
 
-  /**
-   * when the user enter the title of the project
-   * @param value
-   */
   public onChangeProject(value: string) {
     this._title = value;
     this.projectNameChange.emit(this._title);
   }
 
-  /**
-   * when user toggles the checkbox of the collaborators consent.
-   * @param value
-   */
   public consentChange(value: boolean) {
     this._consent = value;
     this.collaboratorsConsent.emit({
@@ -86,10 +62,6 @@ export class NewProjectLastStepComponent {
     });
   }
 
-  /**
-   * email to add as collaborator.
-   * @param value
-   */
   public addEmail(value: string) {
     const find = this._collaborators.find((email) => email === value);
     if (!find) {
@@ -98,24 +70,9 @@ export class NewProjectLastStepComponent {
     }
   }
 
-  /**
-   * click on the cross btn of the collaborators list to remove it.
-   * @param value
-   */
   public removeEmail(value: string) {
     this._collaborators = this._collaborators.filter((email) => email !== value);
     this._emitCollaborators();
-  }
-
-  /**
-   * when user select the date from the date picker.
-   * we emit the selected date as restitution date
-   * @param event
-   */
-  public onChangeRestitutionDate(event: IMyDateModel) {
-    if (event && event.singleDate && event.singleDate.jsDate) {
-      this.restitutionDateChange.emit(event.singleDate.jsDate);
-    }
   }
 
   private _emitCollaborators() {
@@ -148,10 +105,6 @@ export class NewProjectLastStepComponent {
 
   get milestoneDateComment(): string {
     return this._milestoneDateComment;
-  }
-
-  get restitutionDate(): string {
-    return this._restitutionDate;
   }
 
 }
