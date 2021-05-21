af61aeacab56ce4321686b2a1a73ae7a90c50d1a
SAINIAbhishek <saini.abhishek@hotmail.com>
2021-05-21 17:41:06 +0200

    add date picker module.

--- src/app/modules/utility/date-picker/date-picker.module.ts
+++ src/app/modules/utility/date-picker/date-picker.module.ts
@@ -3,15 +3,11 @@ import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
 
 import {DatePickerComponent} from './date-picker.component';
-import {FormsModule} from '@angular/forms';
-import {AngularMyDatePickerModule} from 'angular-mydatepicker';
 
 @NgModule({
   imports: [
     CommonModule,
     TranslateModule.forChild(),
-    FormsModule,
-    AngularMyDatePickerModule,
   ],
   declarations: [
     DatePickerComponent
