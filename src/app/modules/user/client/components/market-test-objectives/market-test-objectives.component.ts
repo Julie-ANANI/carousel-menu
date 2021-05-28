7c037c33ed6b500b636150316afb4c7e3fb6bc51
SAINIAbhishek <saini.abhishek@hotmail.com>
2021-05-28 11:08:17 +0200

    integrate objectives complementary and emit the selected template with all updated value.

--- src/app/modules/user/client/components/market-test-objectives/market-test-objectives.component.ts
+++ src/app/modules/user/client/components/market-test-objectives/market-test-objectives.component.ts
@@ -1,71 +1,21 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {TranslateService} from '@ngx-translate/core';
+import {MissionQuestion, MissionTemplate} from '../../../../../models/mission';
+import {MissionFrontService} from '../../../../../services/mission/mission-front.service';
 
-interface PrincipalObjective {
-  category: string;
-  objective: string;
-}
+/**
+ * example: New project component.
+ */
 
 @Component({
   selector: 'app-market-test-objectives',
   templateUrl: './market-test-objectives.component.html',
   styleUrls: ['./market-test-objectives.component.scss']
 })
-export class MarketTestObjectivesComponent implements OnInit {
-
-  @Input() set objectiveComment(value: string) {
-    this._objectiveComment = value;
-  }
-
-  private _selectedCategory = '';
-
-  private _objectivesCategory: Array<string> = ['INNOVATE', 'INNOVATION'];
-
-  private _selectedPrincipal: PrincipalObjective = <PrincipalObjective>{};
-
-  principalObjectives: Array<PrincipalObjective> = [
-    {category: 'INNOVATE', objective: 'Detecting market needs'},
-    {category: 'INNOVATE', objective: 'Validating market needs'},
-    {category: 'INNOVATE', objective: 'Sourcing solutions / suppliers'},
-    {category: 'INNOVATION', objective: 'Identifying receptive markets'},
-    {category: 'INNOVATION', objective: 'Validating the interest in my project'},
-    {category: 'INNOVATION', objective: 'Optimizing my value proposition'},
-  ];
-
-  @Output() objectiveCommentChange: EventEmitter<string> = new EventEmitter<string>();
-
-  private _objectiveComment = '';
-
-  constructor(private _translateService: TranslateService) { }
-
-  ngOnInit() {
-  }
-
-  public onChangeCategory(event: Event, value: string) {
-    event.preventDefault();
-    if (value !== this._selectedCategory) {
-      this._selectedCategory = value;
-      this._selectedPrincipal = <PrincipalObjective>{};
-    }
-  }
-
-  public onChangePrincipal(event: Event, value: PrincipalObjective) {
-    event.preventDefault();
-    if (this._selectedCategory && this._selectedCategory === value.category) {
-      this._selectedPrincipal = value;
-    }
-  }
+export class MarketTestObjectivesComponent {
 
   get currentLang(): string {
     return this._translateService.currentLang;
-  }
-
-  get objectiveComment(): string {
-    return this._objectiveComment;
-  }
-
-  get selectedPrincipal(): PrincipalObjective {
-    return this._selectedPrincipal;
   }
 
   get selectedCategory(): string {
@@ -74,6 +24,110 @@ export class MarketTestObjectivesComponent implements OnInit {
 
   get objectivesCategory(): Array<string> {
     return this._objectivesCategory;
+  }
+
+  get missionTemplates(): Array<MissionTemplate> {
+    return this._missionTemplates;
+  }
+
+  get selectedTemplate(): MissionTemplate {
+    return this._selectedTemplate;
+  }
+
+  get templateComment(): string {
+    return this._templateComment;
+  }
+
+  set templateComment(value: string) {
+    this._templateComment = value;
+    this._emitTemplate();
+  }
+
+  get selectedObjectivesComplementary(): Array<MissionQuestion> {
+    return this._selectedObjectivesComplementary;
+  }
+
+  /**
+   * actual list of the mission templates we get from the back.
+   * we do not modify it.
+   */
+  @Input() set missionTemplates(value: Array<MissionTemplate>) {
+    this._missionTemplates = value.map((template) => {
+      const enIndex = template.entry.findIndex((entry: any) => entry.lang === 'en');
+      if (enIndex !== -1) {
+        switch (template.entry[enIndex].objective) {
+          case 'Detecting market needs':
+          case 'Validating market needs':
+          case 'Sourcing solutions / suppliers':
+            template['category'] = 'INNOVATE';
+            break;
+
+          case 'Identifying receptive markets':
+          case 'Validating the interest in my project':
+          case 'Optimizing my value proposition':
+            template['category'] = 'INNOVATION';
+            break;
+        }
+      }
+      return template;
+    });
+  }
+
+  /**
+   * emit the selected template with all the updated value in it.
+   */
+  @Output() missionTemplateChange: EventEmitter<MissionTemplate> = new EventEmitter<MissionTemplate>();
+
+  private _selectedCategory = '';
+
+  private _objectivesCategory: Array<string> = ['INNOVATE', 'INNOVATION'];
+
+  private _selectedTemplate: MissionTemplate = <MissionTemplate>{};
+
+  private _missionTemplates: Array<MissionTemplate> = [];
+
+  private _templateComment = '';
+
+  private _selectedObjectivesComplementary: Array<MissionQuestion> = [];
+
+  constructor(private _translateService: TranslateService) { }
+
+  public onChangeCategory(event: Event, value: string) {
+    event.preventDefault();
+    if (value !== this._selectedCategory) {
+      this._selectedCategory = value;
+      this._selectedTemplate = <MissionTemplate>{};
+    }
+  }
+
+  public onChangeTemplate(event: Event, value: MissionTemplate) {
+    event.preventDefault();
+    if (this._selectedCategory && this._selectedCategory === value['category']) {
+      this._selectedTemplate = value;
+      this._emitTemplate();
+    }
+  }
+
+  /**
+   * before emitting it we remove the category attribute because we use it
+   * to enable / disable the template respective to its category.
+   * @private
+   */
+  private _emitTemplate() {
+    const template: MissionTemplate = JSON.parse(JSON.stringify(this._selectedTemplate));
+    delete template['category'];
+    template.complementary = this._selectedObjectivesComplementary;
+    template.comment = this._templateComment;
+    this.missionTemplateChange.emit(template);
+  }
+
+  public onChangeObjectivesComplementary(event: Array<MissionQuestion>) {
+    this._selectedObjectivesComplementary = event;
+    this._emitTemplate();
+  }
+
+  public templateName(template: MissionTemplate, lang = this.currentLang): string {
+    return MissionFrontService.objectiveName(template, lang);
   }
 
 }
