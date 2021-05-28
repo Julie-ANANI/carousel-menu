46ff96ad3f83bcd312168b25e34f5bab7a628713
SAINIAbhishek <saini.abhishek@hotmail.com>
2021-05-28 10:25:21 +0200

    add image in en.

--- src/app/modules/user/client/components/market-test-methodology/market-test-methodology.component.ts
+++ src/app/modules/user/client/components/market-test-methodology/market-test-methodology.component.ts
@@ -16,32 +16,32 @@ export class MarketTestMethodologyComponent {
     switch (value) {
       case 'Detecting market needs':
         this._case = 'DETECTING_MARKET';
-        this._image = 'detecting-market-needs-';
+        this._image = 'detecting-';
         break;
 
       case 'Validating market needs':
         this._case = 'VALIDATING_MARKET';
-        this._image = 'validating-market-needs-';
+        this._image = 'validating-market-';
         break;
 
       case 'Sourcing solutions / suppliers':
         this._case = 'SOURCING_SOLUTIONS';
-        this._image = 'sourcing-solutions-suppliers-';
+        this._image = 'sourcing-';
         break;
 
       case 'Identifying receptive markets':
         this._case = 'IDENTIFYING_RECEPTIVE';
-        this._image = 'identifying-receptive-markets-';
+        this._image = 'identifying-';
         break;
 
       case 'Validating the interest in my project':
         this._case = 'VALIDATING_INTEREST';
-        this._image = 'validating-the-interest-in-my-project-';
+        this._image = 'validating-interest-';
         break;
 
       case 'Optimizing my value proposition':
         this._case = 'OPTIMIZING';
-        this._image = 'optimizing-my-value-proposition-';
+        this._image = 'optimizing-';
         break;
     }
   }
@@ -50,7 +50,7 @@ export class MarketTestMethodologyComponent {
 
   get caseImage(): string {
     if (!!this._image) {
-      return 'https://res.cloudinary.com/umi/image/upload/app/default-images/configurator/' + this._image + 'fr' + '.png';
+      return 'https://res.cloudinary.com/umi/image/upload/app/default-images/configurator/' + this._image + this.currentLang + '.png';
     }
 
     return '';
