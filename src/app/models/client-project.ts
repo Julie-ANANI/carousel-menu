55c8e2632e99b831df04caeb1145ef1643967fa1
SAINIAbhishek <saini.abhishek@hotmail.com>
2021-06-09 10:13:22 +0200

    model comments and add the rest of attributes in the mission question model.

--- src/app/models/client-project.ts
+++ src/app/models/client-project.ts
@@ -2,12 +2,34 @@ import { User } from './user.model';
 
 export interface ClientProject {
   readonly _id?: string;
-
+  readonly created?: string;
+  /**
+   * if the client deleted the project we make the status of the mission Hidden
+   * instead of deleting it. (back takes care of it.)
+   */
+  readonly status?: 'PUBLISHED' | 'HIDDEN';
+  /**
+   * commercial assigned to the client project.
+   */
   commercial?: User;
+  /**
+   * same as the owner of the project that has created the project.
+   */
   client?: User | string;
+  /**
+   * stores the link of different tests for the same project.
+   */
   marketTests?: Array<string>;
 
+  /**
+   * name of the client project
+   */
   name: string;
+
+  /**
+   * for code use the 'Page name' like 'NEW_PROJECT' when save any step dates.
+   * like while creating the new project we are using the code 'NEW_PROJECT'.
+   */
   roadmapDates: Array<{
     name: string;
     code: string;
