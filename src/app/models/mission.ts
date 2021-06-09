c7e11aa3f0642427b223861926bd53621cdf990e
SAINIAbhishek <saini.abhishek@hotmail.com>
2021-06-09 10:13:22 +0200

    model comments and add the rest of attributes in the mission question model.

--- src/app/models/mission.ts
+++ src/app/models/mission.ts
@@ -4,28 +4,171 @@ import { Multiling } from './multiling';
 import {CardSectionTypes} from './innov-card';
 
 export type MissionType = 'USER' | 'CLIENT' | 'DEMO' | 'TEST';
+export type MissionQuestionOptionType = 'checkbox' | 'radio' | 'stars' | 'textarea';
+export type MissionQuestionParameterType = 'color' | 'date' |'datetime-local' | 'email' | 'month' | 'number'
+  | 'password' | 'tel' | 'text' | 'time' | 'url' | 'week' | '';
+
+export interface MissionQuestionEntry {
+  lang: string;
+  /**
+   * Titre de la section dans l'infographie
+   * Comes from the preset model.
+   */
+  title: string;
+  /**
+   * Sous-titre de la section dans le carré bleu de l'infographie
+   * Comes from the preset model.
+   */
+  subtitle: string;
+  /**
+   * it's a text used in the quiz front for help or an instruction based on the questionType.
+   * editable by the operator.
+   * Comes from the preset model.
+   */
+  instruction: string;
+  /**
+   * Positive answers label
+   * Comes from the preset model.
+   */
+  positivesAnswersLabel?: string;
+  /**
+   * Intitulé de la question (text used in the Quiz)
+   */
+  label: string;
+  /**
+   * text that we show to the client.
+   */
+  objective: string;
+}
+
+export interface MissionQuestionOption {
+  identifier: string;
+  /**
+   * La couleur dans laquelle est représentée l'option dans les charts de l'infographie
+   * Comes from the preset model.
+   */
+  color: string;
+  /**
+   * (ie à prendre en compte dans l'infographie dans les x% de réponses positives à la question)
+   * Comes from the preset model.
+   */
+  positive: boolean;
+  entry: Array<{
+    label: string;
+    lang: string;
+  }>;
+}
 
 export interface MissionQuestion {
   readonly _id: string;
+  readonly created?: Date;
 
-  entry: Array<{
-    label: string; // Intitulé de la question (text used in the Quiz)
-    lang: string;
-    objective: string; // text that we show to the client.
-  }>;
-
+  /**
+   * stores the information.
+   */
+  entry: Array<MissionQuestionEntry>;
+  /**
+   * make it Hidden if the client selected it before but now
+   * he has unselected it. The operator wants to know this.
+   */
+  status: 'HIDDEN' | 'PUBLISHED';
+  /***
+   * this is to, when the client and operator having
+   * discussion to finalize the questionnaire template.
+   * Comes from the preset model.
+   */
+  requirements: [{
+    lang: String,
+    text: String
+  }];
+  /**
+   * Comes from the preset model.
+   */
+  isTagged: boolean;
+  /**
+   * maximum options he can select for controlType === 'checkbox'
+   * Comes from the preset model.
+   */
+  maxOptionsSelect: number;
+  /**
+   * Comes from the preset model.
+   */
+  controlType: MissionQuestionOptionType;
+  /**
+   * used this to activate/deactivate a field or legend or anything
+   * For example: Activate/Deactivate "x% favourable responses" for type === 'radio'
+   * Comes from the preset model.
+   */
+  visibility: boolean;
+  /**
+   * Possibilité d'ajouter un commentaire à sa réponse
+   * Comes from the preset model.
+   */
+  canComment: boolean;
+  /**
+   * Answer contains sensitive private data as phone number or email
+   * Comes from the preset model.
+   */
+  sensitiveAnswerData: boolean;
+  /**
+   * Comes from the preset model.
+   */
+  parameters?: {
+    type: {
+      type: String,
+      enum: MissionQuestionParameterType
+      default: 'text'
+    },
+    addon: {
+      type: String
+    },
+    min: {
+      type: Number
+    },
+    max: {
+      type: Number
+    },
+    step: {
+      type: Number
+    }
+  };
+  /**
+   * Liste des réponses possibles à la question for the controlType = 'radio' | 'checkbox' | 'stars'
+   * Comes from the preset model.
+   */
+  options: Array<MissionQuestionOption>;
 }
 
 export interface MissionTemplate {
   readonly _id: string;
-  readonly sections: Array<CardSectionTypes>; // length of array represents the number of the section in innovation card with type.
-  essentials: Array<MissionQuestion>; // questions defined by us, compulsory for every market test.
-  complementary: Array<MissionQuestion>; // additional questions defined by us, user has right to select more then one.
+  /**
+   * number of sections to have in the Innovation Card and of which type.
+   * length of array represents the number of the section in innovation card.
+   */
+  readonly sections: Array<CardSectionTypes>;
+  /**
+   * these are questions defined by us for each template
+   * every template has fixed number of these questions
+   * user dose not have right to change them.
+   */
+  essentials: Array<MissionQuestion>;
+  /**
+   * each template has some additional questions that can be selected
+   * or not.
+   * user has right to change these questions.
+   */
+  complementary: Array<MissionQuestion>;
 
   entry: Array<{
+    /**
+     * can be used for the internal
+     */
     label: string;
     lang: string;
-    objective: string; // name of the template (show to the user)
+    /**
+     * name of the template (show to the user)
+     */
+    objective: string;
   }>;
 }
 
@@ -44,13 +187,33 @@ export interface MailConfiguration {
 
 export interface Mission {
   readonly _id?: string;
+  readonly created?: Date;
+  mailConf?: Array<MailConfiguration>;
+
+  /**
+   * if the client deleted the project we make the status of the mission Hidden
+   * instead of deleting it. (back takes care of it.)
+   */
+  readonly status?: 'PUBLISHED' | 'HIDDEN';
+  /**
+   * name of the mission
+   */
   name?: string;
   innovations?: Array<Innovation>;
   goal?: string;
+  /**
+   * same as owner of the innovation
+   */
   client?: User | string;
+  /**
+   * number of operators working on the mission.
+   */
   team?: Array<User>;
+  /**
+   * use name & code 'RDO', when save the restitution date by owner while creating new project.
+   * for code use the 'Page name' like 'NEW_PROJECT' when save any dates
+   */
   milestoneDates?: Array<Milestone>;
-  mailConf?: Array<MailConfiguration>;
 
   /**
    * the use case selected by the user for the Market Test.
@@ -72,12 +235,20 @@ export interface Mission {
     comment: string
   };
 
+  /**
+   * store the selection of the client for the diffusion of the project
+   * outside the application
+   */
   externalDiffusion?: {
     umi: boolean; // website
     community: boolean;
     social: boolean;
   };
 
+  /**
+   * USER - means the project is in Editing mode
+   * CLIENT - means the project is accepted and now it's market test.
+   */
   type?: MissionType;
 
 }
