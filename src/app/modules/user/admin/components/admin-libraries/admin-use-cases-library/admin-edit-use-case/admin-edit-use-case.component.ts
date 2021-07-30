4c285ce6ca952f4031706960ca722e54216041b3
SAINIAbhishek <saini.abhishek@hotmail.com>
2021-07-30 16:24:12 +0200

    add the possibility to change the use case without changing the page.

--- src/app/modules/user/admin/components/admin-libraries/admin-use-cases-library/admin-edit-use-case/admin-edit-use-case.component.ts
+++ src/app/modules/user/admin/components/admin-libraries/admin-use-cases-library/admin-edit-use-case/admin-edit-use-case.component.ts
@@ -11,6 +11,7 @@ import {RolesFrontService} from '../../../../../../../services/roles/roles-front
 import {MissionFrontService} from '../../../../../../../services/mission/mission-front.service';
 import {TranslateService} from '@ngx-translate/core';
 import {TranslateTitleService} from '../../../../../../../services/title/title.service';
+import {HttpErrorResponse} from '@angular/common/http';
 
 interface ConfirmUpdate {
   tool: boolean;
@@ -23,8 +24,8 @@ interface ConfirmUpdate {
 })
 export class AdminEditUseCaseComponent implements OnInit {
 
-  get templateName(): string {
-    return this._templateName;
+  get templates(): Array<MissionTemplate> {
+    return this._templates;
   }
 
   get validate(): ConfirmUpdate {
@@ -96,7 +97,7 @@ export class AdminEditUseCaseComponent implements OnInit {
 
   private _toSaveQuestion = false;
 
-  private _templateName = '';
+  private _templates: Array<MissionTemplate> = [];
 
   constructor(@Inject(PLATFORM_ID) protected _platformId: Object,
               private _missionService: MissionService,
@@ -109,7 +110,8 @@ export class AdminEditUseCaseComponent implements OnInit {
 
   ngOnInit() {
     this._missionTemplate = this._missionQuestionService.template;
-    this._useCaseName();
+    this._templates = this._missionQuestionService.allTemplates;
+    this._setTitle();
 
     /**
      * if the user refresh the page in that case we do not have the value in the
@@ -121,14 +123,30 @@ export class AdminEditUseCaseComponent implements OnInit {
         this._getTemplate(id);
       }
     });
+
+    if (!this._templates.length) {
+      this._getAllTemplates();
+    }
+
+  }
+
+  private _getAllTemplates() {
+    if (isPlatformBrowser(this._platformId)) {
+      this._missionService.getAllTemplates().pipe(first()).subscribe((response) => {
+        this._templates = response && response.result || [];
+      }, (error: HttpErrorResponse) => {
+        this._translateNotificationsService.error('ERROR.ERROR', ErrorFrontService.adminErrorMessage(error));
+        console.error(error);
+      });
+    }
   }
 
   private _getTemplate(id: string) {
     if (isPlatformBrowser(this._platformId)) {
       this._missionService.getTemplate(id).pipe(first()).subscribe((response) => {
         this._missionTemplate = response;
-        this._useCaseName();
-      }, error => {
+        this._setTitle();
+      }, (error: HttpErrorResponse) => {
         this._fetchingError = true;
         this._translateNotificationsService.error('ERROR.ERROR', ErrorFrontService.adminErrorMessage(error));
         console.error(error);
@@ -204,20 +222,19 @@ export class AdminEditUseCaseComponent implements OnInit {
         this._isSaving = false;
         this._toBeSaved = false;
         this._translateNotificationsService.success('Success', 'The use case has been updated successfully.');
-      }, error => {
+      }, (error: HttpErrorResponse) => {
         this._isSaving = false;
         this._translateNotificationsService.error('ERROR.ERROR', ErrorFrontService.adminErrorMessage(error));
         console.error(error);
       });
   }
 
-  private _useCaseName() {
-    this._templateName = MissionFrontService.objectiveName(this._missionTemplate, this.currentLang);
-    this._setTitle();
+  public objectiveName(template: MissionTemplate) {
+    return MissionFrontService.objectiveName(template, this.currentLang);
   }
 
   private _setTitle() {
-    this._translateTitleService.setTitle(`${this._templateName} | Use cases | Libraries`);
+    this._translateTitleService.setTitle(`${this.objectiveName(this._missionTemplate)} | Use cases | Libraries`);
   }
 
   public onClickSave(event: Event) {
@@ -286,6 +303,12 @@ export class AdminEditUseCaseComponent implements OnInit {
       this._valuesToSave.splice(index, 1);
     }
     this._valuesToSave.push(event);
+  }
+
+  public onChangeUseCase(event: string) {
+    if (!this._toBeSaved) {
+      this._missionTemplate = this._templates.find((_template) => _template._id === event);
+    }
   }
 
 }
