2fc7431affb67b8acfbdada39d0c8eefd07898ed
SAINIAbhishek <saini.abhishek@hotmail.com>
2021-10-14 16:31:53 +0200

    table docs.

--- assets/custom.js
+++ assets/custom.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     const allDocsAccordian = document.querySelectorAll('.docs-accordion');
 
     // first time page load
-    $("#docContent").load('./documentation/elements/icons.html');
+    $("#docContent").load('./documentation/elements/table.html');
 
     $(".docs-accordion").click(function(e) {
         let page = '';
