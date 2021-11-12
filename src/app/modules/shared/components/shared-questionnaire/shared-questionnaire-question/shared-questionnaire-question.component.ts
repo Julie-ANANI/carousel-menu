7d649e40093364bc855453b3169fec0d7a3f5310
Wei WANG <wwang@umi.us>
2021-11-12 14:37:43 +0100

    display: question objective

--- src/app/modules/shared/components/shared-questionnaire/shared-questionnaire-question/shared-questionnaire-question.component.ts
+++ src/app/modules/shared/components/shared-questionnaire/shared-questionnaire-question/shared-questionnaire-question.component.ts
@@ -90,6 +90,7 @@ export class SharedQuestionnaireQuestionComponent implements OnInit {
 
   @Input() set question(value: MissionQuestion) {
     this._question = value;
+    console.log(value);
     this._questionType = value.type;
     this._isTaggedQuestion = this._missionQuestionService.isTaggedQuestion(value.identifier);
 
