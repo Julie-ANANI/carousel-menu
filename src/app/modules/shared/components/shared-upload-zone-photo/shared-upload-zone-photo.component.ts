04a6e315331dcfd1b4d33d83b5f197656962e812
Abhishek SAINI <saini.abhishek@hotmail.com>
2021-11-22 14:25:32 +0100

    exact 5 mb.

--- src/app/modules/shared/components/shared-upload-zone-photo/shared-upload-zone-photo.component.ts
+++ src/app/modules/shared/components/shared-upload-zone-photo/shared-upload-zone-photo.component.ts
@@ -67,7 +67,7 @@ export class SharedUploadZonePhotoComponent {
     const _type = file.type.split('/');
 
     if (!this._isWrongFormat && (_type[1] === 'jpeg' || _type[1] === 'jpg')) {
-      if (file.size > 5242881) {
+      if (file.size > 5242880) {
         this._isUploading = false;
         this._isWrongSize = true;
       } else {
