d4051f22ee57a3a1de2ae1d387d9f6a740b75130
Abhishek SAINI <saini.abhishek@hotmail.com>
2021-11-23 17:24:12 +0100

    update webpack server config.

--- webpack.server.config.js
+++ webpack.server.config.js
@@ -1,23 +1,19 @@
 const path = require('path');
 const {ContextReplacementPlugin} = require('webpack');
+const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
   entry: { server: './server.ts' },
   resolve: { extensions: ['.js', '.ts'] },
   target: 'node',
   mode: 'production',
-  // this makes sure we include node_modules and other 3rd party libraries
-  externals: [/node_modules/, 'bufferutil', 'utf-8-validate'],
+  externals: [nodeExternals()],
   output: {
     path: path.join(__dirname, 'dist'),
     filename: '[name].js'
   },
   module: {
     rules: [{ test: /\.ts$/, loader: 'ts-loader' }]
-  },
-  // TODO: remove the optimization block once minimize work with angular-cli 6+
-  optimization: {
-    minimize: false
   },
   plugins: [
     // Temporary Fix for issue: https://github.com/angular/angular/issues/11580
