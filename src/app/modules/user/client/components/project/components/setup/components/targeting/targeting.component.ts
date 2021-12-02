575e3779603e06705f46ed6b59227b771b126b02
Abhishek SAINI <saini.abhishek@hotmail.com>
2021-12-02 15:16:29 +0100

    ✔️ refactor code.

--- src/app/modules/user/client/components/project/components/setup/components/targeting/targeting.component.ts
+++ src/app/modules/user/client/components/project/components/setup/components/targeting/targeting.component.ts
@@ -1,21 +1,24 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {InnovationFrontService} from '../../../../../../../../../services/innovation/innovation-front.service';
 import {CanComponentDeactivate} from '../../../../../../../../../guards/can-deactivate-guard.service';
-import {Observable} from 'rxjs';
+import {Observable, Subject} from 'rxjs';
+import {takeUntil} from 'rxjs/operators';
 
 @Component({
   templateUrl: './targeting.component.html',
   styleUrls: ['./targeting.component.scss']
 })
 
-export class TargetingComponent implements OnInit, CanComponentDeactivate {
+export class TargetingComponent implements OnInit, CanComponentDeactivate, OnDestroy {
 
   private _changesSaved = false;
+
+  private _ngUnsubscribe: Subject<any> = new Subject();
 
   constructor(private _innovationFrontService: InnovationFrontService) { }
 
   ngOnInit(): void {
-    this._innovationFrontService.getNotifyChanges().subscribe((value) => {
+    this._innovationFrontService.getNotifyChanges().pipe(takeUntil(this._ngUnsubscribe)).subscribe((value) => {
       if (value && value.key === 'settings') {
         this._changesSaved = value && value.state;
       }
@@ -24,6 +27,11 @@ export class TargetingComponent implements OnInit, CanComponentDeactivate {
 
   canDeactivate(): Observable<boolean> | Promise<boolean> | boolean {
     return !this._changesSaved;
+  }
+
+  ngOnDestroy(): void {
+    this._ngUnsubscribe.next();
+    this._ngUnsubscribe.complete();
   }
 
 }
