1c5ca675a92b84cadaac4e0114c518a3179550af
Abhishek SAINI <saini.abhishek@hotmail.com>
2021-12-17 11:04:06 +0100

    UMI 2068 show last mailing date and length in case of Notify project team

--- src/app/models/notification.ts
+++ src/app/models/notification.ts
@@ -1,3 +1,5 @@
+import {User} from './user.model';
+
 export type NotificationTrigger = 'TRIGGER_CREATE_ACCOUNT' | 'TRIGGER_ADD_TEAMMATES' | 'TRIGGER_CHANGE_OWNER'
   | 'TRIGGER_FIRST_ANSWER' | 'TRIGGER_NEW_ANSWER' | 'TRIGGER_COMPLETE_PROJECT' | 'TRIGGER_COMMENT_SUGGESTION'
   | 'TRIGGER_ASK_VALIDATE_PROJECT' | 'TRIGGER_CONFIRM_VALIDATE_PROJECT' | 'TRIGGER_DOWNLOAD_DOCUMENTS'
@@ -16,22 +18,17 @@ export interface NotificationJob {
    *  - 'ERROR': there's an error detected during the process
    *  - 'CANCELLED': the job is cancelled based on the condition
    */
-  status: NotificationJobStatus;
+  readonly status: NotificationJobStatus;
 
   /**
    * List of the users who will receive the notification
    */
-  recipients: [{
-    email: string;
-    language: string;
-    firstName: string;
-    recipientId: string;
-  }];
+  readonly recipients: [User];
 
   /**
    * saving the copy of whole notification
    */
-  notification: Notification;
+  readonly notification: Notification;
 
   /**
    * reference to the user who invoked the route / function of the notification
@@ -39,12 +36,15 @@ export interface NotificationJob {
    * store id of that ope.
    * For certain case it can be empty like Add Teammates.
    */
-  userRef: string;
+  readonly userRef: string;
 
   /**
    * reference to the innovation. We need it to check the conditions later in the process.
    */
-  innovationRef: string;
+  readonly innovationRef: string;
+
+  readonly created?: Date;
+  readonly updated?: Date;
 }
 
 export interface Notification {
@@ -52,7 +52,7 @@ export interface Notification {
    * contain the template info of the notification.
    * Every notification should have one
    */
-  template: {
+  readonly template: {
     name: string;
     content: [{
       lang: string;
@@ -61,24 +61,24 @@ export interface Notification {
     }]
   },
 
-  type: 'USER' | 'PROJECT';
+  readonly type: 'USER' | 'PROJECT';
 
   /**
    * every notification should have one trigger and based on that we define the conditions and
    * process it.
    */
-  trigger: NotificationTrigger;
+  readonly trigger: NotificationTrigger;
 
   /**
    * certain notification we have to send after certain time period, so we identify
    * them with the cron name and process them accordingly
    */
-  cronName: NotificationCronName;
+  readonly cronName: NotificationCronName;
 
   /**
    * it's the priority in the job queue to decide the order of sending
    * notification. The higher the number, the lower is the priority; We also use it
    * for the mailgun priority.
    */
-  jobPriority: number;
+  readonly jobPriority: number;
 }
