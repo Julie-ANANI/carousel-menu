15e18a3cb542ccb0469478e4919628d8e1f5866b
Abhishek SAINI <saini.abhishek@hotmail.com>
2022-01-03 18:50:18 +0100

    UMI-2142 ✅ update the models format: transactionnal-email.

--- src/app/models/transactionnal-email.ts
+++ src/app/models/transactionnal-email.ts
@@ -1,8 +1,19 @@
 import { EmailTemplate } from './email-template';
 
+export interface TransactionalEmailTemplate {
+  lang: string;
+  template: EmailTemplate;
+}
+
+// TODO remove multiling
 export interface TransactionalEmail {
   _id?: string;
   name: string;
+  templates?: Array<TransactionalEmailTemplate>;
+
+  /**
+   * TODO remove this because from the back will send templates
+   */
   en: EmailTemplate;
   fr: EmailTemplate;
 }
