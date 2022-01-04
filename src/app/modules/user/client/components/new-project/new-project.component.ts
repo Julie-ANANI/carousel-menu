0aeb42553eb104fb591def26429b832a7e4212e8
Wei WANG <wwang@umi.us>
2022-01-04 15:36:03 +0100

    UMI-2143: error - create new project

--- src/app/modules/user/client/components/new-project/new-project.component.ts
+++ src/app/modules/user/client/components/new-project/new-project.component.ts
@@ -180,7 +180,7 @@ export class NewProjectComponent implements OnInit, OnDestroy {
       .subscribe((response) => {
         this._router.navigate([`/user/projects/${response['innovation']['_id']}/settings`]);
       }, (err: HttpErrorResponse) => {
-        this._translateNotificationsService.error('ERROR.ERROR', ErrorFrontService.getErrorMessage(err.status));
+        this._translateNotificationsService.error('ERROR.ERROR', ErrorFrontService.getErrorKey(err.error));
         this._isCreating = false;
         console.error(err);
       });
