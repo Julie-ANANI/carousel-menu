3c15af7608035979f752a314a7e4ae7edfa61347
Abhishek SAINI <saini.abhishek@hotmail.com>
2022-01-05 15:11:13 +0100

    UMI-2142 🆙 lang entry pipe to accommodate the actual multiling process.

--- src/app/pipe/lang-entry/lang-entry.pipe.ts
+++ src/app/pipe/lang-entry/lang-entry.pipe.ts
@@ -3,35 +3,48 @@
  */
 
 import { Pipe, PipeTransform } from '@angular/core';
+import {TranslateService} from '@ngx-translate/core';
 
 @Pipe({
   name: 'langEntry'
 })
 export class LangEntryPipe implements PipeTransform {
 
+  constructor(private _translateService: TranslateService) {
+  }
+
   /**
-   * from the list of the entry it will return the single entry based on the 'lang' param.
+   * from the list of the entry or type of Entry it will return the requested based on the 'lang' param.
    *
    * This pipe is also extended by the service 'LangEntryService' to use in the component.ts file so
    * if you make changes in the param do there also.
    *
-   * @param entry - actual entry list can be of any type, but it should have the lang property.
-   * @param requested
+   * @param value - actual entry it can be of type or Array<any> but in that case it should have the lang property.
+   * @param requested - the label or value you want
    * @param lang - requested lang
-   * @param returnDefault = true means if not find the entry in the given lang then return the entry at index 0
+   * @param returnDefault = true means if not find the entry in the given lang then return the default entry
    */
-  transform(entry: Array<any>, lang = 'en', requested: 'ENTRY' | 'INDEX' = 'ENTRY', returnDefault = false): any {
-    if (!entry.length) return '';
+  transform(value: any, requested: string, lang = 'en', returnDefault = false): any {
+    if (!value || !requested) return '';
 
-    const _entryIndex = entry.findIndex((_entry) => _entry.lang === lang);
+    if (Array.isArray(value) && !!value.length) {
+      const entry = value.find((_value) => _value.lang === lang);
+      return !!entry ? requested === 'FULL_ENTRY' ? entry : entry[requested] : returnDefault ? value[0][requested] : '';
+    }
 
-    switch (requested) {
-      case 'ENTRY':
-        return _entryIndex !== -1 ? entry[_entryIndex] : returnDefault ? entry[0] : '';
-      case 'INDEX':
-        return _entryIndex !== -1 ? _entryIndex : returnDefault ? 0 : '';
-      default:
+    if (value[requested] && value[requested][lang]) {
+      return value[requested] && value[requested][lang];
+    }
+
+    if (returnDefault) {
+      if (lang !== this._translateService.defaultLang && value[requested][this._translateService.defaultLang]) {
+        return value[requested][this._translateService.defaultLang];
+      } else {
+        for (const a in value[requested]) {
+          if(value[requested][a]) return value[requested][a];
+        }
         return '';
+      }
     }
   }
 
