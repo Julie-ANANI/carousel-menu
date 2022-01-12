8180de8f3ab5101dac51be44997d92492233e2eb
Abhishek SAINI <saini.abhishek@hotmail.com>
2022-01-12 18:40:01 +0100

    UMi-2193 package and some changes.

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -18,6 +18,8 @@ import { CookieBackendModule } from 'ngx-cookie-backend';
 
 @NgModule({
   imports: [
+    // The AppServerModule should import your AppModule followed
+    // by the ServerModule from @angular/platform-server.
     AppModule,
     ServerModule,
     ServerTransferStateModule,
@@ -31,6 +33,8 @@ import { CookieBackendModule } from 'ngx-cookie-backend';
     { provide: SocketService, useClass: SocketSsrService },
     { provide: EtherpadSocketService, useClass: EtherpadSocketSsrService },
   ],
+  // Since the bootstrapped component is not inherited from your
+  // imported AppModule, it needs to be repeated here.
   bootstrap: [ AppComponent ],
 })
 export class AppServerModule {}
