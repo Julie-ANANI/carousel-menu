7e160ec0170c34ee86d48904e2e032f46cd455fa
Abhishek SAINI <saini.abhishek@hotmail.com>
2022-01-14 15:07:08 +0100

    return type void.

--- src/app/services/download/download.service.ts
+++ src/app/services/download/download.service.ts
@@ -4,23 +4,22 @@
 import { Injectable } from '@angular/core';
 import * as FileSaver from 'file-saver';
 
-// TODO check this again
 @Injectable({providedIn: 'root'})
 export class DownloadService {
 
   constructor() { }
 
-  static saveCsv(csv: string, fileName: string): any {
+  static saveCsv(csv: string, fileName: string): void {
     const file = <Blob> new Blob([csv]);
     return FileSaver.saveAs(file, `${fileName}.csv`);
   }
 
-  static saveJson(json: string, fileName: string): any {
+  static saveJson(json: string, fileName: string): void {
     const file = <Blob> new Blob([json]);
     return FileSaver.saveAs(file, `${fileName}.json`);
   }
 
-  static savePDF(pdf: string, fileName: string): any {
+  static savePDF(pdf: string, fileName: string): void {
     const file = <Blob> new Blob([pdf]);
     return FileSaver.saveAs(file, `${fileName}.pdf`);
   }
