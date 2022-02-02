6756090411abc5d8ffe68f958f3c8963158ffc3d
Julie-ANANI <65860846+Julie-ANANI@users.noreply.github.com>
2022-02-02 15:46:28 +0100

    new color

--- src/app/utils/chartColors.ts
+++ src/app/utils/chartColors.ts
@@ -6,15 +6,9 @@ export const colors = [
   {label: 'neutral-1', value: '#bbc7d6'},
   {label: 'neutral-2', value: '#ffb300'},
   {label: 'neutral-3', value: '#ffcc6e'},
-  {label: 'neutral-4', value: '#2e6188'},
-  {label: 'neutral-5', value: '#3a78a8'},
+  {label: 'neutral-4', value: '#8f9cdb'},
+  {label: 'neutral-5', value: '#db8fb6'},
   {label: 'neutral-6', value: '#dde3ea'},
-
-  {label: 'neutral-7', value: '#5584a9'},
-  {label: 'neutral-8', value: '#0077b6'},
-  {label: 'neutral-9', value: '#00b4d8'},
-  {label: 'neutral-10', value: '#caf0f8'},
-  {label: 'neutral-11', value: '#90e0ef'},
 ];
 
 export const oldColorsToNewMapping = {
