b9d2264da0f2cc2da275542059e33d71d5f9bdb6
Abhishek SAINI <saini.abhishek@hotmail.com>
2022-02-02 14:19:33 +0100

    UMI-2270 comment model

--- src/app/models/tag.ts
+++ src/app/models/tag.ts
@@ -1,4 +1,3 @@
-import { Multiling } from './multiling';
 import { TagAttachment } from './tag-attachment';
 
 /**
@@ -9,7 +8,7 @@ export type TagType = 'UNKNOWN' | 'SECTOR' | 'VALUE_CHAIN' | 'SOLUTION_TYPE' | '
 export type TagStatusType = 'DRAFT' | 'VALID';
 
 export interface TagEntry {
-  originalLabel: string;
+  originalLabel?: string;
   lang: string;
   label: string;
   description: string;
@@ -30,7 +29,7 @@ export interface Tag {
   /**
    * TODO remove these because will be replaced with entry
    */
-  readonly originalLabel?: Multiling;
-  readonly label: Multiling;
-  readonly description?: Multiling;
+  // readonly originalLabel?: Multiling;
+  // readonly label: Multiling;
+  // readonly description?: Multiling;
 }
