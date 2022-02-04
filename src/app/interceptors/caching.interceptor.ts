a91bbb6f4fd16d8d82ad59daa0de15ad6e307046
Abhishek SAINI <saini.abhishek@hotmail.com>
2022-02-04 16:18:14 +0100

    UMI-2258 working version of response caching.

---
+++ src/app/interceptors/caching.interceptor.ts
@@ -0,0 +1,65 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor, HttpResponse
+} from '@angular/common/http';
+import {Observable, of} from 'rxjs';
+import {tap} from 'rxjs/operators';
+import {HttpCacheService} from '../services/http-cache/http-cache.service';
+
+@Injectable()
+export class CachingInterceptor implements HttpInterceptor {
+
+  constructor(private _httpCacheService: HttpCacheService) {}
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+
+    /**
+     * pass for PUT, DELETE, POST requests
+     */
+    if (request.method !== 'GET') {
+      return next.handle(request);
+    }
+
+    /**
+     * clear all the cache entries
+     */
+    if (request.headers.get('cache') === 'clear') {
+      this._httpCacheService.clearCache();
+    }
+
+    /**
+     * delete the cache entry of that URL from the Cached object.
+     */
+    if (request.headers.get('cache') === 'reset') {
+      this._httpCacheService.delete(request.urlWithParams);
+    }
+
+    /**
+     * checking if there is already cached data for this url
+     */
+    const cachedResponse = this._httpCacheService.get(request.urlWithParams);
+
+    /**
+     * returned the cached response
+     */
+    if (cachedResponse) {
+      return of(cachedResponse.clone());
+    }
+
+    /**
+     * request is going for the first time then let the request process
+     * and cache the response
+     */
+    return next.handle(request).pipe(
+      tap( (event) => {
+        if (event instanceof HttpResponse) {
+          this._httpCacheService.set(request.urlWithParams, event.clone());
+        }
+      })
+    );
+
+  }
+}
--- src/app/services/http-cache/http-cache.service.ts
+++ src/app/services/http-cache/http-cache.service.ts
@@ -1,9 +1,49 @@
 import { Injectable } from '@angular/core';
+import { HttpResponse } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpCacheService {
 
+  /**
+   * store the key-value pairs of request and response.
+   * @private
+   */
+  private _cache: Map<string, HttpResponse<any>> = new Map<string, HttpResponse<any>>();
+
   constructor() { }
+
+  /**
+   * return the value from the _cache object based on the URL
+   * @param url
+   */
+  public get(url: string): HttpResponse<any> | undefined {
+    return this._cache[url];
+  }
+
+  /**
+   * set the new key-value pair for the new response and URL.
+   * @param url
+   * @param response
+   */
+  public set(url: string, response: HttpResponse<any>): void {
+    this._cache[url] = response;
+  }
+
+  /**
+   * clear all the cached data.
+   */
+  public clearCache(): void {
+    this._cache = new Map<string, HttpResponse<any>>();
+  }
+
+  /**
+   * delete the entry from the cached data.
+   * @param url
+   */
+  public delete(url: string): void {
+    delete this._cache[url];
+  }
+
 }
