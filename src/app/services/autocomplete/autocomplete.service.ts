98e390ba6a3c4d414358cb8d0c29748b86d12dd5
SAINIAbhishek <saini.abhishek@hotmail.com>
2022-02-16 16:31:47 +0100

    UMI-2315 extends common Services

--- src/app/services/autocomplete/autocomplete.service.ts
+++ src/app/services/autocomplete/autocomplete.service.ts
@@ -1,18 +1,19 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import {UmiusAutocompleteService} from '@umius/umi-common-component';
+import {HttpClient} from '@angular/common/http';
 
-interface AutocompleteObject {_id: string; name: string; domain: string; flag: string; logo: string}
+/**
+ * UmiusAutocompleteService functions:
+ *
+ * 1. get(params: {query: string, type: string, tagType?: string, internalOnly?: string}):
+ *     Observable<UmiusAutoCompleteObjectInterface[]>;
+ */
 
 @Injectable({providedIn: 'root'})
-export class AutocompleteService {
+export class AutocompleteService extends UmiusAutocompleteService {
 
-  constructor(private _http: HttpClient) { }
-
-  public get(params: {query: string, type: string, tagType?: string, internalOnly?: string}): Observable<AutocompleteObject[]> {
-    return this._http.get<AutocompleteObject[]>('/misc/suggestions', {params: params})
-    .pipe(map((res: any) => res.result));
+  constructor(protected _httpClient: HttpClient) {
+    super(_httpClient);
   }
 
 }
