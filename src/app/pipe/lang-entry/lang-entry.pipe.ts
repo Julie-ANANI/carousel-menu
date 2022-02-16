e2ccecf3e7e9539c9c0737a8953673da9d756b61
SAINIAbhishek <saini.abhishek@hotmail.com>
2022-02-16 16:52:25 +0100

    UMI-2315 extends common pipes

--- src/app/pipe/lang-entry/lang-entry.pipe.ts
+++ src/app/pipe/lang-entry/lang-entry.pipe.ts
@@ -2,27 +2,22 @@
  * Created by Abhishek SAINI on 03-01-2022
  */
 
-import { Pipe, PipeTransform } from '@angular/core';
+import { Pipe } from '@angular/core';
 import {LangEntryService} from '../../services/lang-entry/lang-entry.service';
+import {UmiusLangEntryPipe} from '@umius/umi-common-component/pipe/lang-entry/lang-entry.pipe';
+
+/**
+ * UmiusLangEntryPipe:
+ * 1. transform(value: any, requested: string, lang = 'en', returnDefault = false): any;
+ */
 
 @Pipe({
   name: 'langEntry'
 })
-export class LangEntryPipe implements PipeTransform {
+export class LangEntryPipe extends UmiusLangEntryPipe {
 
-  constructor(private _langEntryService: LangEntryService) {
-  }
-
-  /**
-   * from the list of the entry or type of Entry it will return the requested based on the 'lang' param.
-   *
-   * @param value - actual entry it can be of type or Array<any> but in that case it should have the lang property.
-   * @param requested - the label or value you want
-   * @param lang - requested lang
-   * @param returnDefault = true means if not find the entry in the given lang then return the default entry
-   */
-  transform(value: any, requested: string, lang = 'en', returnDefault = false): any {
-    return this._langEntryService.transform(value, requested, lang, returnDefault);
+  constructor(protected _langEntryService: LangEntryService) {
+    super(_langEntryService);
   }
 
 }
