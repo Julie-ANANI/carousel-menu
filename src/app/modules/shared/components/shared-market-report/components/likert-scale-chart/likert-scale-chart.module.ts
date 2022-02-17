7176d43f2cdc8af1feae9a25d601235b570dcba2
SAINIAbhishek <saini.abhishek@hotmail.com>
2022-02-17 14:56:42 +0100

    UMI-2315 update shared market report module

--- src/app/modules/shared/components/shared-market-report/components/likert-scale-chart/likert-scale-chart.module.ts
+++ src/app/modules/shared/components/shared-market-report/components/likert-scale-chart/likert-scale-chart.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import {CommonModule} from '@angular/common';
 import { TranslateModule} from '@ngx-translate/core';
 import { LikertScaleChartComponent } from './likert-scale-chart.component';
-import {ProgressBarModule} from '../../../../../utility/progress-bar/progress-bar.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    TranslateModule,
-    ProgressBarModule,
+    TranslateModule
   ],
   declarations: [LikertScaleChartComponent],
   exports:[LikertScaleChartComponent]
