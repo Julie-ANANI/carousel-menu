0646dd18282d9e9fd3a6040777a6094f478d7b1a
SAINIAbhishek <saini.abhishek@hotmail.com>
2022-02-17 15:03:19 +0100

    UMI-2315 update shared professional list module

--- src/app/modules/shared/components/shared-professionals-list/shared-professionals-list.module.ts
+++ src/app/modules/shared/components/shared-professionals-list/shared-professionals-list.module.ts
@@ -3,30 +3,28 @@ import { TranslateModule } from '@ngx-translate/core';
 
 import { SharedProfessionalsListComponent } from './shared-professionals-list.component';
 
-import { SidebarModule } from '../../../sidebars/templates/sidebar/sidebar.module';
 import { SidebarUserFormModule } from '../../../sidebars/components/user-form/sidebar-user-form.module';
 import { SidebarTagsModule } from '../../../sidebars/components/sidebar-tags/sidebar-tags.module';
-import { ModalModule } from '../../../utility/modals/modal/modal.module';
 import {SidebarSearchModule} from '../../../sidebars/components/sidebar-search/sidebar-search.module';
 import {CommonModule} from '@angular/common';
 import {ReactiveFormsModule} from '@angular/forms';
 import {SharedWorldListModule} from '../shared-world-list/shared-world-list.module';
 import {SharedTargetingWorldModule} from '../shared-targeting-world/shared-targeting-world.module';
-import { TableComponentsModule } from '@umius/umi-common-component/table';
+import {ModalModule, SidebarFullModule, TableModule} from '@umius/umi-common-component';
 
 @NgModule({
   imports: [
     CommonModule,
     ReactiveFormsModule,
     TranslateModule.forChild(),
-    SidebarModule,
     SidebarUserFormModule,
     SidebarTagsModule,
-    ModalModule,
     SidebarSearchModule,
     SharedWorldListModule,
     SharedTargetingWorldModule,
-    TableComponentsModule,
+    TableModule,
+    SidebarFullModule,
+    ModalModule,
   ],
   declarations: [SharedProfessionalsListComponent],
   exports: [SharedProfessionalsListComponent],
