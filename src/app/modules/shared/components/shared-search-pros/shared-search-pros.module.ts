7a3c81c9d379a7f2e24d11049cc7024e1c19a920
SAINIAbhishek <saini.abhishek@hotmail.com>
2022-02-17 15:29:10 +0100

    UMI-2315 update shared search pros module

--- src/app/modules/shared/components/shared-search-pros/shared-search-pros.module.ts
+++ src/app/modules/shared/components/shared-search-pros/shared-search-pros.module.ts
@@ -5,29 +5,27 @@ import { TranslateModule } from '@ngx-translate/core';
 
 import { SharedSearchProsComponent } from './shared-search-pros.component';
 
-import { SidebarModule } from '../../../sidebars/templates/sidebar/sidebar.module';
 import { SidebarSearchModule } from '../../../sidebars/components/sidebar-search/sidebar-search.module';
-import { ModalModule } from "../../../utility/modals/modal/modal.module";
 import { SharedSearchSettingsModule } from '../shared-search-settings/shared-search-settings.module';
 import { SharedTargetingWorldModule } from '../shared-targeting-world/shared-targeting-world.module';
 import { SharedSearchConfigProModule } from '../shared-search-config-pro/shared-search-config-pro.module';
 import { MessageTemplateModule } from '../../../utility/messages/message-template/message-template.module';
 import { SharedProfessionalTargetingModule } from '../shared-professional-targeting/shared-professional-targeting.module';
+import {ModalModule} from '@umius/umi-common-component';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
-    SidebarModule,
     ReactiveFormsModule,
     TranslateModule.forChild(),
     SidebarSearchModule,
-    ModalModule,
     SharedSearchSettingsModule,
     SharedTargetingWorldModule,
     SharedSearchConfigProModule,
     MessageTemplateModule,
-    SharedProfessionalTargetingModule
+    SharedProfessionalTargetingModule,
+    ModalModule
   ],
   declarations: [
     SharedSearchProsComponent
