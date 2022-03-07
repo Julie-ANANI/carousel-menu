dfc549d926fe94afecaf0aad4b046f1f57daae99
WeiWANG0118 <wwang@umi.us>
2022-03-07 09:53:57 +0100

    reset to apiUrl,why apisix is not stable?

--- src/app/interceptors/apiUrl.interceptor.ts
+++ src/app/interceptors/apiUrl.interceptor.ts
@@ -27,7 +27,7 @@ export class ApiUrlInterceptor implements HttpInterceptor {
   private _setAppV3Url(req: HttpRequest<any>): HttpRequest<any> {
     const newParameters: any = {
       //url: environment.apiUrl + req.url, //use this at local
-      url: environment.apiGatewayUrl + req.url,
+      url: environment.apiUrl + req.url,
       withCredentials: true,
     };
     this._setCookie(newParameters, req);
