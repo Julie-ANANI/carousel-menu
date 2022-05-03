09b43ffb06837470fce8dc72f28fe2a536a7e331
Wei WANG <wwang@umi.us>
2022-05-03 09:28:22 +0200

    add test, good configure

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,12 @@
+console.log('loading jest.config.js file');
 module.exports = {
   preset: 'jest-preset-angular',
   roots: ['<rootDir>/src/'],
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
   testMatch: ['**/+(*.)+(spec).+(ts|js)'],
   setupFilesAfterEnv: ['<rootDir>/src/setupJest.ts'],
   collectCoverage: true,
--- src/app/modules/user/user.module.ts
+++ src/app/modules/user/user.module.ts
@@ -2,12 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
 import { UserRoutingModule } from './user-routing.module';
-
 import { FooterModule } from '../common/footer/footer.module';
 import { HeaderModule } from '../common/header/header.module';
-
 import { UserComponent } from './user.component';
-
 import { SpinnerLoaderModule } from '../utility/spinner-loader/spinner-loader.module';
 import { BannerModule } from '../utility/banner/banner.module';
 
