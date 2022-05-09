78f1dd9d10bec785d6689729e8a6fac1bf6d3594
Wei WANG <wwang@umi.us>
2022-05-09 10:05:43 +0200

    add list of languages

--- src/app/models/static-data/language.ts
+++ src/app/models/static-data/language.ts
@@ -4,6 +4,24 @@ export interface Language {
 }
 
 export const lang: Array<Language> = [
+  { type: 'ar', alias: 'Arabic' },
+  { type: 'zh', alias: 'Chinese' },
+  { type: 'da', alias: 'Danish' },
+  { type: 'nl', alias: 'Dutch' },
   { type: 'en', alias: 'English' },
   { type: 'fr', alias: 'French' },
+  { type: 'fi', alias: 'Finnish' },
+  { type: 'de', alias: 'German' },
+  { type: 'it', alias: 'Italian' },
+  { type: 'ja', alias: 'Japanese' },
+  { type: 'ko', alias: 'Korean' },
+  { type: 'el', alias: 'Greek' },
+  { type: 'no', alias: 'Norwegian' },
+  { type: 'pl', alias: 'Polish' },
+  { type: 'pt', alias: 'Portuguese' },
+  { type: 'ru', alias: 'Russian' },
+  { type: 'es', alias: 'Spanish' },
+  { type: 'sv', alias: 'Swedish' },
+  { type: 'tr', alias: 'Turkish' },
+  { type: 'uk', alias: 'Ukrainian' },
 ];
