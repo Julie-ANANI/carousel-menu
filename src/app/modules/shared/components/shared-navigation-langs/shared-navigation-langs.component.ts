6ae2da8a7dc1e71caaba5c44717b8748da3641b1
Julie-ANANI <65860846+Julie-ANANI@users.noreply.github.com>
2022-05-10 10:49:53 +0200

    add active card

--- src/app/modules/shared/components/shared-navigation-langs/shared-navigation-langs.component.ts
+++ src/app/modules/shared/components/shared-navigation-langs/shared-navigation-langs.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import {Innovation} from '../../../../models/innovation';
+import {RouteFrontService} from '../../../../services/route/route-front.service';
+import {InnovCard} from '../../../../models/innov-card';
+import {InnovationFrontService} from '../../../../services/innovation/innovation-front.service';
 
 @Component({
   selector: 'app-shared-navigation-langs',
@@ -8,10 +11,13 @@ import {Innovation} from '../../../../models/innovation';
 })
 export class SharedNavigationLangsComponent {
 
+  private _activeTab = this._routeFrontService.activeTab(8, 7);
   private _project: Innovation = <Innovation>{};
+  private _activeCardIndex = 0;
+
   private _username = 'JULIE';
 
-  constructor() { }
+  constructor(private _routeFrontService: RouteFrontService) { }
 
   get user() {
     return this._username;
@@ -19,6 +25,14 @@ export class SharedNavigationLangsComponent {
 
   get showLangDrop(): boolean {
     return this._project.innovationCards && this._project.innovationCards.length > 1;
+  }
+
+  get activeTab(): string {
+    return this._activeTab;
+  }
+
+  get activeCard(): InnovCard {
+    return InnovationFrontService.activeCard(this._project, this._activeCardIndex);
   }
 
 }
