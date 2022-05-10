823c6f0143522ab868cc4ef37381229272b0be77
Julie-ANANI <65860846+Julie-ANANI@users.noreply.github.com>
2022-05-10 10:59:02 +0200

    add open modal

--- src/app/modules/shared/components/shared-navigation-langs/shared-navigation-langs.component.ts
+++ src/app/modules/shared/components/shared-navigation-langs/shared-navigation-langs.component.ts
@@ -3,6 +3,7 @@ import {Innovation} from '../../../../models/innovation';
 import {RouteFrontService} from '../../../../services/route/route-front.service';
 import {InnovCard} from '../../../../models/innov-card';
 import {InnovationFrontService} from '../../../../services/innovation/innovation-front.service';
+import {RolesFrontService} from '../../../../services/roles/roles-front.service';
 
 @Component({
   selector: 'app-shared-navigation-langs',
@@ -11,13 +12,62 @@ import {InnovationFrontService} from '../../../../services/innovation/innovation
 })
 export class SharedNavigationLangsComponent {
 
-  private _activeTab = this._routeFrontService.activeTab(8, 7);
   private _project: Innovation = <Innovation>{};
+  private _activeTab = this._routeFrontService.activeTab(8, 7);
   private _activeCardIndex = 0;
+  private _setActiveCardIndex() {
+    this._innovationFrontService.setActiveCardIndex(this._activeCardIndex);
+  }
+  private _cardToDelete: InnovCard = <InnovCard>{};
+  private _modelType = '';
+  private _showModal = false;
+  private _isAddingCard = false;
+  private _showCardModal = false;
+  private _isDeletingCard = false;
 
   private _username = 'JULIE';
 
-  constructor(private _routeFrontService: RouteFrontService) { }
+  constructor(private _routeFrontService: RouteFrontService,
+              private _innovationFrontService: InnovationFrontService,
+              private _rolesFrontService: RolesFrontService) { }
+
+  public setCardLang(value: string) {
+    if (this.activeCard && !(this._cardToDelete._id)) {
+      this._activeCardIndex = InnovationFrontService.currentLangInnovationCard(this._project, value, 'INDEX');
+      this._setActiveCardIndex();
+    }
+  }
+
+  public canAccess(path?: Array<string>) {
+    const _default: Array<string> = ['projects', 'project'];
+    return this._rolesFrontService.hasAccessAdminSide(_default.concat(path));
+  }
+
+  public openModal(event: Event, type: 'ADD_LANG' | 'DELETE_LANG' | 'ASK_VALIDATION', deleteCard?: InnovCard) {
+    event.preventDefault();
+    this._modelType = type;
+
+    switch (type) {
+
+      case 'ASK_VALIDATION':
+        this._showModal = true;
+        break;
+
+      case 'ADD_LANG':
+        if (this.canAddCard && !this._isAddingCard) {
+          this._showCardModal = true;
+        }
+        break;
+
+      case 'DELETE_LANG':
+        if (!this._isDeletingCard) {
+          this._cardToDelete = deleteCard;
+          this._showCardModal = true;
+        }
+        break;
+
+    }
+  }
 
   get user() {
     return this._username;
@@ -33,6 +83,18 @@ export class SharedNavigationLangsComponent {
 
   get activeCard(): InnovCard {
     return InnovationFrontService.activeCard(this._project, this._activeCardIndex);
+  }
+
+  get canAddCard(): boolean {
+    return this._project.innovationCards && this._project.innovationCards.length === 1;
+  }
+
+  get project(): Innovation {
+    return this._project;
+  }
+
+  get canEditDescription(): boolean {
+    return this.canAccess(['settings', 'edit', 'description']);
   }
 
 }
