a40c011480153ce26b68bb6c121cfe9be5d4b543
Julie-ANANI <65860846+Julie-ANANI@users.noreply.github.com>
2022-05-12 22:10:33 +0200

    :fire: Menu kebab
    mini bug btn next and start btn prev
    
    find first condition btn prev
    
    add condition max view
    
    :wastebasket: finish clean function prev, next and :lipstick: css for kebab menu
    
    manual repair bad rebase aigain
    
    replace by good value
    
    click not work
    
    clean code
    
    add good css for button
    
    bug background
    
    logic for quantity to quantity
    
    finish function next
    
    css btn view less and more
    
    function prev works
    
    finish correction bug
    
    start clean code file description
    
    clean css
    
    clean code and comment file ts menu kebab
    
    work in progress
    
    two state
    
    finish bug dsiplay two state
    
    clean code
    
    nug no value transfered by click
    
    stry add setCard in menukebab
    
    :fire: Menu kebab
    
    find logic button next
    
    new kebab menu
    
    reset to zero
    
    add false item in admin-project
    
    add directive kebab carousel item
    
    create template
    
    add comment and contentChildren
    
    add style for temnplate kebab item
    
    recrate hmtl template
    
    finish change template to html
    
    logic button prev and next
    
    dont have good language
    
    add ng-container for always items and just 5 items
    
    clean file ts
    
    clean html
    
    clean file ts
    
    work in progress
    
    test ultim
    
    next not work corectly
    
    state initial menu
    
    finish logic button next
    
    switch template to urlHtml
    
    remove add button lang
    
    change name selector menu-kebab
    
    remove ref template carousel and add todo
    
    try to change displays items
    
    add dynamic value
    
    clean code
    
    delete code in file ts
    
    deleted code html
    
    display simple button
    
    have good design
    
    :fire: function next work !
    
    find logic button next
    
    :recycle: :lipstick: version one style css
    
    mini bug btn next and start btn prev
    
    find first condition btn prev
    
    :fire: new structure
    
    :recycle: :lipstick: create complex structure menu kebab eith advanced concept

--- src/app/modules/utility/menu-kebab/menu-kebab.module.ts
+++ src/app/modules/utility/menu-kebab/menu-kebab.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MenuKebabComponent } from './menu-kebab.component';
+import {TranslateModule} from '@ngx-translate/core';
 
 @NgModule({
   declarations: [MenuKebabComponent],
-  exports:[MenuKebabComponent
+  exports: [MenuKebabComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    TranslateModule
   ]
 })
 export class MenuKebabModule { }
