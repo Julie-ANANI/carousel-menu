67dd88cfc8a5d7f958f730f4070e8eb9b6293207
blusy38 <contact@guillaumebraillon.fr>
2022-05-19 14:42:39 +0200

    UMI-2656-Importing_CSV_pros_without_emails

--- src/app/modules/shared/components/shared-import-pros/shared-import-pros.component.ts
+++ src/app/modules/shared/components/shared-import-pros/shared-import-pros.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input } from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 import { first } from 'rxjs/operators';
 import { HttpErrorResponse } from '@angular/common/http';
 import { Campaign } from '../../../../models/campaign';
 import { SearchService } from '../../../../services/search/search.service';
 import { TranslateNotificationsService } from '../../../../services/translate-notifications/translate-notifications.service';
 import { RolesFrontService } from '../../../../services/roles/roles-front.service';
+import {ErrorFrontService} from '../../../../services/error/error-front.service';
 
 @Component({
   selector: 'app-shared-import-pros',
@@ -16,8 +17,15 @@ export class SharedImportProsComponent {
 
   @Input() campaign: Campaign = <Campaign>{};
 
+  private _importingErrors: Array<any>;
+
+  private _slicedErrors: Array<any> = [];
+
+  private _errorsModal: boolean = false;
+
   private _importRequestKeywords = '';
 
+  @Output() importFinished = new EventEmitter();
 
   constructor(private _searchService: SearchService,
               private _rolesFrontService: RolesFrontService,
@@ -48,14 +56,52 @@ export class SharedImportProsComponent {
         },
         (err: HttpErrorResponse) => {
           this._translateNotificationsService.error(
-            'ERROR.ERROR',
-            err.error.message
-          );
-          console.error(err);
+            'Importing Error...',
+            ErrorFrontService.getErrorKey(err.error));
+          if (!!err.error.detailedMessage) {
+            this._importingErrors = err.error.detailedMessage;
+            this._slicedErrors = this._importingErrors.slice(0, 10);
+            this._errorsModal = true;
+          }
         }
       );
   }
 
+  public onClickSeeMore() {
+    const currentNumberOfErrors = this._slicedErrors.length;
+    const end = currentNumberOfErrors + 10 > this._importingErrors.length ?
+      this._importingErrors.length : currentNumberOfErrors + 10;
+    this._slicedErrors = this._importingErrors.slice(0, end);
+  }
+
+  public closeModal(event: Event) {
+    event.preventDefault();
+    this.onImportFinished();
+  }
+
+  onImportFinished() {
+    this.importFinished.emit();
+    this._importingErrors = null;
+    this._slicedErrors = null;
+    //this._importingInfos = null;
+    this._errorsModal = false;
+  }
+
+  get importingErrors(): any[] {
+    return this._importingErrors;
+  }
+
+  get errorsModal(): boolean {
+    return this._errorsModal;
+  }
+
+  set errorsModal(value: boolean) {
+    this._errorsModal = value;
+  }
+
+  get slicedErrors(): Array<any> {
+    return this._slicedErrors;
+  }
   get importRequestKeywords(): string {
     return this._importRequestKeywords;
   }
--- src/app/modules/shared/components/shared-import-pros/shared-import-pros.module.ts
+++ src/app/modules/shared/components/shared-import-pros/shared-import-pros.module.ts
@@ -5,13 +5,19 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 // Components
 import { SharedImportProsComponent } from './shared-import-pros.component';
+import {SharedCsvErrorModule} from '../shared-csv-error/shared-csv-error.module';
+import {ModalModule} from '@umius/umi-common-component';
+import {TranslateModule} from '@ngx-translate/core';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    ReactiveFormsModule,
-    FormsModule
-  ],
+    imports: [
+        CommonModule,
+        ReactiveFormsModule,
+        FormsModule,
+        SharedCsvErrorModule,
+        ModalModule,
+        TranslateModule
+    ],
   declarations: [
     SharedImportProsComponent
   ],
